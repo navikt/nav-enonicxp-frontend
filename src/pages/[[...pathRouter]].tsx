60342c7cab076968b77152df8ca7c17922aafdb4
Morten Jansrud <morten.jansrud@nav.no>
2020-09-29 09:36:28 +0200

    Benytt breadcrumbs-endepunkt fra enonic

--- src/pages/[[...pathRouter]].tsx
+++ src/pages/[[...pathRouter]].tsx
@@ -2,25 +2,29 @@ import React from 'react';
 import { routerQueryToEnonicPath } from '../utils/paths';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import ContentToComponentMapper from '../components/ContentToComponentMapper';
-import { fetchPage } from '../utils/fetchContent';
+import { fetchBreadcrumbs, fetchPage } from '../utils/fetchContent';
 import { ContentTypeSchema } from '../types/content-types/_schema';
 import DynamicPageWrapper from '../components/DynamicPageWrapper';
 import { useRouter } from 'next/router';
 import { FallbackPage } from '../components/page-components/fallback-page/FallbackPage';
+import { Breadcrumb } from '../types/breadcrumb';
 
 type Props = {
     content: ContentTypeSchema;
+    breadcrumbs: Breadcrumb[];
 };
 
 const PathRouter = (props: Props) => {
+    const { breadcrumbs, content } = props;
+
     const router = useRouter();
     if (router.isFallback) {
         return <FallbackPage />;
     }
 
     return props?.content ? (
-        <DynamicPageWrapper content={props.content}>
-            <ContentToComponentMapper content={props.content} />
+        <DynamicPageWrapper content={content} breadcrumbs={breadcrumbs}>
+            <ContentToComponentMapper content={content} />
         </DynamicPageWrapper>
     ) : null;
 };
@@ -31,9 +35,11 @@ export const getStaticProps: GetStaticProps = async (context) => {
     );
 
     const content = await fetchPage(enonicPath);
+    const breadcrumbs = await fetchBreadcrumbs(enonicPath);
 
     return {
         props: {
+            breadcrumbs: breadcrumbs,
             content: content,
         },
         revalidate: 1,
