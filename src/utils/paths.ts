7414cf7c73e86338005d840bfc4e6323f1804108
anders-nom <anders@nomerstad.com>
2020-09-30 20:45:11 +0200

    Støtter id som router query, fikser edit-view i content studio

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -6,6 +6,12 @@ export type EnonicContentRef = string;
 export const isEnonicPath = (path: string) =>
     /(www.*.nav.no|^nav.no|^)($|\/$|\/no|\/en|\/se|\/nav.no)/.test(path);
 
+export const isUUID = (id: string) =>
+    id &&
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
+        id
+    );
+
 export const enonicPathToAppPath = (path: string) =>
     isEnonicPath(path) ? path.split(enonicContentBasePath)[1] : null;
 
@@ -14,10 +20,17 @@ export const enonicPathToUrl = (path: string) =>
         ? `${process.env.APP_ORIGIN}${enonicPathToAppPath(path)}`
         : null;
 
-export const routerQueryToEnonicPath = (routerQuery: string | string[]) => {
-    const queryPath = `/${
+export const routerQueryToEnonicPathOrId = (routerQuery: string | string[]) => {
+    const possibleId =
+        typeof routerQuery === 'string' ? routerQuery : routerQuery[0];
+
+    if (isUUID(possibleId)) {
+        return possibleId;
+    }
+
+    const path = `/${
         typeof routerQuery === 'string' ? routerQuery : routerQuery.join('/')
     }`;
 
-    return `${enonicContentBasePath}${queryPath}`;
+    return `${enonicContentBasePath}${path}`;
 };
