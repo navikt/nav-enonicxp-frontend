1b05ee3af17076f82c651f94fd80d4614c5934f9
anders <anders@nomerstad.com>
2020-11-09 13:32:33 +0100

    Håndterer eksternal-link redirects. Oppdaterer next.js

--- src/components/ContentToComponentMapper.tsx
+++ src/components/ContentToComponentMapper.tsx
@@ -6,6 +6,7 @@ import { makeErrorProps } from '../types/content-types/error-props';
 import { DynamicPage } from './page-components/_dynamic/DynamicPage';
 import { FragmentPage } from './page-components/fragment-page/FragmentPage';
 import LargeTablePage from './page-components/large-table-page/LargeTablePage';
+import { ExternalRedirect } from './ExternalRedirect';
 
 export const contentToComponentMap = {
     [ContentType.Error]: ErrorPage,
@@ -16,6 +17,7 @@ export const contentToComponentMap = {
     [ContentType.DynamicPage]: DynamicPage,
     [ContentType.LargeTable]: LargeTablePage,
     [ContentType.TemplatePage]: DynamicPage,
+    [ContentType.ExternalLink]: ExternalRedirect,
 };
 
 type Props = {
--- src/pages/[[...pathRouter]].tsx
+++ src/pages/[[...pathRouter]].tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { routerQueryToEnonicPathOrId } from '../utils/paths';
 import PageBase, { fetchPageBaseProps } from '../components/PageBase';
+import { ContentType } from '../types/content-types/_schema';
 
 export const getStaticProps: GetStaticProps = async (context) => {
     const enonicPath = routerQueryToEnonicPathOrId(
@@ -9,6 +10,14 @@ export const getStaticProps: GetStaticProps = async (context) => {
     );
 
     const props = await fetchPageBaseProps(enonicPath);
+
+    if (props.content.__typename === ContentType.ExternalLink) {
+        return {
+            props: {},
+            revalidate: 1,
+            redirect: { destination: props.content.data.url, statusCode: 301 },
+        };
+    }
 
     return { props: props, revalidate: 1 };
 };
