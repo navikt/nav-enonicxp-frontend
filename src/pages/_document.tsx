ac76a5cddf6d1c6a7083383c417a6793d8932c16
anders <anders@nomerstad.com>
2020-11-16 12:57:31 +0100

    Type for dekoratør-parametre. Refaktor av 404 breadcrumbs-styling.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,17 +6,22 @@ import Document, {
     NextScript,
     DocumentContext,
 } from 'next/document';
-import { DecoratorFragments, getDecorator } from '../utils/fetch-decorator';
+import {
+    DecoratorFragments,
+    DecoratorParams,
+    getDecorator,
+} from '../utils/fetch-decorator';
 import { decoratorParams404 } from '../components/page-components/error-page/errorcode-content/Error404Content';
 import { fetchBreadcrumbs, fetchLanguages } from '../utils/fetch-content';
 import { appPathToXpPath } from '../utils/paths';
 
 type Props = {
     decoratorFragments: DecoratorFragments;
-    is404: boolean;
 };
 
-const decoratorParamsFromContext = async (ctx: DocumentContext) => {
+const decoratorParamsFromContext = async (
+    ctx: DocumentContext
+): Promise<DecoratorParams> => {
     if (ctx.pathname === '/404') {
         return decoratorParams404;
     }
@@ -42,12 +47,11 @@ class MyDocument extends Document<Props> {
         return {
             ...initialProps,
             decoratorFragments,
-            is404: ctx.pathname === '/404',
         };
     }
 
     render() {
-        const { decoratorFragments, is404 } = this.props;
+        const { decoratorFragments } = this.props;
         const { HEADER, FOOTER, SCRIPTS, STYLES } = decoratorFragments;
 
         return (
@@ -58,14 +62,6 @@ class MyDocument extends Document<Props> {
                         src={`${process.env.APP_ORIGIN}/legacy/scripts/jquery.min.js`}
                     />
                     {/* Legacy scripts */}
-                    {/* Hide the context-element of breadcrumbs on the 404-page */}
-                    {is404 && (
-                        <style type={'text/css'}>
-                            {
-                                '.brodsmulesti li:nth-child(2):not(:last-child) {display: none;}'
-                            }
-                        </style>
-                    )}
                     {STYLES}
                 </Head>
                 <body data-portal-component-type="page">
