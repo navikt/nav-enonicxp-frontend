0e9106fb7fff736ca11076255babb98c46d1f02b
Ida Onshus <Ida.Onshus@nav.no>
2020-11-16 15:58:53 +0100

    rydder

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -28,7 +28,7 @@ export const PageWrapper = (props: Props) => {
 
     useEffect(() => {
         onBreadcrumbClick((breadcrumb) => router.push(breadcrumb.url));
-        onLanguageSelect((language) => router.push(language.url));
+        onLanguageSelect((breadcrumb) => router.push(breadcrumb.url));
 
         initAmplitude();
 
