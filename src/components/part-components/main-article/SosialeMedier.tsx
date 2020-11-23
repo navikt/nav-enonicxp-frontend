b81351b8a03eaf424eeb10c22984e564a2d5131f
Ida Onshus <Ida.Onshus@nav.no>
2020-11-23 08:59:57 +0100

    test

--- src/components/part-components/main-article/SosialeMedier.tsx
+++ src/components/part-components/main-article/SosialeMedier.tsx
@@ -3,7 +3,11 @@ import { xpPathToUrl } from '../../../utils/paths';
 import { LenkeUstylet } from '../_common/lenke/LenkeUstylet';
 import './SosialeMedier.less';
 
-const getSocialmediaShareUrl = (el: string, displayName: string, requestUrl: string) => {
+const getSocialmediaShareUrl = (
+    el: string,
+    displayName: string,
+    requestUrl: string
+) => {
     if (!requestUrl) {
         return null;
     }
@@ -11,32 +15,33 @@ const getSocialmediaShareUrl = (el: string, displayName: string, requestUrl: str
     const shareUrl = {
         facebook: `https://www.facebook.com/sharer/sharer.php?u=${requestUrl}&title=${displayName}`,
         twitter: `https://twitter.com/intent/tweet?text=${displayName}:${requestUrl}`,
-        linkedin: `https://www.linkedin.com/shareArticle?mini=true&url=${requestUrl}&title=${displayName}&source=nav.no`
-    }
+        linkedin: `https://www.linkedin.com/shareArticle?mini=true&url=${requestUrl}&title=${displayName}&source=nav.no`,
+    };
 
     return encodeURI(shareUrl[el]) || null;
-}
+};
 
 interface Props {
-    social: string[],
-    displayName: string,
-    contentPath: string
+    social: string[];
+    displayName: string;
+    contentPath: string;
 }
 const SosialeMedier = (props: Props) => {
-
-    const sosialMediaName: {[key: string]: string} = {
+    const sosialMediaName: { [key: string]: string } = {
         linkedin: 'LinkedIn',
         facebook: 'Facebook',
-        twitter: 'Twitter'
-    }
+        twitter: 'Twitter',
+    };
 
-    const socialMedia = props.social?.map((el) => (
-        {
-            type: el,
-            text: `Del på ${sosialMediaName[el]}`,
-            href: getSocialmediaShareUrl(el, props.displayName, xpPathToUrl(props.contentPath))
-        })
-    );
+    const socialMedia = props.social?.map((el) => ({
+        type: el,
+        text: `Del på ${sosialMediaName[el]}`,
+        href: getSocialmediaShareUrl(
+            el,
+            props.displayName,
+            xpPathToUrl(props.contentPath)
+        ),
+    }));
 
     if (socialMedia === []) {
         return null;
@@ -45,18 +50,17 @@ const SosialeMedier = (props: Props) => {
     return (
         <div className="social-media">
             <ul>
-                {socialMedia.map(item => (
+                {socialMedia.map((item) => (
                     <li key={item.type}>
-                        <LenkeUstylet href={item.href} >
+                        <LenkeUstylet href={item.href}>
                             <span className={`share-social share-${item.type}`}>
-                                {item.text}
+                                        {item.text}
                             </span>
                         </LenkeUstylet>
                     </li>
-                ))
-                }
+                ))}
             </ul>
         </div>
     );
-}
+};
 export default SosialeMedier;
