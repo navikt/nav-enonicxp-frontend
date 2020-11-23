f2c526618f21ccc2fd3d76bf54c1cdebbd90bca6
Morten Jansrud <morten.jansrud@nav.no>
2020-11-23 15:49:47 +0100

    Hent env i getInitialProps

--- src/pages/[[...pathRouter]].tsx
+++ src/pages/[[...pathRouter]].tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import PageBase, { fetchPageProps } from '../components/PageBase';
-import dotenv from 'dotenv';
-
-if (process.env.NODE_ENV === 'production') {
-    dotenv.config({ path: '/var/run/secrets/nais.io/vault/.env' });
-}
 
 export const getStaticProps: GetStaticProps = async (context) => {
     const secret = process.env.SERVICE_SECRET as string;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import Document, {
-    Html,
-    Head,
-    Main,
-    NextScript,
-    DocumentContext,
-} from 'next/document';
-import {
-    DecoratorFragments,
-    DecoratorParams,
-    getDecorator,
-} from '../utils/fetch-decorator';
+import Document, { NextScript, DocumentContext } from 'next/document';
+import { Html, Head, Main } from 'next/document';
+import { getDecorator } from '../utils/fetch-decorator';
+import { DecoratorFragments, DecoratorParams } from '../utils/fetch-decorator';
 import { decoratorParams404 } from '../components/page-components/error-page/errorcode-content/Error404Content';
 import { fetchBreadcrumbs, fetchLanguages } from '../utils/fetch-content';
 import { appPathToXpPath } from '../utils/paths';
+import dotenv from 'dotenv';
 
 type Props = {
     decoratorFragments: DecoratorFragments;
@@ -41,6 +34,10 @@ const decoratorParamsFromContext = async (
 
 class MyDocument extends Document<Props> {
     static async getInitialProps(ctx: DocumentContext) {
+        if (process.env.NODE_ENV === 'production') {
+            dotenv.config({ path: '/var/run/secrets/nais.io/vault/.env' });
+        }
+
         const initialProps = await Document.getInitialProps(ctx);
         const decoratorParams = await decoratorParamsFromContext(ctx);
         const decoratorFragments = await getDecorator(decoratorParams);
