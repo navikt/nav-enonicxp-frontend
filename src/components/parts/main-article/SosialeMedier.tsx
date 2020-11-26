aaef14f4bebfc13e6ce889b65ca784922f4f7909
Ida Onshus <Ida.Onshus@nav.no>
2020-11-26 08:05:55 +0100

    test

--- src/components/parts/main-article/SosialeMedier.tsx
+++ src/components/parts/main-article/SosialeMedier.tsx
@@ -51,7 +51,8 @@ const SosialeMedier = (props: Props) => {
         <div
           className="social-media">
             <ul>
-                {socialMedia.map((item) => (
+                {
+                    socialMedia.map((item) => (
                     <li key={item.type}>
                         <LenkeUstylet href={item.href}>
                             <span className={`share-social share-${item.type}`}>
