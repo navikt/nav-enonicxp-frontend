f6bbac6acfd7af84e682d1c805ad659c9772b086
anders <anders@nomerstad.com>
2020-12-04 16:41:01 +0100

    tester

--- src/pages/api/cache-revalidator.ts
+++ src/pages/api/cache-revalidator.ts
@@ -3,7 +3,7 @@ const revalidationPeriodSecs = 1; // process.env.REVALIDATION_PERIOD
 
 const revalidationPeriodMs = revalidationPeriodSecs * 1000;
 
-const maxPods = 8;
+const maxPods = 1;
 const reqsPerPod = 1;
 const reqsPerRevalidation = maxPods * reqsPerPod;
 
@@ -13,10 +13,12 @@ const nets = networkInterfaces();
 const getHandler = async (req, res) => {
     const { secret } = req.headers;
     const { path } = req.query;
-    const url = `${origin}${path}`;
 
+    const myIp = nets.eth0?.[0]?.address || origin;
+    console.log(`Pod IP: ${myIp}`);
+
+    const url = `https://${myIp}:3000${path}`;
     console.log('revalidating cache for ', url);
-    console.log(JSON.stringify(nets));
 
     [...Array(reqsPerRevalidation)].forEach((_, index) => {
         setTimeout(() => {
