5fbfd776b91cae7f2522a46d07a58b0cf8633e27
anders <anders@nomerstad.com>
2020-12-04 19:21:16 +0100

    tester

--- src/pages/api/cache-revalidator.ts
+++ src/pages/api/cache-revalidator.ts
@@ -1,5 +1,5 @@
-import k8s from '@kubernetes/client-node';
 import { fetchWithTimeout } from '../../utils/fetch-utils';
+const k8s = require('@kubernetes/client-node');
 
 const origin = process.env.APP_ORIGIN;
 const revalidationPeriodSecs = 1; // process.env.REVALIDATION_PERIOD
@@ -15,7 +15,7 @@ const nets = networkInterfaces();
 const host = hostname();
 
 const kc = new k8s.KubeConfig();
-kc.loadFromCluster();
+kc.load();
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
 const getHandler = async (req, res) => {
@@ -27,16 +27,24 @@ const getHandler = async (req, res) => {
     console.log(`Pod hostname: ${host}`);
 
     const electorPath = process.env.ELECTOR_PATH;
-    const elector = await fetchWithTimeout(
-        `http://${electorPath}`,
-        1000
-    ).then((res) => (res.ok ? res.json() : {}));
-    console.log(elector.name);
+    const leader = await fetchWithTimeout(`http://${electorPath}`, 1000)
+        .then((res) => (res.ok ? res.json() : {}))
+        .catch(console.error);
+    console.log(`Leader: ${leader.name}`);
 
     const url = `${origin}${path}`;
     console.log('revalidating cache for ', url);
 
-    k8sApi.listNamespacedPod('q6').then((res) => console.log(res.body));
+    k8sApi
+        .listNamespacedPod(
+            'q6',
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            'app=nav-enonicxp-frontend'
+        )
+        .then((res) => console.log(res.body));
 
     [...Array(reqsPerRevalidation)].forEach((_, index) => {
         setTimeout(() => {
