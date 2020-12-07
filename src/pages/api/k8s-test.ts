28688dbd120a70374e09772eb852760533724ceb
anders <anders@nomerstad.com>
2020-12-07 11:21:53 +0100

    tester

--- src/pages/api/k8s-test.ts
+++ src/pages/api/k8s-test.ts
@@ -2,7 +2,7 @@ const k8s = require('@kubernetes/client-node');
 
 const getHandler = (req, res) => {
     const kc = new k8s.KubeConfig();
-    kc.loadFromDefault();
+    kc.loadFromCluster();
     const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
     k8sApi
@@ -14,10 +14,14 @@ const getHandler = (req, res) => {
             undefined,
             'app=nav-enonicxp-frontend'
         )
-        .then((res) => console.log(res.body, res.response))
+        .then((res) => {
+            console.log(res.body, res.response);
+        })
         .catch((e) => {
             console.log(`k8s call failed: ${e}`);
         });
+
+    res.status(200).json(kc.getCurrentCluster());
 };
 
 export default getHandler;
