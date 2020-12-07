aa495caaad545662fc3fb00478fe98d5c5a825cf
anders <anders@nomerstad.com>
2020-12-07 12:00:24 +0100

    tester

--- src/pages/api/k8s-test.ts
+++ src/pages/api/k8s-test.ts
@@ -1,4 +1,5 @@
 const k8s = require('@kubernetes/client-node');
+const request = require('request');
 
 const getHandler = (req, res) => {
     const kc = new k8s.KubeConfig();
@@ -7,12 +8,26 @@ const getHandler = (req, res) => {
     const opts = {};
     kc.applyToRequest(opts);
 
-    const response = fetch(
+    const response = request.get(
         `${kc.getCurrentCluster().server}/api/v1/namespaces/default/pods`,
-        opts
-    )
-        .then((res) => res.json())
-        .catch((e) => console.error(`fetch error: ${e}`));
+        opts,
+        (error, response, body) => {
+            if (error) {
+                console.log(`error: ${error}`);
+            }
+            if (response) {
+                console.log(`statusCode: ${response.statusCode}`);
+            }
+            console.log(`body: ${body}`);
+        }
+    );
+
+    // const response = fetch(
+    //     `${kc.getCurrentCluster().server}/api/v1/namespaces/default/pods`,
+    //     opts
+    // )
+    //     .then((res) => res.json())
+    //     .catch((e) => console.error(`fetch error: ${e}`));
 
     // const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
     // k8sApi
