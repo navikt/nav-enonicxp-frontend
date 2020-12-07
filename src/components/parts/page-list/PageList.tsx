897b0e0aac0895af48e31a9f50dcc9f10b1ac945
Morten Jansrud <morten.jansrud@nav.no>
2020-12-07 13:37:20 +0100

    Legg til støtte for hide_date, hideSectionContentsDate, orderSectionContentsByPublished

--- src/components/parts/page-list/PageList.tsx
+++ src/components/parts/page-list/PageList.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { BEM } from 'utils/bem';
-import { Innholdstittel, Normaltekst } from 'nav-frontend-typografi';
+import { Normaltekst, Undertekst } from 'nav-frontend-typografi';
+import { Innholdstittel } from 'nav-frontend-typografi';
 import Lenke from 'nav-frontend-lenker';
-import { xpPathToAppPath } from '../../../utils/paths';
-import { ContentProps } from '../../../types/content-props/_content-common';
+import { xpPathToAppPath } from 'utils/paths';
+import { formatDate } from 'utils/datetime';
+import { Language, translator } from 'translations';
+import { PageListProps } from 'types/content-props/page-list-props';
 import './PageList.less';
 
-const PageList = (props: ContentProps) => {
+const PageList = (props: PageListProps) => {
     const bem = BEM('page-list');
-    const sectionContents = (props.data?.sectionContents || []).filter(
-        (section) => props._id !== section._id
-    );
+    const { modifiedTime, createdTime, language } = props;
+    const data = props.data;
+    const hidePageDates = data?.hideSectionContentsDate;
+    const hideDatesInList = data?.hide_date;
+    const orderListByPublishedDate = data?.orderSectionContentsByPublished;
+    const sectionContents = (data?.sectionContents || [])
+        .filter((section) => props._id !== section._id)
+        .sort((a, b) =>
+            orderListByPublishedDate
+                ? new Date(b.createdTime).getTime() -
+                  new Date(a.createdTime).getTime()
+                : undefined
+        );
 
     return (
         <div className={bem()}>
@@ -18,10 +31,21 @@ const PageList = (props: ContentProps) => {
             <div className={bem('ingress')}>
                 <Normaltekst>{props.data.ingress}</Normaltekst>
             </div>
+            {hidePageDates === true && (
+                <CreatedAndModifiedDate
+                    language={language}
+                    className={bem('date')}
+                    createdTime={createdTime}
+                    modifiedTime={modifiedTime}
+                />
+            )}
             <div className={bem('list')}>
                 {sectionContents.map((section) => {
                     const { displayName, _path } = section;
-                    const ingress = section.data?.ingress;
+                    const data = section.data;
+                    const ingress = data?.ingress || data?.description;
+                    const modifiedTime = section.modifiedTime;
+                    const createdTime = section.createdTime;
                     return (
                         <div key={section._path} className={bem('row')}>
                             <Normaltekst>
@@ -31,15 +55,42 @@ const PageList = (props: ContentProps) => {
                             </Normaltekst>
                             {ingress && (
                                 <div className={bem('ingress')}>
-                                    <Normaltekst>
-                                        {section.data?.ingress}
-                                    </Normaltekst>
+                                    <Normaltekst>{ingress}</Normaltekst>
                                 </div>
+                            )}
+                            {hideDatesInList === true && (
+                                <CreatedAndModifiedDate
+                                    language={language}
+                                    className={bem('date')}
+                                    createdTime={createdTime}
+                                    modifiedTime={modifiedTime}
+                                />
                             )}
                         </div>
                     );
                 })}
             </div>
+        </div>
+    );
+};
+
+interface DatesProps {
+    language: Language;
+    className: string;
+    createdTime: string;
+    modifiedTime: string;
+}
+
+const CreatedAndModifiedDate = (props: DatesProps) => {
+    const { createdTime, modifiedTime, language, className } = props;
+    const getDateLabel = translator('dates', language);
+    return (
+        <div className={className}>
+            <Undertekst>
+                {`${getDateLabel('published')}: ${formatDate(createdTime)}`}
+                {' | '}
+                {`${getDateLabel('lastChanged')}: ${formatDate(modifiedTime)}`}
+            </Undertekst>
         </div>
     );
 };
