35460472be13eabb94b1cf922d12170d5cf265b2
anders <anders@nomerstad.com>
2020-12-09 10:47:34 +0100

    Variabelnavn/logging

--- src/pages/api/internal/revalidate-cache.ts
+++ src/pages/api/internal/revalidate-cache.ts
@@ -2,17 +2,19 @@ import { networkInterfaces } from 'os';
 import { fetchWithTimeout } from '../../../utils/fetch-utils';
 import Config from '../../../Config';
 
-const revalidateTimeMs = Config.vars.revalidatePeriod * 1000;
+const revalidatePeriodMs = Config.vars.revalidatePeriod * 1000;
 
 const nets = networkInterfaces();
 const podIp = nets.eth0?.[0]?.address;
 
-const regeneratePageCache = (path: string) =>
+const regeneratePageCache = (path: string) => {
+    console.log(`Regenerating page cache for ${path}`);
     fetchWithTimeout(`http://localhost:3000${path}`, 1000, {
         method: 'HEAD',
     }).catch((e) =>
         console.error(`Regeneration for path ${path} on ${podIp} failed - ${e}`)
     );
+};
 
 const revalidateCache = async (req, res) => {
     const { path } = req.query;
@@ -21,7 +23,7 @@ const revalidateCache = async (req, res) => {
         return res.status(400).send('No path specified');
     }
 
-    setTimeout(() => regeneratePageCache(path), revalidateTimeMs);
+    setTimeout(() => regeneratePageCache(path), revalidatePeriodMs);
 
     return res.status(200).send(`Regenerating cache for ${path}`);
 };
