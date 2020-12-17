19166cd2d10f0aa5f1c739670a145e77410729fd
anders <anders@nomerstad.com>
2020-12-17 13:56:35 +0100

    type-feil

--- src/components/_common/lenke/LenkeInline.tsx
+++ src/components/_common/lenke/LenkeInline.tsx
@@ -5,7 +5,7 @@ type Props = {
     href: string;
     className?: string;
     analyticsLabel?: string;
-    children: string;
+    children: React.ReactNode;
 } & React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
 export const LenkeInline = ({ href, className, children, ...rest }: Props) => {
