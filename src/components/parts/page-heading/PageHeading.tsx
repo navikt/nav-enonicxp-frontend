02604480654e7bdc78784be7b80f29ee0aa1b4d4
anders <anders@nomerstad.com>
2021-01-11 13:16:14 +0100

    Fjerner ingress kun på section-page

--- src/components/parts/page-heading/PageHeading.tsx
+++ src/components/parts/page-heading/PageHeading.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Sidetittel } from 'nav-frontend-typografi';
+import { Ingress, Sidetittel } from 'nav-frontend-typografi';
 import { BEM } from 'utils/bem';
-import { ContentProps } from 'types/content-props/_content-common';
+import { ContentProps, ContentType } from 'types/content-props/_content-common';
 import './PageHeading.less';
 
 const bem = BEM('page-heading');
 
 const PageHeading = (props: ContentProps) => {
     const displayName = props.displayName;
+    const ingress =
+        props.__typename !== ContentType.SectionPage && props.data?.ingress;
     return (
         <div className={bem('container')}>
             <Sidetittel>{displayName || 'Tittel'}</Sidetittel>
+            {ingress && (
+                <div className={bem('ingress')}>
+                    <Ingress>{ingress}</Ingress>
+                </div>
+            )}
         </div>
     );
 };
