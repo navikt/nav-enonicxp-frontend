a11f5bdf9e1ebd3ff0313d7f2a3bb02cc82e3d9b
anders <anders@nomerstad.com>
2021-01-11 12:52:46 +0100

    Fjerner ingress fra section-page og transport-page

--- src/components/parts/page-heading/PageHeading.tsx
+++ src/components/parts/page-heading/PageHeading.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Ingress, Sidetittel } from 'nav-frontend-typografi';
+import { Sidetittel } from 'nav-frontend-typografi';
 import { BEM } from 'utils/bem';
 import { ContentProps } from 'types/content-props/_content-common';
 import './PageHeading.less';
@@ -8,15 +8,9 @@ const bem = BEM('page-heading');
 
 const PageHeading = (props: ContentProps) => {
     const displayName = props.displayName;
-    const ingress = props.data?.ingress;
     return (
         <div className={bem('container')}>
             <Sidetittel>{displayName || 'Tittel'}</Sidetittel>
-            {ingress && (
-                <div className={bem('ingress')}>
-                    <Ingress>{ingress}</Ingress>
-                </div>
-            )}
         </div>
     );
 };
