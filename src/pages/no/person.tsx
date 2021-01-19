bfb9a7d2cdf1e6fc6eed72035ea8ebf2a8c0f95a
anders <anders@nomerstad.com>
2021-01-19 12:56:55 +0100

    Tester om service secret er undefined...

--- src/pages/no/person.tsx
+++ src/pages/no/person.tsx
@@ -4,6 +4,7 @@ import Config from '../../Config';
 
 export const getStaticProps: GetStaticProps = async () => {
     const secret = process.env.SERVICE_SECRET as string;
+    console.log(`service secret exists? ${!!secret}`);
     const props = await fetchPageProps(['no', 'privatperson'], false, secret);
     return {
         ...props,
