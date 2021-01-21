ad6a5cda15f442a4f427b0100b2afc19ffad0aae
anders <anders@nomerstad.com>
2021-01-21 15:14:42 +0100

    Fjerner test-page + logging

--- src/pages/Feilside.tsx
+++ src/pages/Feilside.tsx
@@ -2,7 +2,6 @@ import { GetServerSideProps } from 'next';
 import PageBase from '../components/PageBase';
 
 export const getServerSideProps: GetServerSideProps = async () => {
-    console.log('Rendrer 404-side');
     return {
         notFound: true,
     };
