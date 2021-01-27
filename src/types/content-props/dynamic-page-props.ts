9a92c6ba3f1bd1ac5132d100b1772c90d7ef00d1
anders <anders@nomerstad.com>
2021-01-27 11:48:43 +0100

    DynamicPageData type

--- src/types/content-props/dynamic-page-props.ts
+++ src/types/content-props/dynamic-page-props.ts
@@ -1,11 +1,11 @@
 import { ContentType, ContentProps } from './_content-common';
 
-export type DynamicPageData = {
+export type DynamicPageData = Partial<{
     metaDescription: string;
     canonicalUrl: string;
     noindex: boolean;
     feedbackToggle: boolean;
-};
+}>;
 
 export interface DynamicPageProps extends ContentProps {
     __typename: ContentType.DynamicPage;
