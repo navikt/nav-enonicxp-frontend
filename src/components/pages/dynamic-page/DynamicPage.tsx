7fddf1fe658092d425dbd439ef4067a60a99cfe2
anders <anders@nomerstad.com>
2021-02-01 17:26:50 +0100

    Bruker ComponentMapper i DynamicPage

--- src/components/pages/dynamic-page/DynamicPage.tsx
+++ src/components/pages/dynamic-page/DynamicPage.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
 import { ContentProps } from 'types/content-props/_content-common';
-import { ComponentType } from '../../../types/component-props/_component-common';
-import Region from '../../layouts/Region';
+import { ComponentMapper } from '../../ComponentMapper';
 
 export const DynamicPage = (props: ContentProps) => {
-    const { regions } = props.page;
-
-    const editorProps = props.editMode
-        ? {
-              'data-portal-component-type': ComponentType.Page,
-              'data-portal-component': props.page.path,
-          }
-        : undefined;
-
-    return regions ? (
-        <div {...editorProps}>
-            {Object.values(regions).map((region, index) => (
-                <Region pageProps={props} regionProps={region} key={index} />
-            ))}
-        </div>
+    return props.page?.regions ? (
+        <ComponentMapper componentProps={props.page} pageProps={props} />
     ) : (
         <div data-portal-region={'main'} />
     );
