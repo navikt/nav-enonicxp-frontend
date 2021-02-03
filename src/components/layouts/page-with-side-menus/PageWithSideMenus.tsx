c44ae3f079008aec74e54115ce96eee9ce9fd9ac
anders <anders@nomerstad.com>
2021-02-03 11:06:51 +0100

    Fikser meny-styles

--- src/components/layouts/page-with-side-menus/PageWithSideMenus.tsx
+++ src/components/layouts/page-with-side-menus/PageWithSideMenus.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { ContentProps } from '../../../types/content-props/_content-common';
 import { PageWithSideMenusProps } from '../../../types/component-props/layouts/page-with-side-menus';
 import { LayoutContainer } from '../LayoutContainer';
@@ -10,6 +10,11 @@ type Props = {
     layoutProps?: PageWithSideMenusProps;
 };
 
+const getMenuStyle = (sticky: boolean, width: number): CSSProperties => ({
+    ...(sticky && { position: 'sticky' }),
+    ...(width !== undefined && { width: `${width}rem` }),
+});
+
 export const PageWithSideMenus = ({ pageProps, layoutProps }: Props) => {
     const { regions, config } = layoutProps;
 
@@ -17,14 +22,34 @@ export const PageWithSideMenus = ({ pageProps, layoutProps }: Props) => {
         return null;
     }
 
+    const {
+        leftMenuToggle,
+        leftMenuStickyToggle,
+        leftMenuWidth,
+        rightMenuToggle,
+        rightMenuStickyToggle,
+        rightMenuWidth,
+    } = config;
+
+    const leftStyle = getMenuStyle(leftMenuStickyToggle, leftMenuWidth);
+    const rightStyle = getMenuStyle(rightMenuStickyToggle, rightMenuWidth);
+
     return (
         <LayoutContainer pageProps={pageProps} layoutProps={layoutProps}>
-            {config.leftMenuToggle && (
-                <Region pageProps={pageProps} regionProps={regions.leftMenu} />
+            {leftMenuToggle && (
+                <Region
+                    pageProps={pageProps}
+                    regionProps={regions.leftMenu}
+                    regionStyle={leftStyle}
+                />
             )}
             <Region pageProps={pageProps} regionProps={regions.pageContent} />
-            {config.rightMenuToggle && (
-                <Region pageProps={pageProps} regionProps={regions.rightMenu} />
+            {rightMenuToggle && (
+                <Region
+                    pageProps={pageProps}
+                    regionProps={regions.rightMenu}
+                    regionStyle={rightStyle}
+                />
             )}
         </LayoutContainer>
     );
