f5ba21c35f38e01bcca78c049ea506d3ae08f0da
Morten Jansrud <morten.jansrud@nav.no>
2021-02-04 10:58:41 +0100

    Oppdater kommentar for enklere debugging

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withLess = require('@zeit/next-less');
 const withImages = require('next-images');
 const packageJson = require('./package.json');
+const { NAIS_ENV } = process.env;
 
 const navFrontendModuler = Object.keys(packageJson.dependencies).reduce(
     (acc, key) => (key.startsWith('nav-frontend-') ? acc.concat(key) : acc),
@@ -16,13 +17,15 @@ const configWithAllTheThings = (config) =>
     withTranspileModules(withLess(withImages(config)));
 
 const runtimeConfig = {};
-(async () => {
-    console.log('Fetching url-lookup-table');
-    const urlLookupTableUrl = `https://raw.githubusercontent.com/navikt/nav-enonicxp-iac/master/url-lookup-tables/${process.env.NAIS_ENV}.json`;
-    runtimeConfig.urlLookupTable = await fetch(urlLookupTableUrl)
-        .then((res) => res.json())
-        .catch((err) => `Failed to fetch url-lookup-table: ${err}`);
-})();
+if (NAIS_ENV !== 'prod') {
+    (async () => {
+        console.log(`Fetching url-lookup-table from ${NAIS_ENV}`);
+        const urlLookupTableUrl = `https://raw.githubusercontent.com/navikt/nav-enonicxp-iac/master/url-lookup-tables/${NAIS_ENV}.json`;
+        runtimeConfig.urlLookupTable = await fetch(urlLookupTableUrl)
+            .then((res) => res.json())
+            .catch((err) => `Failed to fetch url-lookup-table: ${err}`);
+    })();
+}
 
 module.exports = configWithAllTheThings({
     assetPrefix: process.env.APP_ORIGIN,
