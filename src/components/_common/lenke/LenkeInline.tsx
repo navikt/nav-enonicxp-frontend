d6ba8052bd396233e2a1293e67f2ebace77db683
Morten Jansrud <morten.jansrud@nav.no>
2021-02-04 10:46:46 +0100

    Fjern duplikat lookup fra LenkeInline.tsx

--- src/components/_common/lenke/LenkeInline.tsx
+++ src/components/_common/lenke/LenkeInline.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { LenkeUstylet } from './LenkeUstylet';
-import { getEnvUrl } from 'utils/url-lookup-table';
 
 type Props = {
     href: string;
@@ -9,15 +8,8 @@ type Props = {
     children: React.ReactNode;
 } & React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
-export const LenkeInline = ({ href, className, children, ...rest }: Props) => {
-    const _href = getEnvUrl(href);
-    return (
-        <LenkeUstylet
-            href={_href}
-            className={`lenke ${className || ''}`}
-            {...rest}
-        >
-            {children}
-        </LenkeUstylet>
-    );
-};
+export const LenkeInline = ({ href, className, children, ...rest }: Props) => (
+    <LenkeUstylet href={href} className={`lenke ${className || ''}`} {...rest}>
+        {children}
+    </LenkeUstylet>
+);
