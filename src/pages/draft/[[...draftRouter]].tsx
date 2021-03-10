69251b6bf6a7024e1bf089f7e20e48fdbbc65317
anders <anders@nomerstad.com>
2021-03-10 16:54:40 +0100

    Hacker til visning av ny forside i draft preview...

--- src/pages/draft/[[...draftRouter]].tsx
+++ src/pages/draft/[[...draftRouter]].tsx
@@ -1,6 +1,11 @@
 import { GetServerSideProps } from 'next';
 import PageBase, { fetchPageProps } from '../../components/PageBase';
 
+const isFrontpage = (pathSegments: string[] | string) =>
+    pathSegments?.length === 2 &&
+    pathSegments[0] === 'no' &&
+    pathSegments[1] === 'person';
+
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const secret = context.req.headers.secret as string;
     const pathSegments = context?.params?.draftRouter;
@@ -9,6 +14,11 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
         return {
             notFound: true,
         };
+    }
+
+    // Fetches data for the new frontpage for preview mode
+    if (isFrontpage(pathSegments)) {
+        return await fetchPageProps(['no', 'privatperson'], true, secret);
     }
 
     return await fetchPageProps(pathSegments, true, secret);
