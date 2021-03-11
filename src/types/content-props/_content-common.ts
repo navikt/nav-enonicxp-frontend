59fa38e1ebca0f0697d4652599da6c063d290565
anders <anders@nomerstad.com>
2021-03-11 21:52:33 +0100

    Fjerner ubrukt kode

--- src/types/content-props/_content-common.ts
+++ src/types/content-props/_content-common.ts
@@ -38,7 +38,6 @@ export enum ContentType {
     LargeTable = 'no_nav_navno_LargeTable',
     OfficeInformation = 'no_nav_navno_OfficeInformation',
     PublishingCalendar = 'no_nav_navno_PublishingCalendar',
-    PageWithSideMenus = 'no_nav_navno_ContentPageWithSidemenus',
 }
 
 export type ContentProps = {
