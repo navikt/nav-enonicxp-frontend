b0dc4cfa537f49565a4142d6b6e72debd88bd1d1
anders <anders@nomerstad.com>
2021-03-16 15:27:52 +0100

    Refactor

--- src/utils/decorator-utils-serverside.tsx
+++ src/utils/decorator-utils-serverside.tsx
@@ -11,22 +11,26 @@ const decoratorUrl = process.env.DECORATOR_FALLBACK_URL;
 const decoratorEnv = process.env.ENV as Props['env'];
 const decoratorLocalPort = process.env.DECORATOR_LOCAL_PORT || 8100;
 
-const decoratorComponentsCSR = (query?: string): Components => ({
-    Header: () => <div id="decorator-header"></div>,
-    Styles: () => (
-        <link href={`${decoratorUrl}/css/client.css`} rel="stylesheet" />
-    ),
-    Footer: () => <div id="decorator-footer"></div>,
-    Scripts: () => (
-        <>
-            <div
-                id="decorator-env"
-                data-src={`${decoratorUrl}/env${query || ''}`}
-            ></div>
-            <script async={true} src={`${decoratorUrl}/client.js`}></script>
-        </>
-    ),
-});
+const decoratorComponentsCSR = (params?: DecoratorParams): Components => {
+    const query = objectToQueryString(params);
+
+    return {
+        Header: () => <div id="decorator-header"></div>,
+        Styles: () => (
+            <link href={`${decoratorUrl}/css/client.css`} rel="stylesheet" />
+        ),
+        Footer: () => <div id="decorator-footer"></div>,
+        Scripts: () => (
+            <>
+                <div
+                    id="decorator-env"
+                    data-src={`${decoratorUrl}/env${query || ''}`}
+                ></div>
+                <script async={true} src={`${decoratorUrl}/client.js`}></script>
+            </>
+        ),
+    };
+};
 
 export const getDecoratorComponents = async (
     params?: DecoratorParams
@@ -39,7 +43,7 @@ export const getDecoratorComponents = async (
 
     // Fallback to client-side rendered decorator components if fetch failed
     if (!decoratorComponents) {
-        return decoratorComponentsCSR(objectToQueryString(params));
+        return decoratorComponentsCSR(params);
     }
 
     return decoratorComponents;
