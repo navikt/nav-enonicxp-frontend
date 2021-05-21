5a8cd49512b77a11c7ac7e11171ba35cc8848bcd
anders <anders@nomerstad.com>
2021-05-21 09:46:42 +0200

    Committer MacroMapper

--- src/components/macros/MacroMapper.tsx
+++ src/components/macros/MacroMapper.tsx
@@ -3,6 +3,19 @@ import {
     MacroPropsCommon,
     MacroType,
 } from '../../types/macro-props/_macros-common';
+import { MacroButton } from './button/MacroButton';
+import { MacroButtonBlue } from './button-blue/MacroButtonBlue';
+import { MacroChevronLinkExternal } from './chevron-link-external/MacroChevronLinkExternal';
+import { MacroChevronLinkInternal } from './chevron-link-internal/MacroChevronLinkInternal';
+import { MacroChatbotLink } from './chatbot-link/MacroChatbotLink';
+import { MacroFotnote } from './fotnote/MacroFotnote';
+import { MacroInfoBoks } from './infoboks/MacroInfoBoks';
+import { MacroLenkeFiler } from './lenke-filer/MacroLenkeFiler';
+import { MacroPhoneLink } from './phone-link/MacroPhoneLink';
+import { MacroQuote } from './quote/MacroQuote';
+import { MacroTankestrek } from './tankestrek/MacroTankestrek';
+import { MacroVarselBoks } from './varselboks/MacroVarselBoks';
+import { MacroVideo } from './Video/MacroVideo';
 
 const macroComponents: {
     [key in MacroType]: React.FunctionComponent<MacroPropsCommon>;
@@ -22,12 +35,28 @@ const macroComponents: {
     [MacroType.Video]: MacroVideo,
 };
 
-export const MacroMapper = (props: MacroPropsCommon) => {
-    const { descriptor, ref } = props;
+type Props = {
+    macros: MacroPropsCommon[];
+    macroRef: string;
+};
+
+export const MacroMapper = ({ macros, macroRef }: Props) => {
+    if (!macroRef) {
+        return null;
+    }
+
+    const macroProps = macros?.find((macro) => macro.ref === macroRef);
+
+    if (!macroProps) {
+        console.log(`Invalid macro ref: ${macroRef}`);
+        return <>{`Error: macro with ref ${macroRef} not found!`}</>;
+    }
+
+    const { name } = macroProps;
 
     const MacroComponent = macroComponents[name];
     if (MacroComponent) {
-        return <MacroComponent {...props} />;
+        return <MacroComponent {...macroProps} ref={undefined} />;
     }
 
     return <>{`Unimplemented macro: ${name}`}</>;
