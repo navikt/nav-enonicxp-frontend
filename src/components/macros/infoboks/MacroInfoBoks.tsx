b7acf48f4028833e124726372bcf53913b8e6bd7
anders <anders@nomerstad.com>
2021-05-27 15:55:21 +0200

    Fikser parsing av alertstripe-macroer

--- src/components/macros/infoboks/MacroInfoBoks.tsx
+++ src/components/macros/infoboks/MacroInfoBoks.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { MacroInfoBoksProps } from '../../../types/macro-props/infoBoks';
 import AlertStripe from 'nav-frontend-alertstriper';
+import { ParsedHtml } from '../../ParsedHtml';
 
 export const MacroInfoBoks = ({ config }: MacroInfoBoksProps) => {
     if (!config?.infoBoks) {
         return null;
     }
 
+    // TODO: this should only be plain text, but it seems to be used
+    // with HTML by some editors
     const { infoBoks } = config.infoBoks;
 
-    return <AlertStripe type={'info'}>{infoBoks}</AlertStripe>;
+    return (
+        <AlertStripe type={'info'}>
+            <ParsedHtml htmlProps={infoBoks} />
+        </AlertStripe>
+    );
 };
