91e42f065057018ba0bf231466a9ca7e24f8d0b1
Terje Karlsen <terjeofnorway@gmail.com>
2021-06-01 11:37:36 +0200

    oppdatert styling på meny når in-content

--- src/components/_common/page-navigation-menu/PageNavigationMenu.tsx
+++ src/components/_common/page-navigation-menu/PageNavigationMenu.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import { AnchorLink } from '../../../types/component-props/parts/page-navigation-menu';
 import { AccordionAnchorMenu, AccordionAnchorMenuItem } from '@navikt/ds-react';
+import { classNames, BEM } from '../../../utils/classnames';
+
+import './PageNavigationMenu.less';
 
 type Props = {
     title: string;
     anchorLinks: AnchorLink[];
+    placement: string;
 };
 
-export const PageNavigationMenu = ({ anchorLinks, title }: Props) => {
+export const PageNavigationMenu = ({
+    anchorLinks,
+    title,
+    placement,
+}: Props) => {
+    const bem = BEM(
+        placement === 'content'
+            ? 'inpage-navigation-menu'
+            : 'sidebar-navigation-menu'
+    );
+
     return (
-        <AccordionAnchorMenu title={title}>
-            {anchorLinks?.map((link) => (
-                <AccordionAnchorMenuItem
-                    href={`#${link.anchorId}`}
-                    key={link.anchorId}
-                >
-                    {link.linkText}
-                </AccordionAnchorMenuItem>
-            ))}
-        </AccordionAnchorMenu>
+        <div className={classNames(bem(), bem('wrapper'))}>
+            <AccordionAnchorMenu title={title}>
+                {anchorLinks?.map((link) => (
+                    <AccordionAnchorMenuItem
+                        href={`#${link.anchorId}`}
+                        key={link.anchorId}
+                        className={bem('menu-item')}
+                    >
+                        {link.linkText}
+                    </AccordionAnchorMenuItem>
+                ))}
+            </AccordionAnchorMenu>
+        </div>
     );
 };
