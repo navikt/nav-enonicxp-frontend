9cd66c75e472304672e02c4a1f4c45427f874465
Terje Karlsen <terjeofnorway@gmail.com>
2021-06-03 16:13:19 +0200

    Fikset part-navning

--- src/components/parts/_dynamic/micro-card/MicroCard.tsx
+++ src/components/parts/_dynamic/micro-card/MicroCard.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { MicroCardProps } from '../../../../types/component-props/parts/micro-card';
-import { ParsedHtml } from '../../../ParsedHtml';
 
-export const MicroCard = ({ config }: MicroCardProps) => {
+export const MicroCardPart = ({ config }: MicroCardProps) => {
     console.log(config);
     if (!config?.microcard) {
         return <div>{'Tom felt'}</div>;
     }
     return <div>Hello world!</div>;
 };
-
