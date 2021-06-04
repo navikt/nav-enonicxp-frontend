bcf3c03d0816e67b22a7bd285b4279d607380eb8
Anders Bråthen Nomerstad <anders.brathen.nomerstad@nav.no>
2021-06-04 09:34:26 +0200

    Debugging

--- src/components/macros/html-fragment/MacroHtmlFragment.tsx
+++ src/components/macros/html-fragment/MacroHtmlFragment.tsx
@@ -3,9 +3,13 @@ import { MacroHtmlFragmentProps } from '../../../types/macro-props/html-fragment
 import { ParsedHtml } from '../../ParsedHtml';
 
 export const MacroHtmlFragment = ({ config }: MacroHtmlFragmentProps) => {
-    if (!config?.html_fragment?.processedHtml) {
+    const htmlProps = config?.html_fragment?.processedHtml;
+
+    if (!htmlProps) {
         return null;
     }
 
-    return <ParsedHtml htmlProps={config.html_fragment.processedHtml} />;
+    console.log(`Parsing html from fragment ${config.html_fragment.fragmentId}`);
+
+    return <ParsedHtml htmlProps={htmlProps} />;
 };
