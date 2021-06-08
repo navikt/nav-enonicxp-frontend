ccd4f5567c663a67f62799fa4b78c16420b05cea
eivind.skifjeld@nav.no <eivind.skifjeld@nav.no>
2021-06-08 11:03:56 +0200

    MicroCard komponent

--- src/components/parts/_dynamic/micro-card/MicroCard.tsx
+++ src/components/parts/_dynamic/micro-card/MicroCard.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { MicroCardProps } from '../../../../types/component-props/parts/micro-card';
+import { LenkeBase } from '../../../_common/lenke/LenkeBase';
+import './MicroCard.less';
+
 
 export const MicroCardPart = ({ config }: MicroCardProps) => {
     if (!config.targetPage) {
-        return <div>{'Tom felt'}</div>;
+        return <div >{'Tom felt'}</div>;
     }
-    return <div>Hello world! {config.targetPage.displayName}</div>;
+    return <LenkeBase href={config.targetPage._path}> <div className="micro-card"> {config.targetPage.displayName}</div></LenkeBase>;
 };
+
