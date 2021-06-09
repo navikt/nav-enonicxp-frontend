bf26fcee90167759b21333f47b94570edbde79da
Terje Karlsen <terjeofnorway@gmail.com>
2021-06-09 21:18:41 +0200

    legger til origin og transformStart for ikonene

--- src/components/_common/illustration/Illustration.tsx
+++ src/components/_common/illustration/Illustration.tsx
@@ -21,9 +21,18 @@ export const Illustration = ({
         return null;
     }
 
+    const buildTransformStyling = (icon, defaultStyling) => {
+        if (!icon) {
+            return '';
+        }
+        return icon.transformStart || defaultStyling;
+    };
+
     const { icons } = illustration?.data;
 
     const [icon1, icon2, icon3] = icons;
+
+    console.log(icon1);
 
     return (
         <div className={classNames(bem('image'), className)}>
@@ -33,11 +42,23 @@ export const Illustration = ({
             />
             <div
                 className={classNames(bem('icon'), bem('icon', 'icon1'))}
-                style={{ backgroundImage: `url(${icon1.icon.mediaUrl})` }}
+                style={{
+                    backgroundImage: `url(${icon1.icon.mediaUrl})`,
+                    transform: buildTransformStyling(
+                        icon1,
+                        'translateX(-40%) translateY(40%)'
+                    ),
+                }}
             />
             <div
                 className={classNames(bem('icon'), bem('icon', 'icon3'))}
-                style={{ backgroundImage: `url(${icon3.icon.mediaUrl})` }}
+                style={{
+                    backgroundImage: `url(${icon3.icon.mediaUrl})`,
+                    transform: buildTransformStyling(
+                        icon3,
+                        'translateX(40%) translateY(-40%)'
+                    ),
+                }}
             />
         </div>
     );
