bb466832f7d9d38bfeb707c311cf3dd4e8c29d5a
anders <anders@nomerstad.com>
2021-06-10 16:42:20 +0200

    Rydder i /parts-mappe

---
+++ src/components/parts/_legacy/main-article/Bilde.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Picture } from '../../../../types/content-props/main-article-props';
+import { XpImage } from '../../../_common/image/XpImage';
+
+interface Props {
+    picture?: Picture;
+}
+
+const Bilde = (props: Props) => {
+    const { picture } = props;
+    if (!picture?.target) {
+        return null;
+    }
+
+    const { size, target } = picture;
+
+    const imgClass =
+        size === '40'
+            ? 'figure-small'
+            : size === '70'
+            ? 'figure-medium'
+            : 'figure-full';
+
+    return (
+        <div className="figure-container">
+            <figure className={imgClass}>
+                <XpImage
+                    imageProps={target}
+                    alt={picture.altText || ''}
+                    scale={'max-768'}
+                />
+                {picture.caption && (
+                    <figcaption className="decorated">
+                        {picture.caption}
+                    </figcaption>
+                )}
+            </figure>
+        </div>
+    );
+};
+export default Bilde;
---
+++ src/components/parts/_legacy/main-article/MainArticleText.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { ParsedHtml } from '../../../ParsedHtml';
+import { ProcessedHtmlProps } from '../../../../types/processed-html-props';
+
+const modifyHtml = (htmlText: string, hasTableOfContent: boolean) => {
+    // Fjern tomme headings og br-tagger fra HTML
+    let tmp = htmlText;
+    tmp = tmp?.replace(/<h\d>\s*<\/h\d>/g, '');
+    tmp = tmp?.replace(/<h\d>&nbsp;<\/h\d>/g, '');
+    tmp = tmp?.replace(/<br \/>/g, '');
+
+    // legg på id'er for innholdsfortegnelse
+    if (hasTableOfContent) {
+        let index = 1;
+        tmp = tmp?.replace(/<h3>/g, () => {
+            return `<h3 id="chapter-${index++}" class="chapter-header">`;
+        });
+    }
+
+    return tmp;
+};
+
+interface Props {
+    htmlProps: ProcessedHtmlProps;
+    className: string;
+    hasTableOfContents: boolean;
+}
+
+const MainArticleText = ({
+    htmlProps,
+    className,
+    hasTableOfContents,
+}: Props) => {
+    const modifiedHtml = modifyHtml(
+        htmlProps.processedHtml,
+        hasTableOfContents
+    );
+
+    return (
+        <div className={className}>
+            <ParsedHtml
+                htmlProps={{ ...htmlProps, processedHtml: modifiedHtml }}
+            />
+        </div>
+    );
+};
+export default MainArticleText;
