0d85a088e62698b7de482128a681e622339ae0cf
Anders Bråthen Nomerstad <anders.brathen.nomerstad@nav.no>
2021-06-15 17:31:06 +0200

    Duh...

--- src/components/layouts/page-with-side-menus/PageWithSideMenus.tsx
+++ src/components/layouts/page-with-side-menus/PageWithSideMenus.tsx
@@ -17,6 +17,10 @@ type Props = {
 
 const mobileWidthBreakpoint = 648;
 
+const mqlWidthBreakpoint = windowMatchMedia(
+    `(min-width: ${mobileWidthBreakpoint}px)`
+);
+
 export const PageWithSideMenus = ({ pageProps, layoutProps }: Props) => {
     const { regions, config } = layoutProps;
 
@@ -28,10 +32,6 @@ export const PageWithSideMenus = ({ pageProps, layoutProps }: Props) => {
         };
 
         updateLayout();
-
-        const mqlWidthBreakpoint = windowMatchMedia(
-            `(min-width: ${mobileWidthBreakpoint}px)`
-        );
 
         mqlWidthBreakpoint.addEventListener('change', updateLayout);
         return () => {
