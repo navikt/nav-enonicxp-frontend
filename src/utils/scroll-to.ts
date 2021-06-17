3dc8da0962610283c29c3239954c7b74bc2622bd
Anders Bråthen Nomerstad <anders.brathen.nomerstad@nav.no>
2021-06-17 21:30:10 +0200

    Comment

--- src/utils/scroll-to.ts
+++ src/utils/scroll-to.ts
@@ -3,12 +3,18 @@ export const smoothScrollToTarget = (targetId: string, offset = 0) => {
     if (targetElement) {
         const { scrollX, scrollY } = window;
         const top = targetElement.getBoundingClientRect().top + scrollY;
+
+        // Ensure the target element gets focus...
         targetElement.focus();
+
+        // ...but immediately scroll back to the current position
+        // so we can specify our own scroll behavior and position
         window.scrollTo({
             behavior: 'auto',
             left: scrollX,
             top: scrollY,
         });
+
         window.scrollTo({
             behavior: 'smooth',
             top: top - offset,
