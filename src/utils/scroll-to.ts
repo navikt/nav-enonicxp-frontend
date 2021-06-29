3ea1c5bc1b60b3c7581034c0f3c3ba84429bd79a
anders <anders@nomerstad.com>
2021-06-29 11:00:18 +0200

    Polyfill for scrollto...

--- src/utils/scroll-to.ts
+++ src/utils/scroll-to.ts
@@ -1,23 +1,30 @@
+const windowScrollTo = (targetElement: HTMLElement, offset: number) => {
+    const { scrollY } = window;
+    const top = targetElement.getBoundingClientRect().top + scrollY;
+
+    targetElement.focus({ preventScroll: true });
+
+    window.scrollTo({
+        behavior: 'smooth',
+        top: top - offset,
+    });
+};
+
+const targetScrollIntoView = (targetElement: HTMLElement) => {
+    targetElement.focus({ preventScroll: true });
+    targetElement.scrollIntoView();
+};
+
+const scrollToTarget =
+    typeof window !== 'undefined'
+        ? !!window.scrollTo
+            ? windowScrollTo
+            : targetScrollIntoView
+        : () => null;
+
 export const smoothScrollToTarget = (targetId: string, offset = 0) => {
     const targetElement = document.getElementById(targetId);
     if (targetElement) {
-        const { scrollX, scrollY } = window;
-        const top = targetElement.getBoundingClientRect().top + scrollY;
-
-        // Ensure the target element gets focus...
-        targetElement.focus({ preventScroll: true });
-
-        // ...but immediately scroll back to the current position
-        // so we can specify our own scroll behavior and position
-        // window.scrollTo({
-        //     behavior: 'auto',
-        //     left: scrollX,
-        //     top: scrollY,
-        // });
-
-        window.scrollTo({
-            behavior: 'smooth',
-            top: top - offset,
-        });
+        scrollToTarget(targetElement, offset);
     }
 };
