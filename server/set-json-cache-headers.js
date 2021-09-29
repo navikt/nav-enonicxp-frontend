f2c54f4f3590cd3d701321d60ac046c9bb6f477e
anders-nom <anders@nomerstad.com>
2021-09-29 18:20:16 +0200

    Comment

--- server/set-json-cache-headers.js
+++ server/set-json-cache-headers.js
@@ -2,6 +2,9 @@ const onHeaders = require('on-headers');
 
 const isJsonCacheUrl = (url) => /_next\/data\/.*\.json/.test(url);
 
+// Set the no-cache header on json files from the incremental cache to ensure
+// data requested during client side navigation is always validated if cached
+// by browsers/proxies/CDNs etc
 const setJsonCacheHeaders = (req, res) => {
     if (isJsonCacheUrl(req.url)) {
         onHeaders(res, () => {
