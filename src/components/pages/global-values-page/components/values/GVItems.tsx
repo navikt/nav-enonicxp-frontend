2d872cea5f5dc34538741d62ad052eda378cc8c6
anders-nom <anders@nomerstad.com>
2021-12-03 15:18:08 +0100

    Fjerner dato-sortering som default

--- src/components/pages/global-values-page/components/values/GVItems.tsx
+++ src/components/pages/global-values-page/components/values/GVItems.tsx
@@ -1,16 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { GVItem } from './item/GVItem';
 import { BEM } from '../../../../../utils/classnames';
 import { useGvEditorState } from '../../../../../store/hooks/useGvEditorState';
+import { GlobalValueItem } from '../../../../../types/content-props/global-values-props';
 
 const bem = BEM('gv-items');
 
-export const GVItems = () => {
+const norwegianSort = new Intl.Collator(['no', 'nb', 'nn'], {
+    usage: 'sort',
+}).compare;
+
+const sortByDate = (a: GlobalValueItem, b: GlobalValueItem) =>
+    norwegianSort(a.itemName.toLowerCase(), b.itemName.toLowerCase());
+
+type Props = {
+    sortBy?: 'default' | 'date';
+};
+
+export const GVItems = ({ sortBy = 'default' }: Props) => {
     const { valueItems } = useGvEditorState();
+    const [sortedItems, setSortedItems] = useState(valueItems);
+
+    useEffect(() => {
+        if (sortBy === 'date') {
+            setSortedItems(valueItems.sort(sortByDate));
+        }
+    }, [sortBy, valueItems]);
 
     return (
         <div className={bem()}>
-            {valueItems.map((item) => (
+            {sortedItems.map((item) => (
                 <Fragment key={item.key}>
                     <hr />
                     <GVItem item={item} />
