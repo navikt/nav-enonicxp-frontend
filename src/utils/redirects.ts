b68ab6ca3c0f3110ed491d3aeaf215283753bfb3
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-03-16 10:44:01 +0100

    Redirect target refactor

--- src/utils/redirects.ts
+++ src/utils/redirects.ts
@@ -4,35 +4,37 @@ import {
 } from '../types/content-props/_content-common';
 import { stripXpPathPrefix } from './urls';
 
-export const getTargetIfRedirect = (contentData: ContentProps) => {
+const getTargetPath = (contentData: ContentProps) => {
     switch (contentData?.__typename) {
         case ContentType.SituationPage:
         case ContentType.ProductPage:
         case ContentType.ToolsPage:
         case ContentType.GuidePage:
         case ContentType.ThemedArticlePage:
-            // Redirect to the externalProductUrl if it is defined
-            return (
-                !contentData.isDraft &&
-                stripXpPathPrefix(contentData.data?.externalProductUrl)
-            );
+            return !contentData.isDraft
+                ? contentData.data?.externalProductUrl
+                : null;
         case ContentType.Site:
             return '/no/person';
         case ContentType.InternalLink:
             return contentData.data?.target?._path;
         case ContentType.ExternalLink:
         case ContentType.Url:
-            return stripXpPathPrefix(contentData.data?.url);
+            return contentData.data?.url;
         case ContentType.MainArticleChapter:
             // If the main article chapter content is anything other than a main article
             // we want to redirect to the actual content page. This is provided as a way
             // to gradually migrate individual pages from the chapter structure
-            return (
-                contentData.data.article.__typename !==
-                    ContentType.MainArticle &&
-                stripXpPathPrefix(contentData.data.article._path)
-            );
+            return contentData.data.article.__typename !==
+                ContentType.MainArticle
+                ? contentData.data.article._path
+                : null;
         default:
             return null;
     }
+};
+
+export const getTargetIfRedirect = (contentData: ContentProps) => {
+    const targetPath = getTargetPath(contentData);
+    return targetPath ? stripXpPathPrefix(targetPath) : null;
 };
