52db43b96beaf281ee41de508a4a0c2db4bc85bc
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-03-18 09:50:21 +0100

    Rendrer redirect-page ved clientside redirect fra skyggeside

--- src/components/pages/redirect-page/RedirectPage.tsx
+++ src/components/pages/redirect-page/RedirectPage.tsx
@@ -1,29 +1,33 @@
 import { useEffect } from 'react';
-import { NextRouter, useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import { getTargetIfRedirect } from '../../../utils/redirects';
 import { BodyLong } from '@navikt/ds-react';
 import { LenkeInline } from '../../_common/lenke/LenkeInline';
 import { ContentProps } from '../../../types/content-props/_content-common';
 import { stripXpPathPrefix } from '../../../utils/urls';
 
-const getTarget = (props: ContentProps, router: NextRouter) => {
+const getTarget = (props: ContentProps, isShadow: boolean) => {
     const target = getTargetIfRedirect(props) || stripXpPathPrefix(props._path);
-    if (router.query.shadowRouter) {
+
+    if (isShadow) {
         return `/shadow${target}`;
     }
+
     return target;
 };
 
 export const RedirectPage = (props: ContentProps) => {
     const { editorView, _path } = props;
     const router = useRouter();
-    const target = getTarget(props, router);
+    const isShadow = !!router.query?.shadowRouter;
+    const shouldNotRedirect = editorView || isShadow;
+    const target = getTarget(props, isShadow);
 
     useEffect(() => {
-        // When viewed from the editor, we don't want to redirect. Instead we
-        // render a page showing the redirect target, while also giving access
-        // to the version history selector
-        if (editorView) {
+        // When viewed from the editor or a shadow page, we don't want to redirect. Instead we
+        // render a page showing the redirect target, while also giving access to the version
+        // history selector
+        if (shouldNotRedirect) {
             return;
         }
 
@@ -31,9 +35,9 @@ export const RedirectPage = (props: ContentProps) => {
             console.log(`Redirecting from ${_path} to ${target}`);
             router.push(target);
         }
-    }, [target, editorView, _path, router]);
+    }, [target, shouldNotRedirect, _path, router]);
 
-    return editorView ? (
+    return shouldNotRedirect ? (
         <div className={'redirect-page'}>
             <BodyLong size="medium">
                 {`Dette er en redirect til `}
