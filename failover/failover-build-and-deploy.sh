5e3e7af29d0cc1d09632b2add52b0c23770692e1
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-04-04 11:27:14 +0200

    Kommenterer ut nais deploy

--- failover/failover-build-and-deploy.sh
+++ failover/failover-build-and-deploy.sh
@@ -20,12 +20,12 @@ docker build -t $IMAGE_NAME .
 echo "Pushing docker image"
 docker push $IMAGE_NAME
 
-echo "Deploying to NAIS k8s"
-./deploy \
---apikey="$NAIS_DEPLOY_APIKEY" \
---cluster="$NAIS_CLUSTER_NAME" \
---repository="nav-enonicxp-frontend" \
---resource="/app/.nais/config-failover.yml" \
---var="image=$IMAGE_NAME,$GITHUB_PAT=$GITHUB_PAT" \
---vars="/app/.nais/vars-$APP_ENV-failover.yml" \
---wait=true
+#echo "Deploying to NAIS k8s"
+#./deploy \
+#--apikey="$NAIS_DEPLOY_APIKEY" \
+#--cluster="$NAIS_CLUSTER_NAME" \
+#--repository="nav-enonicxp-frontend" \
+#--resource="/app/.nais/config-failover.yml" \
+#--var="image=$IMAGE_NAME,$GITHUB_PAT=$GITHUB_PAT" \
+#--vars="/app/.nais/vars-$APP_ENV-failover.yml" \
+#--wait=true
