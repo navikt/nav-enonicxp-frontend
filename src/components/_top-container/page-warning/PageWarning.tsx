2a915f7a1a5f672cf1d5cfc68c06334259e3664c
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-04-04 19:52:59 +0200

    Fjerner ubrukt kode

--- src/components/_top-container/page-warning/PageWarning.tsx
+++ src/components/_top-container/page-warning/PageWarning.tsx
@@ -1,13 +1,11 @@
+import React from 'react';
 import { usePageConfig } from 'store/hooks/usePageConfig';
 import { translator } from 'translations';
 import { Translations } from '../../../translations/default';
 import { AlertBox } from '../../_common/alert-box/AlertBox';
-import React, { useState } from 'react';
+import { classNames } from '../../../utils/classnames';
 
 import style from './PageWarning.module.scss';
-import { ClearIcon } from '../../pages/error-page/errorcode-content/clear-icon/ClearIcon';
-import { Button } from '../../_common/button/Button';
-import { classNames } from '../../../utils/classnames';
 
 type Props = {
     labelKey: keyof Translations['pageWarnings'];
@@ -15,37 +13,18 @@ type Props = {
 };
 
 export const PageWarning = ({ labelKey, whiteBg }: Props) => {
-    const [isOpen, setIsOpen] = useState(true);
     const { language } = usePageConfig();
     const getDraftLabels = translator('pageWarnings', language);
 
     return (
-        isOpen && (
-            <div
-                className={classNames(
-                    style.container,
-                    whiteBg && style.whiteBg
-                )}
+        <div className={classNames(style.container, whiteBg && style.whiteBg)}>
+            <AlertBox
+                variant={'warning'}
+                size={'small'}
+                className={style.warning}
             >
-                <AlertBox
-                    variant={'warning'}
-                    size={'small'}
-                    className={style.warning}
-                >
-                    {getDraftLabels(labelKey)}
-                    {/*<Button*/}
-                    {/*    variant={'tertiary'}*/}
-                    {/*    size={'small'}*/}
-                    {/*    onClick={(e) => {*/}
-                    {/*        e.preventDefault();*/}
-                    {/*        setIsOpen(false);*/}
-                    {/*    }}*/}
-                    {/*>*/}
-                    {/*    {'Lukk varsel'}*/}
-                    {/*    <ClearIcon />*/}
-                    {/*</Button>*/}
-                </AlertBox>
-            </div>
-        )
+                {getDraftLabels(labelKey)}
+            </AlertBox>
+        </div>
     );
 };
