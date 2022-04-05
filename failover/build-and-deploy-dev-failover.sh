dfab6a6a975220a4f7a107be778af2134712343a
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-04-05 14:46:17 +0200

    Tillater ikke images fra andre kilder

--- failover/build-and-deploy-dev-failover.sh
+++ failover/build-and-deploy-dev-failover.sh
@@ -34,9 +34,8 @@ else
 fi
 
 GITHUB_PAT=$(<../.github-token)
-IMAGE_NAME_FULL="ghcr.io/navikt/nav-enonicxp-frontend:$IMAGE_NAME"
 
-docker build -f Dockerfile -t "$IMAGE_NAME_FULL" --no-cache --build-arg ENV_FILE="$ENV_FILE" --build-arg SERVICE_SECRET="$SERVICE_SECRET" --build-arg GITHUB_PAT="$GITHUB_PAT" ../.
-docker push "$IMAGE_NAME_FULL"
+docker build -f Dockerfile -t "$IMAGE_NAME" --no-cache --build-arg ENV_FILE="$ENV_FILE" --build-arg SERVICE_SECRET="$SERVICE_SECRET" --build-arg GITHUB_PAT="$GITHUB_PAT" ../.
+docker push "$IMAGE_NAME"
 
-npx run-func ../trigger-workflow.js triggerWorkflow deploy-image.yml "" "{\"image\":\"$IMAGE_NAME_FULL\", \"app_env\":\"$APP_ENV\"}"
+npx run-func ../trigger-workflow.js triggerWorkflow deploy-image.yml "" "{\"image_name\":\"$IMAGE_NAME\", \"app_env\":\"$APP_ENV\"}"
