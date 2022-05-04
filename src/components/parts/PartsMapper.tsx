298001ddbbf938077376412ab339d1ace2503e34
perolavm <per.olav.mariussen@nav.no>
2022-05-04 09:03:55 +0200

    Remove _legacy/LinkLists (not in use) - part II

--- src/components/parts/PartsMapper.tsx
+++ src/components/parts/PartsMapper.tsx
@@ -5,7 +5,6 @@ import {
     PartWithOwnData,
     PartWithPageData,
 } from 'types/component-props/parts';
-import LinkLists from './_legacy/link-lists/LinkLists';
 import { LinkPanelsLegacyPart } from './_legacy/link-panels/LinkPanelsLegacyPart';
 import { MainArticleChapterNavigation } from './_legacy/main-article-chapter-navigation/MainArticleChapterNavigation';
 import MainPanels from './_legacy/main-panels/MainPanels';
@@ -46,7 +45,6 @@ type Props = {
 const partsWithPageData: {
     [key in PartWithPageData]: React.FunctionComponent<ContentProps>;
 } = {
-    [PartType.LinkLists]: LinkLists,
     [PartType.LinkPanels]: LinkPanelsLegacyPart,
     [PartType.MainArticle]: MainArticle,
     [PartType.MainArticleLinkedList]: MainArticleChapterNavigation,
