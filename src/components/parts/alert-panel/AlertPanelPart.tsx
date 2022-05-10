b2b942a228ec2b71e49e5eaccef12620388fcea0
Bjørn Harbo Dahle <bdahle@hey.com>
2022-05-10 14:19:36 +0200

    bruker Link panel i ds

--- src/components/parts/alert-panel/AlertPanelPart.tsx
+++ src/components/parts/alert-panel/AlertPanelPart.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import { Alert, Heading, BodyShort, LinkPanel } from '@navikt/ds-react';
+import { Information } from '@navikt/ds-icons';
 import { AlertPanelPartProps } from '../../../types/component-props/parts/alert-panel';
+import style from './AlertPanelPart.module.scss';
+import { LenkeBase } from 'components/_common/lenke/LenkeBase';
 
 export const AlertPanelPart = ({ config }: AlertPanelPartProps) => {
     if (!config) {
         return null;
     }
 
-    const { ingress, header } = config;
+    const { header, ingress } = config;
 
-    return <div>{'Dette er en ny part!'}</div>;
+    return (
+        <LinkPanel
+            border={false}
+            className={style.alertPanelPart}
+            as={(props) => (
+                <LenkeBase
+                    href="https://familie.nav.no/" // TODO: endre link
+                    // analyticsLabel={linkProps.text}
+                    // component={legacyAnalyticsComponentLabel}
+                    {...props}
+                >
+                    {props.children}
+                </LenkeBase>
+            )}
+        >
+            <Information className={style.informationIcon} />
+            <div className={!ingress && style.ifOnlyHeader}>
+                <LinkPanel.Title>{header}</LinkPanel.Title>
+                {ingress && (
+                    <LinkPanel.Description>{ingress}</LinkPanel.Description>
+                )}
+            </div>
+        </LinkPanel>
+    );
 };
