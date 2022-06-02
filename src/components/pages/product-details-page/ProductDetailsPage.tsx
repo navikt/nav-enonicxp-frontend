6a3816dcf4be6bf822e17eeeff2eed78bdf92031
perolavm <per.olav.mariussen@nav.no>
2022-06-02 11:17:50 +0200

    TopContainer & version-history + ProductDetails + Fjerne less helt fra prosjektet

--- src/components/pages/product-details-page/ProductDetailsPage.tsx
+++ src/components/pages/product-details-page/ProductDetailsPage.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { ComponentMapper } from '../../ComponentMapper';
 import { ProductPageProps } from '../../../types/content-props/dynamic-page-props';
-import { BEM } from '../../../utils/classnames';
 import ErrorPage404 from 'pages/404';
-
-const bem = BEM('product-details-page');
 
 export const ProductDetailsPage = (props: ProductPageProps) => {
     if (!props.editorView) {
         return <ErrorPage404 />;
     }
     return (
-        <div className={bem()}>
-            <div className={bem('content')}>
+        // Samme styling som ProductPage
+        <div className={'productPage'}>
+            <div className={'content'}>
                 <ComponentMapper
                     componentProps={props.page}
                     pageProps={props}
