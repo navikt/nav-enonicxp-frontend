7e2e6ca19842e9340f0e4d9d41583b563cbdd47a
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-06-03 12:08:36 +0200

    EditorHelp

--- src/components/macros/payout-dates/MacroPayoutDates.tsx
+++ src/components/macros/payout-dates/MacroPayoutDates.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { MacroPayoutDatesProps } from '../../../types/macro-props/payout-dates';
 import { PayoutDates } from '../../_common/payout-dates/PayoutDates';
+import { EditorHelp } from '../../_editor-only/editor-help/EditorHelp';
 
 export const MacroPayoutDates = ({ config }: MacroPayoutDatesProps) => {
     const payoutDatesData = config?.payout_dates?.payoutDates?.data;
 
     if (!payoutDatesData) {
-        return null;
+        return (
+            <EditorHelp text={'Macroen mangler data for utbetalingsdatoer'} />
+        );
     }
 
     return <PayoutDates payoutDatesData={payoutDatesData} />;
