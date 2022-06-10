376fefd733470e18500a84236a5a1ad52ec1d238
Bjørn Harbo Dahle <bdahle@hey.com>
2022-06-10 17:34:59 +0200

    animasjon sosiale tjenester

--- src/components/parts/area-card/AreaCardPart.tsx
+++ src/components/parts/area-card/AreaCardPart.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { LinkPanel } from '@navikt/ds-react';
-import { Information } from '@navikt/ds-icons';
 import { AreaCardPartProps } from '../../../types/component-props/parts/area-card';
 import { getSelectableLinkProps } from '../../../utils/links-from-content';
 import { LenkeBase } from 'components/_common/lenke/LenkeBase';
+import { StaticImage } from 'components/_common/image/StaticImage';
 import style from './AreaCardPart.module.scss';
+
+import compass from './gfx/compass.svg';
+import letterS from './gfx/letterS.svg';
+import heart from './gfx/heart.svg';
+import hand from './gfx/hand.svg';
 
 export const AreaCardPart = ({ config }: AreaCardPartProps) => {
     if (!config) {
@@ -31,6 +36,21 @@ export const AreaCardPart = ({ config }: AreaCardPartProps) => {
             )}
         >
             <LinkPanel.Title>{linkProps.text}</LinkPanel.Title>
+            <div className={style.animationArea}>
+                <StaticImage
+                    imageData={compass}
+                    className={style.compass}
+                    alt=""
+                />
+                <StaticImage
+                    imageData={letterS}
+                    className={style.letterS}
+                    alt=""
+                />
+                <div className={style.mask} />
+                <StaticImage imageData={heart} className={style.heart} alt="" />
+                <StaticImage imageData={hand} className={style.hand} alt="" />
+            </div>
         </LinkPanel>
     );
 };
