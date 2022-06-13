cd2e8f91d78a86797e98e7336c7cc2d7559e655b
Bjørn Harbo Dahle <bdahle@hey.com>
2022-06-13 11:02:51 +0200

    generaliser koden for å støtte flere animasjoner

--- src/components/parts/area-card/AreaCardPart.tsx
+++ src/components/parts/area-card/AreaCardPart.tsx
@@ -3,13 +3,9 @@ import { LinkPanel } from '@navikt/ds-react';
 import { AreaCardPartProps } from '../../../types/component-props/parts/area-card';
 import { getSelectableLinkProps } from '../../../utils/links-from-content';
 import { LenkeBase } from 'components/_common/lenke/LenkeBase';
-import { StaticImage } from 'components/_common/image/StaticImage';
 import style from './AreaCardPart.module.scss';
 
-import compass from './gfx/compass.svg';
-import letterS from './gfx/letterS.svg';
-import heart from './gfx/heart.svg';
-import hand from './gfx/hand.svg';
+import { SocialServicesAnimation } from './social-services/SocialServicesAnimation';
 
 export const AreaCardPart = ({ config }: AreaCardPartProps) => {
     if (!config) {
@@ -17,7 +13,6 @@ export const AreaCardPart = ({ config }: AreaCardPartProps) => {
     }
 
     const { link } = config;
-
     const linkProps = getSelectableLinkProps(link);
 
     return (
@@ -37,19 +32,7 @@ export const AreaCardPart = ({ config }: AreaCardPartProps) => {
         >
             <LinkPanel.Title>{linkProps.text}</LinkPanel.Title>
             <div className={style.animationArea}>
-                <StaticImage
-                    imageData={compass}
-                    className={style.compass}
-                    alt=""
-                />
-                <StaticImage
-                    imageData={letterS}
-                    className={style.letterS}
-                    alt=""
-                />
-                <div className={style.mask} />
-                <StaticImage imageData={heart} className={style.heart} alt="" />
-                <StaticImage imageData={hand} className={style.hand} alt="" />
+                <SocialServicesAnimation />
             </div>
         </LinkPanel>
     );
---
+++ src/components/parts/area-card/social-services/SocialServicesAnimation.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StaticImage } from 'components/_common/image/StaticImage';
+
+import compass from './gfx/compass.svg';
+import letterS from './gfx/letterS.svg';
+import heart from './gfx/heart.svg';
+import hand from './gfx/hand.svg';
+
+import style from './SocialServicesAnimation.module.scss';
+
+export const SocialServicesAnimation = () => {
+    return (
+        <>
+            <StaticImage imageData={compass} className={style.compass} alt="" />
+            <StaticImage imageData={letterS} className={style.letterS} alt="" />
+            <div className={style.mask} />
+            <StaticImage imageData={heart} className={style.heart} alt="" />
+            <StaticImage imageData={hand} className={style.hand} alt="" />
+        </>
+    );
+};
