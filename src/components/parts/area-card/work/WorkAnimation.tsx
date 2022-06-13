d35645218e33731be24f1456703a4e7a430762fd
Bjørn Harbo Dahle <bdahle@hey.com>
2022-06-13 16:10:22 +0200

    fullfør animasjon Arbeid

--- src/components/parts/area-card/work/WorkAnimation.tsx
+++ src/components/parts/area-card/work/WorkAnimation.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { StaticImage } from 'components/_common/image/StaticImage';
 
-import compass from './gfx/compass.svg';
-import letterS from './gfx/letterS.svg';
-import heart from './gfx/heart.svg';
-import hand from './gfx/hand.svg';
+import briefcase from './gfx/briefcase.svg';
 
 import style from './WorkAnimation.module.scss';
 
 export const WorkAnimation = () => {
     return (
         <>
-            <StaticImage imageData={compass} className={style.compass} alt="" />
-            <StaticImage imageData={letterS} className={style.letterS} alt="" />
+            <div className={style.letterPartOrange} />
+            <div className={style.letterPartBlue} />
             <div className={style.mask} />
-            <StaticImage imageData={heart} className={style.heart} alt="" />
-            <StaticImage imageData={hand} className={style.hand} alt="" />
+            <StaticImage
+                imageData={briefcase}
+                className={style.briefcase}
+                alt=""
+            />
         </>
     );
 };
