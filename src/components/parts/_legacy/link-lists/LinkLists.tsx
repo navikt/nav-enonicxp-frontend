296c0cbbdc190b6001ecac6cbd2c0b854ffeca32
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-06-14 09:13:53 +0200

    Fjerner ubrukt import

--- src/components/parts/_legacy/link-lists/LinkLists.tsx
+++ src/components/parts/_legacy/link-lists/LinkLists.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { ContentList } from '../../../_common/content-list/ContentList';
 import { LenkeStandalone } from '../../../_common/lenke/LenkeStandalone';
 import { translator } from 'translations';
-import { ContentProps } from '../../../../types/content-props/_content-common';
+import { SectionPageProps } from '../../../../types/content-props/section-page-props';
 
 import style from './LinkLists.module.scss';
-import { SectionPageProps } from '../../../../types/content-props/section-page-props';
 
 const LinkLists = (props: SectionPageProps) => {
     const getLabel = translator('linkLists', props.language);
