18680c47b16876ed7110cb570fbe9d0c0686f9c2
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-06-16 15:11:50 +0200

    Gjeninnfører layoutconfig state med react contexts

---
+++ src/components/layouts/useLayoutConfig.tsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { LayoutType } from '../../types/component-props/layouts';
+import { EmptyObject } from '../../types/util-types';
+
+type LayoutConfig = {
+    type: LayoutType;
+    title?: string;
+};
+
+const LayoutContext = React.createContext<LayoutConfig | EmptyObject>({});
+
+export const useLayoutConfig = () => {
+    const layoutConfig = useContext(LayoutContext);
+    return { layoutConfig, LayoutConfigProvider: LayoutContext.Provider };
+};
