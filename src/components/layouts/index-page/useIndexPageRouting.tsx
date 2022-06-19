f87478565050a40ac2defa51a997e7664921c65f
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-06-20 00:18:30 +0200

    Fjerner unødvendig await

--- src/components/layouts/index-page/useIndexPageRouting.tsx
+++ src/components/layouts/index-page/useIndexPageRouting.tsx
@@ -45,13 +45,13 @@ export const useIndexPageRouting = (pageProps: IndexPageContentProps) => {
         setLocalPageCache({ ...localPageCache, ...cacheEntries });
     };
 
-    const navigate = async (path: string) => {
+    const navigate = (path: string) => {
         const cachedPage = localPageCache[path];
 
         if (cachedPage) {
             setCurrentPageProps(cachedPage);
         } else {
-            await fetchIndexPageContentProps(path).then((contentProps) => {
+            fetchIndexPageContentProps(path).then((contentProps) => {
                 addLocalPageCacheEntries({
                     ...localPageCache,
                     [path]: contentProps,
@@ -100,7 +100,7 @@ export const useIndexPageRouting = (pageProps: IndexPageContentProps) => {
         return () => {
             router.beforePopState(undefined);
         };
-    }, [router, localPageCache, navigate]);
+    }, [router, localPageCache]);
 
     return { currentPageProps, navigate };
 };
