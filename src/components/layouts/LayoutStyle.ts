db43be57d0f2306bb83d362791f51cd04ebc880f
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-06-21 15:03:06 +0200

    Legger til mulighet for top/bottom padding på layouts

--- src/components/layouts/LayoutStyle.ts
+++ src/components/layouts/LayoutStyle.ts
@@ -5,7 +5,8 @@ export const getCommonLayoutStyle = (config: LayoutCommonConfigMixin) => {
         return undefined;
     }
 
-    const { bgColor, marginTop, marginBottom, paddingSides } = config;
+    const { bgColor, marginTop, marginBottom, paddingSides, paddingTopBottom } =
+        config;
 
     return {
         // Check for undefined specifically. We want to allow margin values of 0
@@ -17,6 +18,10 @@ export const getCommonLayoutStyle = (config: LayoutCommonConfigMixin) => {
         ...(paddingSides?._selected === 'custom' && {
             paddingLeft: `${paddingSides.custom?.remValue}rem`,
             paddingRight: `${paddingSides.custom?.remValue}rem`,
+        }),
+        ...(paddingTopBottom?._selected === 'custom' && {
+            paddingTop: `${paddingTopBottom.custom?.top}rem`,
+            paddingBottom: `${paddingTopBottom.custom?.bottom}rem`,
         }),
     };
 };
