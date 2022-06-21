c2f39b5c8e654d0034581664cd338047e2015160
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-06-21 15:19:59 +0200

    Bruker useLayoutEffectClientSide for å hindre irriterende react-feil

---
+++ src/utils/react.ts
@@ -0,0 +1,5 @@
+import { useLayoutEffect } from 'react';
+
+// Hack to prevent irrelevant React warning for useLayoutEffect server-side
+export const useLayoutEffectClientSide =
+    typeof window !== 'undefined' ? useLayoutEffect : () => {};
