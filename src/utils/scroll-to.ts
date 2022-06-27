b3296af739515dd40362687bcc455a320f556a9a
Terje Karlsen <terjeofnorway@gmail.com>
2022-06-27 14:47:22 +0200

    Fikser issues med responsivitet

--- src/utils/scroll-to.ts
+++ src/utils/scroll-to.ts
@@ -44,7 +44,7 @@ export const isElementVisible = (element: Element, parent: Element) => {
 
 export const isHorizontalScrollAtStart = (element: Element) => {
     const elementRect = element.getBoundingClientRect();
-
+    console.log(elementRect.x);
     return elementRect.x === 0;
 };
 
