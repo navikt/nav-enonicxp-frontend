b72ce5ce4532ce3e016626bab11ed197d1b6b4d7
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-07-08 08:56:50 +0200

    Hjelpetekst i CS for produktkort uten config

--- src/types/macro-props/product-card-mini.ts
+++ src/types/macro-props/product-card-mini.ts
@@ -4,7 +4,6 @@ import { ProductTarget } from '../component-props/parts/product-card';
 export interface MacroProductCardMiniProps extends MacroPropsCommon {
     name: MacroType.ProductCardMini;
     config: {
-        string?: string;
         product_card_mini: ProductTarget;
     };
 }
