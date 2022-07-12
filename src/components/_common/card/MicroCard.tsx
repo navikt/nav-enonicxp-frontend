7358efdf2a2769d5ae13bd6b553cc0a5d7223ba8
Terje Karlsen <terjeofnorway@gmail.com>
2022-07-12 13:51:41 +0200

    Fikser mikrokort som hadde falt ut av styling

--- src/components/_common/card/MicroCard.tsx
+++ src/components/_common/card/MicroCard.tsx
@@ -3,6 +3,9 @@ import { CardSize, CardType } from 'types/card';
 import { LenkeBase } from '../lenke/LenkeBase';
 import { useCard } from './useCard';
 
+import style from './Card.module.scss';
+import { classNames } from 'utils/classnames';
+
 export type MicroCardProps = {
     link: LinkProps;
     type: CardType;
@@ -11,8 +14,14 @@ export type MicroCardProps = {
 export const MicroCard = ({ link, type }: MicroCardProps) => {
     const { analyticsProps } = useCard({ type, size: CardSize.Micro, link });
     return (
-        <LenkeBase href={link.url} {...analyticsProps}>
-            <span>{link.text}</span>
+        <LenkeBase
+            href={link.url}
+            {...analyticsProps}
+            className={classNames(style.card, style.inline)}
+        >
+            <div className={classNames(style.bed, type, CardSize.Micro)}>
+                {link.text}
+            </div>
         </LenkeBase>
     );
 };
