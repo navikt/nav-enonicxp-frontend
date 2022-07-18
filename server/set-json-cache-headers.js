21bc0d4b01a91448ed5ace024b1868415d111dc1
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-07-18 15:58:26 +0200

    Revert

--- server/set-json-cache-headers.js
+++ server/set-json-cache-headers.js
@@ -5,10 +5,7 @@ const onHeaders = require('on-headers');
 // by browsers/proxies/CDNs etc
 const setJsonCacheHeaders = (req, res) => {
     onHeaders(res, () => {
-        res.setHeader(
-            'Cache-Control',
-            's-maxage=1000, max-age=1000, must-revalidate, proxy-revalidate'
-        );
+        res.setHeader('Cache-Control', 's-maxage=0, must-revalidate');
     });
 };
 
