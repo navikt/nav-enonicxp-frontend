9ae6a915eb92e1ee7fa5e5d1a60c92507c6b2d00
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-07-18 15:39:05 +0200

    Longshot...

--- server/set-json-cache-headers.js
+++ server/set-json-cache-headers.js
@@ -5,6 +5,7 @@ const onHeaders = require('on-headers');
 // by browsers/proxies/CDNs etc
 const setJsonCacheHeaders = (req, res) => {
     onHeaders(res, () => {
+        res.setHeader('ETag', res.getHeader('etag'));
         res.setHeader(
             'Cache-Control',
             's-maxage=0, max-age=0, must-revalidate, proxy-revalidate'
