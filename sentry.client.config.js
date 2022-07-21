9673adbf91a187cc7c7f2ed3d6029105b37d5e68
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-07-21 13:23:17 +0200

    Sentry dsn env var

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/nextjs';
 import { CaptureConsole } from '@sentry/integrations';
 
 Sentry.init({
-    dsn: 'https://7d6de132401448989071436f6e54c748@sentry.gc.nav.no/131',
+    dsn: process.env.SENTRY_DSN,
     tracesSampleRate: 1.0,
     environment: process.env.ENV,
     integrations: [new CaptureConsole()],
