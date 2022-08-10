3d1a09d86e201fbb82500a9b7f53550bd31cd611
Terje Karlsen <terjeofnorway@gmail.com>
2022-08-10 16:05:16 +0200

    Fikser feil oversettelse for forms

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -15,7 +15,7 @@ const productTaxonomies: { [key in Taxonomy]: string } = {
     [Taxonomy.FOR_PROVIDERS]: 'For providers',
     [Taxonomy.MEASURES]: 'Measures',
     [Taxonomy.RIGHTS]: 'Counselling',
-    [Taxonomy.FORMS]: 'Skjema',
+    [Taxonomy.FORMS]: 'Forms',
 };
 
 const areas: { [key in Area]: string } = {
