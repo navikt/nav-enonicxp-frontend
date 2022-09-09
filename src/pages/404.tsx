8ec99905e33e158ea4a8f5d0377955659f730982
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-09-09 16:17:26 +0200

    Comment + sentry-logging

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,23 @@
 import React, { useEffect } from 'react';
-import { make404Props } from '../utils/make-error-props';
-import { PageBase } from '../components/PageBase';
+import { make404Props } from 'utils/make-error-props';
+import { PageBase } from 'components/PageBase';
 import { useRouter } from 'next/router';
+import * as Sentry from '@sentry/react';
 
 export const ErrorPage404 = () => {
     const props = make404Props();
     const router = useRouter();
 
     useEffect(() => {
+        // If the 404 is triggered client-side, the pathname will refer to the
+        // next.js page route. Client-side 404 can sometimes be a result of badly formed
+        // links to external apps, so we do a full reload to ensure such links will resolve
+        // anyway.
         if (router.pathname !== '/404') {
-            console.log('reloading');
+            Sentry.captureMessage(
+                `Client-side 404 error on path: ${router.asPath}`,
+                'error'
+            );
             window.location.reload();
         }
     }, []);
