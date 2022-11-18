acf1312bb7fc9d7dd5d34c5dc7fb1d0b7774ce77
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-11-18 15:32:37 +0100

    Tester revalidator-proxy-heartbeat

---
+++ server/revalidator-proxy-heartbeat.test.ts
@@ -0,0 +1,24 @@
+import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
+
+const revalidatorProxyOrigin = 'revalidator-proxy';
+
+describe('Revalidator proxy heartbeat', () => {
+    enableFetchMocks();
+    process.env.REVALIDATOR_PROXY_ORIGIN = revalidatorProxyOrigin;
+    process.env.ENV = 'localhost';
+
+    test('Should call revalidator proxy', async () => {
+        const { initRevalidatorProxyHeartbeat } = await import(
+            './revalidator-proxy-heartbeat'
+        );
+
+        fetchMock.mockResponse('Hello!');
+
+        initRevalidatorProxyHeartbeat();
+
+        expect(fetchMock.mock.calls.length).toEqual(1);
+        expect(fetchMock.mock.calls[0][0]).toMatch(
+            new RegExp(`^${revalidatorProxyOrigin}`)
+        );
+    });
+});
