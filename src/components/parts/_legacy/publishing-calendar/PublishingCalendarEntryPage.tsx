5c0641f94f172dbc94501242c66e48836bdf86db
perolavm <per.olav.mariussen@nav.no>
2022-12-02 12:18:54 +0100

    404 i frontend når det ikke er forhåndsvisning

--- src/components/parts/_legacy/publishing-calendar/PublishingCalendarEntryPage.tsx
+++ src/components/parts/_legacy/publishing-calendar/PublishingCalendarEntryPage.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { classNames } from 'utils/classnames';
+import ErrorPage404 from 'pages/404';
 import { PublishingCalendarEntryProps } from 'types/content-props/publishing-calendar-props';
 import PublishingCalendarEntry from './PublishingCalendarEntry';
 import { Table } from '@navikt/ds-react';
@@ -8,6 +9,10 @@ import style from './PublishingCalendar.module.scss';
 
 // For preview only - hacks layout from PublishingCalendar
 const PublishingCalendarEntryPage = (props: PublishingCalendarEntryProps) => {
+    if (!props.editorView) {
+        return <ErrorPage404 />;
+    }
+
     return (
         <div className = {classNames('layout', 'layout__main')}>
             <div className={classNames('region', 'region region__first')}>
