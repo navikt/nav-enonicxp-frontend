d0cbc7ca2787f951436d6884f1f935d0c9d4ca59
Terje Karlsen <terjeofnorway@gmail.com>
2022-12-12 12:08:32 +0100

    Legger til avviksmelding på forsidechatmodul

--- src/components/_common/chatbot/ChatbotLinkPanel.tsx
+++ src/components/_common/chatbot/ChatbotLinkPanel.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { translator } from 'translations';
 import { LinkPanelNavno } from '../linkpanel/LinkPanelNavno';
 import { openChatbot } from '@navikt/nav-dekoratoren-moduler';
 
 import style from './ChatbotLinkPanel.module.scss';
+import { usePageConfig } from 'store/hooks/usePageConfig';
+import { Alert } from '@navikt/ds-react';
 
 type Props = { analyticsGroup: string; linkText: string; ingress: string };
 
@@ -11,6 +14,8 @@ export const ChatbotLinkPanel = ({
     linkText,
     ingress,
 }: Props) => {
+    const { language } = usePageConfig();
+    const getTranslations = translator('contactPoint', language);
     return (
         <LinkPanelNavno
             className={style.chat}
@@ -22,6 +27,9 @@ export const ChatbotLinkPanel = ({
                 openChatbot();
             }}
         >
+            <Alert variant="warning" inline className={style.alert}>
+                {getTranslations('chat').downAlert}
+            </Alert>
             {ingress}
         </LinkPanelNavno>
     );
