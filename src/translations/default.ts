2ae23b57211b64e0598640bc3a6a6ca97e7abaeb
Eivind Skifjeld <eivind.skifjeld@gmail.com>
2023-01-05 15:09:18 +0100

    Fjernet Selvstendig næringsdrivende

--- src/translations/default.ts
+++ src/translations/default.ts
@@ -62,7 +62,6 @@ const areas: { [key in Area]: string } = {
     [Area.MUNICIPALITY]: 'For kommunen',
     [Area.OTHER]: 'Annet',
     [Area.PENSION]: 'Pensjon',
-    [Area.SELF_EMPLOYED]: 'For selvstendig næringsdrivende',
     [Area.SOCIAL_COUNSELLING]: 'Økonomisk sosialhjelp, råd og veiledning',
     [Area.WORK]: 'Arbeid',
 };
