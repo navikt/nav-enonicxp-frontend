52a333a2fc2a2e89ee3346d307b6fd14d340c5ab
Bjørn Harbo Dahle <bdahle@hey.com>
2023-01-18 11:56:07 +0100

    test å disable regelen i dev

--- src/components/parts/frontpage-contact/FrontPageContactAlert.tsx
+++ src/components/parts/frontpage-contact/FrontPageContactAlert.tsx
@@ -13,6 +13,8 @@ export const FrontpageContactAlert = ({ alertText, yellow = false }: Props) => {
     return (
         //Inspirert av Alert fra Designsystemet, med noen modifikasjoner på ikon og styling.
         <div
+            // Av en eller annen grunn må jeg disable denne regelen. Npm run lint virker lokalt men ikke ved deploy til dev
+            // eslint-disable-next-line css-modules/no-undef-class
             className={`${style.alert} ${yellow ? style.yellow : style.white}`}
         >
             <Warning title="Advarsel" className={style.alertIcon} />
