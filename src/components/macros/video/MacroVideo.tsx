67cef87fb0bef9b5281f52dbfb22189f15d6620d
Bjørn Harbo Dahle <bdahle@hey.com>
2023-02-08 11:29:47 +0100

    bytt til video med useState

--- src/components/macros/video/MacroVideo.tsx
+++ src/components/macros/video/MacroVideo.tsx
@@ -1,34 +1,46 @@
 import { BodyShort, Detail } from '@navikt/ds-react';
-import React from 'react';
+import { set } from 'immer/dist/internal';
+import React, { useState } from 'react';
 import { MacroVideoProps } from '../../../types/macro-props/video';
 import style from './MacroVideo.module.scss';
 
 export const MacroVideo = ({ config }: MacroVideoProps) => {
+    const [isClicked, setIsClicked] = useState(false);
+
     if (!config?.video) {
         return null;
     }
     const { video, title } = config.video;
     return (
-        // <div className={style.macroVideo}>
-        //     <iframe
-        //         title={`Video: ${title}`}
-        //         src={video}
-        //         allow={'fullscreen'}
-        //     />
-        // </div>
-
-        <figure className={style.figure}>
-            <img
-                className={style.previewImage}
-                src="https://eb5c686abfd0ac2c5fae39833b9cd350-httpcache0-15227-cachedown0.dna.ip-only.net/15227-cachedown0/assets/2018-09-04/daf2f6bd-00015227/daf2f6bd-00015227725.jpg"
-                alt=""
-            />
-            <figcaption>
-                <BodyShort className={style.text}>Se video "{title}"</BodyShort>
-                <Detail className={style.text}>
-                    Varighet er 02.33 minutter
-                </Detail>
-            </figcaption>
-        </figure>
+        <>
+            {isClicked ? (
+                <div className={style.macroVideo}>
+                    <iframe
+                        title={`Video: ${title}`}
+                        src={video}
+                        allow={'fullscreen'}
+                    />
+                </div>
+            ) : (
+                <figure
+                    className={style.figure}
+                    onClick={() => setIsClicked(true)}
+                >
+                    <img
+                        className={style.previewImage}
+                        src="https://eb5c686abfd0ac2c5fae39833b9cd350-httpcache0-15227-cachedown0.dna.ip-only.net/15227-cachedown0/assets/2018-09-04/daf2f6bd-00015227/daf2f6bd-00015227725.jpg"
+                        alt=""
+                    />
+                    <figcaption>
+                        <BodyShort className={style.text}>
+                            Se video "{title}"
+                        </BodyShort>
+                        <Detail className={style.text}>
+                            Varighet er 02.33 minutter
+                        </Detail>
+                    </figcaption>
+                </figure>
+            )}
+        </>
     );
 };
