251b255d834d0a09ef0aea1be0af278628c8927f
Terje Karlsen <terjeofnorway@gmail.com>
2023-04-03 18:50:27 +0200

    Skriver om OfficeEditorialPage til modules

--- src/components/pages/office-editorial-page/OfficeEditorialPage.tsx
+++ src/components/pages/office-editorial-page/OfficeEditorialPage.tsx
@@ -3,11 +3,13 @@ import { ComponentMapper } from '../../ComponentMapper';
 import { OfficeEditorialPageProps } from '../../../types/content-props/dynamic-page-props';
 import { ThemedPageHeader } from '../../_common/headers/themed-page-header/ThemedPageHeader';
 
+import styles from './OfficeEditorialPage.module.scss';
+
 export const OfficeEditorialPage = (props: OfficeEditorialPageProps) => {
     return (
-        <div className={'officeEditorialPage'}>
+        <div className={styles.officeEditorialPage}>
             <ThemedPageHeader contentProps={props} showTimeStamp={false} />
-            <div className={'content'}>
+            <div className={styles.content}>
                 <ComponentMapper
                     componentProps={props.page}
                     pageProps={props}
