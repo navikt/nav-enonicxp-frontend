2152f70e8911cb2b07feef3e3f311455bc02108c
Terje Karlsen <terjeofnorway@gmail.com>
2023-04-03 15:00:04 +0200

    Fjerner sjekk av utkast-prefix

--- src/components/pages/office-branch-page/OfficeBranchPage.tsx
+++ src/components/pages/office-branch-page/OfficeBranchPage.tsx
@@ -3,8 +3,6 @@ import { ComponentMapper } from '../../ComponentMapper';
 import { OfficeBranchPageProps } from '../../../types/content-props/dynamic-page-props';
 import { OfficePageHeader } from '../../_common/headers/office-page-header/OfficePageHeader';
 import { OfficeDetails } from 'components/_common/office-details/OfficeDetails';
-import { useRouter } from 'next/router';
-import ErrorPage404 from 'pages/404';
 
 export const OfficeBranchPage = (props: OfficeBranchPageProps) => {
     const editorialPage = props.editorial;
@@ -12,14 +10,6 @@ export const OfficeBranchPage = (props: OfficeBranchPageProps) => {
     if (!editorialPage) {
         console.error(`No editorial page found for ${props.displayName}`);
     }
-
-    /* This to be removed before pilot starts after easter */
-    const router = useRouter();
-
-    if (!router.route.includes('utkast')) {
-        return <>Finnes ikke</>;
-    }
-    /* End */
 
     return (
         <div className={'officeBranchPage'}>
