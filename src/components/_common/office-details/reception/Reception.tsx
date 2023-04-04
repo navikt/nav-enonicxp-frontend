2d16b57e2476b2f4f1edb3be9758752f8516b28c
Terje Karlsen <terjeofnorway@gmail.com>
2023-04-04 10:44:32 +0200

    Bedre typesjekk på feil hvis tom receptionArray

--- src/components/_common/office-details/reception/Reception.tsx
+++ src/components/_common/office-details/reception/Reception.tsx
@@ -19,10 +19,17 @@ export const Reception = ({ receptions }: LocationsProps) => {
     const getOfficeTranslations = translator('office', language);
 
     const getLocation = (reception: AudienceReception) => {
-        return reception.stedsbeskrivelse || reception.besoeksadresse.poststed;
+        if (!reception) {
+            return '(Ukjent sted)';
+        }
+        return (
+            reception.stedsbeskrivelse ||
+            reception.besoeksadresse.poststed ||
+            '(Ukjent sted)'
+        );
     };
 
-    const firstLocation = getLocation(receptionArray[0]) || '(Ukjent sted)';
+    const firstLocation = getLocation(receptionArray[0]);
     const [state, setState] = useState(firstLocation);
 
     if (!receptions || receptions.length === 0) {
