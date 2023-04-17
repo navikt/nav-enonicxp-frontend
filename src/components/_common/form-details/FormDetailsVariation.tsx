f306f340eb04475e2234145253637c67e680d5a9
Terje Karlsen <terjeofnorway@gmail.com>
2023-04-17 17:09:32 +0200

    Fikser feil i sjekk av label på skjemavariasjon

--- src/components/_common/form-details/FormDetailsVariation.tsx
+++ src/components/_common/form-details/FormDetailsVariation.tsx
@@ -14,7 +14,7 @@ export const FormDetailsVariation = (props: FormsListItemProps) => {
     const { variation, index, direction } = props;
     const { url, label } = variation;
 
-    if (!url || label) {
+    if (!url || !label) {
         // url or label is not required in CS as some form details are ment to only contain informational text
         // via the form details title and ingress.
         return null;
