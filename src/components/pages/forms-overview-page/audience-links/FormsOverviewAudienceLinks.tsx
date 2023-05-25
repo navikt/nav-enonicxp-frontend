405da06aba1ebdd1e05c36b5ef25965442809ee6
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-05-25 18:41:31 +0200

    Endrer layout på seksjon for innganger til andre oversikter

--- src/components/pages/forms-overview-page/audience-links/FormsOverviewAudienceLinks.tsx
+++ src/components/pages/forms-overview-page/audience-links/FormsOverviewAudienceLinks.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { LenkeInline } from 'components/_common/lenke/LenkeInline';
 import { Ingress } from '@navikt/ds-react';
+import { LenkeInline } from 'components/_common/lenke/LenkeInline';
 
 import style from './FormsOverviewAudienceLinks.module.scss';
 
@@ -12,11 +12,9 @@ export const FormsOverviewAudienceLinks = ({ links }: Props) => {
     return (
         <ul className={style.list}>
             {links.map((link) => (
-                <li key={link.url} className={style.item}>
-                    <Ingress>
-                        <LenkeInline href={link.url}>{link.text}</LenkeInline>
-                    </Ingress>
-                </li>
+                <Ingress as={'li'} key={link.url} className={style.item}>
+                    <LenkeInline href={link.url}>{link.text}</LenkeInline>
+                </Ingress>
             ))}
         </ul>
     );
