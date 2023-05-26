9bb7010c2ffe4f6541b25d048addb66767e95f66
Terje Karlsen <terjeofnorway@gmail.com>
2023-05-26 09:53:39 +0200

    Forbedrer sjekk av makro for skjemadetaljer

--- src/components/macros/form-details/MacroFormDetails.tsx
+++ src/components/macros/form-details/MacroFormDetails.tsx
@@ -3,12 +3,11 @@ import { MacroFormDetailsProps } from 'types/macro-props/form-details';
 
 export const MacroFormDetails = ({ config }: MacroFormDetailsProps) => {
     const macroConfig = config?.form_details;
+    const formDetailsData = macroConfig?.targetFormDetails?.data;
 
-    if (!macroConfig || !macroConfig?.targetFormDetails?.data) {
+    if (!macroConfig || !formDetailsData) {
         return null;
     }
-
-    const formDetailsData = macroConfig?.targetFormDetails?.data;
 
     const displayConfig = {
         showTitle: macroConfig.showTitle,
