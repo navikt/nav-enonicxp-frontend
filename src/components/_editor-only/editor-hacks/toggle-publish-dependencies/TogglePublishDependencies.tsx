bbdad915e2c5580046cdfc400506cd012ecf964b
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-05-26 12:09:23 +0200

    Rydder v2

--- src/components/_editor-only/editor-hacks/toggle-publish-dependencies/TogglePublishDependencies.tsx
+++ src/components/_editor-only/editor-hacks/toggle-publish-dependencies/TogglePublishDependencies.tsx
@@ -70,6 +70,22 @@ const mutationCallback: MutationCallback = (mutations) => {
 const enablePublishDialogObserver = () => {
     console.log('Enabling togglePublishDependenciesObserver');
 
+    // If the observer was previously defined, the callback function may have
+    // gone out of scope and will no longer be active
+    // The observer may still have collected records which has not been processed.
+    // We process them and disconnect the old observer
+    if (parent.window.togglePublishDependenciesObserver) {
+        const records =
+            parent.window.togglePublishDependenciesObserver.takeRecords();
+
+        mutationCallback(
+            records,
+            parent.window.togglePublishDependenciesObserver
+        );
+
+        parent.window.togglePublishDependenciesObserver.disconnect();
+    }
+
     parent.window.togglePublishDependenciesObserver = new MutationObserver(
         mutationCallback
     );
@@ -86,26 +102,11 @@ const enablePublishDialogObserver = () => {
 
 export const TogglePublishDependencies = () => {
     useEffect(() => {
-        // If the observer was previously defined, the callback function will
-        // have gone out of scope and will no longer be active
-        // The observer may still have collected records which has not been processed.
-        // We process them and create a new observer
-
-        const records =
-            parent.window.togglePublishDependenciesObserver?.takeRecords();
-        if (records) {
-            console.log('Processing records');
-            mutationCallback(
-                records,
-                parent.window.togglePublishDependenciesObserver
-            );
-        }
-
-        enablePublishDialogObserver();
-
         // Run the uncheck function on mount, in case the frontend was not loaded
         // in the editor preview when the publish dialog first appeared
         uncheckPublishAllDependants();
+
+        enablePublishDialogObserver();
     }, []);
 
     return null;
