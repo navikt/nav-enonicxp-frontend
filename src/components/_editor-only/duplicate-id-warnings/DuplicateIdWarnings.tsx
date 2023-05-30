dca734f8878f1cd7ac181ad86d0ed698f240e309
Anders Bråthen Nomerstad <anders.brathen.nomerstad@nav.no>
2023-05-30 22:19:56 +0200

    Tydeliggjør varsel for duplikate id'er

--- src/components/_editor-only/duplicate-id-warnings/DuplicateIdWarnings.tsx
+++ src/components/_editor-only/duplicate-id-warnings/DuplicateIdWarnings.tsx
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { EditorHelp } from 'components/_editor-only/editor-help/EditorHelp';
+import { removeDuplicates } from '../../../utils/arrays';
+import { BodyLong } from '@navikt/ds-react';
+import { Header } from '../../_common/headers/Header';
 
 export const DuplicateIdWarnings = () => {
-    const [elements, setElements] = useState<HTMLElement[]>([]);
+    const [elementsWithDupeIds, setElementsWithDupeIds] = useState<
+        HTMLElement[]
+    >([]);
+
+    const uniqueDupeIds = removeDuplicates(
+        elementsWithDupeIds,
+        (a, b) => a.id === b.id
+    ).map((element) => element.id);
 
     useEffect(() => {
         // Delay the check slightly to avoid certain false positives.
@@ -19,19 +29,37 @@ export const DuplicateIdWarnings = () => {
                 )
             );
 
-            setElements(elementsWithDuplicateIds);
+            setElementsWithDupeIds(elementsWithDuplicateIds);
         }, 1000);
     }, []);
 
     return (
         <>
-            {elements.map((element) => {
+            {uniqueDupeIds.length > 0 && (
+                <div>
+                    <Header
+                        level={'2'}
+                    >{`Obs! Denne siden har ${uniqueDupeIds.length} id'er med duplikate forekomster:`}</Header>
+                    <ul>
+                        {uniqueDupeIds.map((id) => (
+                            <li key={id}>{id}</li>
+                        ))}
+                    </ul>
+                    <BodyLong>
+                        {
+                            'Hver id må være unike på siden, bl.a. for at anker-lenker skal fungere konsistent.'
+                        }
+                    </BodyLong>
+                </div>
+            )}
+            {elementsWithDupeIds.map((element, index) => {
                 return createPortal(
                     <EditorHelp
-                        text={`Obs! Elementet ovenfor har en duplikat id: "${element.id}" - id'er må være unike på siden for at anker-lenker skal fungere konsistent!`}
+                        text={`Elementet ovenfor har en duplikat id: "${element.id}"`}
                         type={'error'}
                     />,
-                    element
+                    element,
+                    `${element.id}-${index}`
                 );
             })}
         </>
