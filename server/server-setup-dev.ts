1b48fae2f48c11294df3c4497413560a03777166
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-06-07 08:17:24 +0200

    Sjekker på IP

--- server/server-setup-dev.ts
+++ server/server-setup-dev.ts
@@ -4,8 +4,12 @@ import { NextServer } from 'next/dist/server/next';
 
 const LOGIN_COOKIE = 'dev-login';
 
+const isNavIp = (ip: string) => ip.startsWith('155.55');
+
 export const serverSetupDev = (expressApp: Express, nextApp: NextServer) => {
     const nextRequestHandler = nextApp.getRequestHandler();
+
+    expressApp.set('trust proxy', true);
 
     expressApp.all('*', (req, res, next) => {
         res.setHeader('X-Robots-Tag', 'noindex');
@@ -18,14 +22,14 @@ export const serverSetupDev = (expressApp: Express, nextApp: NextServer) => {
 
     expressApp.get('/login', (req, res) => {
         return res
-            .cookie(LOGIN_COOKIE, true, { maxAge: 3600 * 24 * 365 })
+            .cookie(LOGIN_COOKIE, true, { maxAge: 3600 * 24 })
             .redirect(302, '/');
     });
 
     expressApp.all('*', cookieParser(), (req, res, next) => {
-        console.log(req.ip);
+        console.log(`Client ip: ${req.ip} ${JSON.stringify(req.ips)}`);
 
-        if (req.cookies[LOGIN_COOKIE]) {
+        if (isNavIp(req.ip) || req.cookies[LOGIN_COOKIE]) {
             return next();
         }
 
