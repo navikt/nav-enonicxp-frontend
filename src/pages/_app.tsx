8f83d3fa380b78949d418ed539541ea82ef37439
Terje Karlsen <terjeofnorway@gmail.com>
2023-08-09 11:58:00 +0200

    Legger til støtte for nais-frontend observability

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { initializeFaro } from '@grafana/faro-web-sdk';
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
 
 import 'global.scss';
+
+initializeFaro({
+    url: process.env.REACT_APP_TELEMETRY_URL,
+    app: {
+        name: 'nav-enonicxp-frontend',
+        version: process.env.RELEASE_TAG,
+    },
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
