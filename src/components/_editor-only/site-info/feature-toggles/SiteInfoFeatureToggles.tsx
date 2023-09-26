50a4f574607a805e847e8f24d754264b830e6dc5
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-09-26 13:25:59 +0200

    Legger til feature-toggles for AutoReloadDisableHack

--- src/components/_editor-only/site-info/feature-toggles/SiteInfoFeatureToggles.tsx
+++ src/components/_editor-only/site-info/feature-toggles/SiteInfoFeatureToggles.tsx
@@ -8,6 +8,8 @@ import { SiteInfoSubHeader } from '../_common/sub-header/SiteInfoSubHeader';
 
 export enum EditorFeature {
     HideLeftPanel = 'hide-left-panel',
+    EditorReloadBlocker = 'editor-reload-blocker',
+    ContentModifiedWarning = 'content-modified-warning',
 }
 
 export type EditorFeatureProps = {
@@ -21,6 +23,18 @@ export const editorFeatures: Record<EditorFeature, EditorFeatureProps> = {
         key: EditorFeature.HideLeftPanel,
         description:
             'Skjuler venstre-panelet i editoren som standard på komponent-baserte sider',
+        defaultValue: false,
+    },
+    [EditorFeature.EditorReloadBlocker]: {
+        key: EditorFeature.EditorReloadBlocker,
+        description:
+            'Hindrer editoren fra å reloades når andre gjør endringer på innholdet du jobber med',
+        defaultValue: true,
+    },
+    [EditorFeature.ContentModifiedWarning]: {
+        key: EditorFeature.ContentModifiedWarning,
+        description:
+            'Viser advarsel når noen andre gjør endringer på innholdet du jobber med. Obs: Denne krever at funksjonaliteten over også er slått på!',
         defaultValue: false,
     },
 };
