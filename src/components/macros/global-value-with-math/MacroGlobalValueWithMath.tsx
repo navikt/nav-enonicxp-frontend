31a72009962a8f3207dc871ac509720ea6fa7fdc
Bjørn Harbo Dahle <bdahle@hey.com>
2023-10-09 10:07:56 +0200

    bytt ut mathjs med jsep

--- src/components/macros/global-value-with-math/MacroGlobalValueWithMath.tsx
+++ src/components/macros/global-value-with-math/MacroGlobalValueWithMath.tsx
@@ -1,54 +1,82 @@
 import React from 'react';
 import { MacroGlobalValueWithMathProps } from '../../../types/macro-props/global-value-with-math';
-import {
-    create,
-    evaluateDependencies,
-    addDependencies,
-    subtractDependencies,
-    multiplyDependencies,
-    divideDependencies,
-} from 'mathjs/lib/esm/number';
+import jsep from 'jsep';
 import globalState from '../../../globalState';
 import { usePageConfig } from 'store/hooks/usePageConfig';
 import { Language } from 'translations';
-
 import { formatNumber } from '../../../utils/math';
-
-const math = create({
-    evaluateDependencies,
-    addDependencies,
-    subtractDependencies,
-    multiplyDependencies,
-    divideDependencies,
-});
 
 type ExpressionProps =
     MacroGlobalValueWithMathProps['config']['global_value_with_math'];
 
-const evaluateExpression = (
+export const evaluateExpression = (
     { expression, decimals, variables }: ExpressionProps,
     language: Language
 ) => {
     try {
-        // Map variable values to placeholder names used in the expression ($1, $2, etc...)
-        const scope = variables.reduce(
-            (acc, variable, index) => ({ ...acc, [`$${index + 1}`]: variable }),
-            {}
+        function substituteExpression(expression, variables) {
+            return expression.replace(
+                /\$(\d+)/g,
+                (_, idx) => variables[parseInt(idx) - 1]
+            );
+        } //Eks: "$1 + $2 * 5", [50, 100] -> "50 + 100 * 5"
+        const expressionSubstituted = substituteExpression(
+            expression,
+            variables
         );
 
         // Mathjs only accepts . as decimal separator
-        const expressionWithDotSeparators = expression.replace(',', '.');
+        const expressionWithDotSeparators = expressionSubstituted.replace(
+            ',',
+            '.'
+        );
 
-        const result = math.evaluate(expressionWithDotSeparators, scope);
+        const parsedExpression = jsep(expressionWithDotSeparators);
+        const result = evaluateExpressionJSEP(parsedExpression);
         return formatNumber(result, decimals, language);
     } catch (e) {
         if (globalState.isEditorView) {
             return `[feil ved evaluering av uttrykk: ${e}]`;
         }
         console.error(`Global values calculation error: ${e}`);
-        return '[teknisk feil: verdi ikke tilgjengelig]';
     }
 };
+
+// This approach uses jsep to parse the input into an AST (Abstract Syntax Tree) and then recursively evaluates the tree for basic arithmetic operations.
+// It's lightweight and doesn't rely on eval(), but it's also limited to the basics.
+function evaluateExpressionJSEP(node) {
+    switch (node.type) {
+        case 'BinaryExpression':
+            switch (node.operator) {
+                case '+':
+                    return (
+                        evaluateExpressionJSEP(node.left) +
+                        evaluateExpressionJSEP(node.right)
+                    );
+                case '-':
+                    return (
+                        evaluateExpressionJSEP(node.left) -
+                        evaluateExpressionJSEP(node.right)
+                    );
+                case '*':
+                    return (
+                        evaluateExpressionJSEP(node.left) *
+                        evaluateExpressionJSEP(node.right)
+                    );
+                case '/':
+                    return (
+                        evaluateExpressionJSEP(node.left) /
+                        evaluateExpressionJSEP(node.right)
+                    );
+                default:
+                    throw new Error(`Unsupported operator: ${node.operator}`);
+            }
+        case 'Literal':
+            return node.value;
+        default:
+            throw new Error(`Unsupported type: ${node.type}`);
+    }
+}
 
 export const MacroGlobalValueWithMath = ({
     config,
