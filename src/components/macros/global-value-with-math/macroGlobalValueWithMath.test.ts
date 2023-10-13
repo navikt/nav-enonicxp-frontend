eebaf4441f811874ef8c4a01dc6c162f698bc268
Bjørn Harbo Dahle <bdahle@hey.com>
2023-10-13 14:32:48 +0200

    tester kompleks matte

--- src/components/macros/global-value-with-math/macroGlobalValueWithMath.test.ts
+++ src/components/macros/global-value-with-math/macroGlobalValueWithMath.test.ts
@@ -22,6 +22,34 @@ test('dagpenger', () => {
     expect(result).toBe('711\xa0720');
 });
 
+test('kompleks matte', () => {
+    const expression = '$1 * 6 / 2.545 + 85.744 - 100';
+    const decimals = 3;
+    const variables = [118620];
+    const language: Language = 'no';
+
+    const result = evaluateExpression(
+        { expression, decimals, variables },
+        language
+    );
+
+    expect(result).toBe('279\xa0639,968');
+});
+
+test('kompleks matte med paranteser', () => {
+    const expression = '$1 * 6 / (2.545 + 85.744)';
+    const decimals = 3;
+    const variables = [118620];
+    const language: Language = 'no';
+
+    const result = evaluateExpression(
+        { expression, decimals, variables },
+        language
+    );
+
+    expect(result).toBe('8\xa0061,253');
+});
+
 test('should evaluate a simple expression', () => {
     const expression = '2 + 2';
     const decimals = 0;
