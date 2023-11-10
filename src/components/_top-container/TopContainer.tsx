e28e2285be5e0bd3a9e6511b82b09dce1e94f02c
Terje Karlsen <terjeofnorway@gmail.com>
2023-11-10 14:50:03 +0100

    Flytter over useende-relatert logikk fra TopContainer

--- src/components/_top-container/TopContainer.tsx
+++ src/components/_top-container/TopContainer.tsx
@@ -1,63 +1,16 @@
 import React from 'react';
 import { classNames } from 'utils/classnames';
-import { ContentProps, ContentType } from 'types/content-props/_content-common';
+import { ContentProps } from 'types/content-props/_content-common';
 import { getContentLanguages } from 'utils/languages';
 import { PageWarning } from './page-warning/PageWarning';
 import { translator } from 'translations';
-import { getAudience } from 'types/component-props/_mixins';
 import { EditorWidgets } from 'components/_editor-only/EditorWidgets';
 
 import style from './TopContainer.module.scss';
-
-const contentTypesWithWhiteHeader: ReadonlySet<ContentType> = new Set([
-    ContentType.ProductPage,
-    ContentType.SituationPage,
-    ContentType.CurrentTopicPage,
-    ContentType.GuidePage,
-    ContentType.GenericPage,
-    ContentType.ThemedArticlePage,
-    ContentType.Overview,
-    ContentType.OfficeEditorialPage,
-    ContentType.OfficeBranchPage,
-    ContentType.FrontPage,
-    ContentType.FrontPageNested,
-    ContentType.AreaPage,
-    ContentType.PressLandingPage,
-    ContentType.FormIntermediateStepPage,
-    ContentType.FormsOverview,
-]);
+import { checkForNoGap, checkForWhiteHeader } from 'utils/appearance';
 
 type Props = {
     content: ContentProps;
-};
-
-const checkForNoGap = (content: ContentProps) => {
-    if (
-        content.type === ContentType.FrontPage &&
-        (getAudience(content.data.audience) === 'employer' ||
-            getAudience(content.data.audience) === 'provider')
-    ) {
-        return true;
-    }
-    if (
-        content.type === ContentType.FrontPageNested &&
-        getAudience(content.data.audience) === 'provider'
-    ) {
-        return true;
-    }
-
-    return false;
-};
-
-export const checkForWhiteHeader = (content: ContentProps) => {
-    const { type } = content;
-
-    if (type === ContentType.MainArticle) {
-        const contentType = content.data?.contentType;
-        return contentType === 'news' || contentType === 'pressRelease';
-    }
-
-    return contentTypesWithWhiteHeader.has(type);
 };
 
 // For legacy reasons, this component is presently used for adjusting padding/margins at the top of
--- src/utils/appearance.ts
+++ src/utils/appearance.ts
@@ -1,3 +1,4 @@
+import { getAudience } from 'types/component-props/_mixins';
 import { ContentProps, ContentType } from 'types/content-props/_content-common';
 
 const contentTypeWithWhiteBackground = [
@@ -6,6 +7,53 @@ const contentTypeWithWhiteBackground = [
     ContentType.FormIntermediateStepPage,
     ContentType.FormsOverview,
 ];
+
+const contentTypesWithWhiteHeader: ReadonlySet<ContentType> = new Set([
+    ContentType.ProductPage,
+    ContentType.SituationPage,
+    ContentType.CurrentTopicPage,
+    ContentType.GuidePage,
+    ContentType.GenericPage,
+    ContentType.ThemedArticlePage,
+    ContentType.Overview,
+    ContentType.OfficeEditorialPage,
+    ContentType.OfficeBranchPage,
+    ContentType.FrontPage,
+    ContentType.FrontPageNested,
+    ContentType.AreaPage,
+    ContentType.PressLandingPage,
+    ContentType.FormIntermediateStepPage,
+    ContentType.FormsOverview,
+]);
+
+export const checkForNoGap = (content: ContentProps) => {
+    if (
+        content.type === ContentType.FrontPage &&
+        (getAudience(content.data.audience) === 'employer' ||
+            getAudience(content.data.audience) === 'provider')
+    ) {
+        return true;
+    }
+    if (
+        content.type === ContentType.FrontPageNested &&
+        getAudience(content.data.audience) === 'provider'
+    ) {
+        return true;
+    }
+
+    return false;
+};
+
+export const checkForWhiteHeader = (content: ContentProps) => {
+    const { type } = content;
+
+    if (type === ContentType.MainArticle) {
+        const contentType = content.data?.contentType;
+        return contentType === 'news' || contentType === 'pressRelease';
+    }
+
+    return contentTypesWithWhiteHeader.has(type);
+};
 
 export const hasWhiteBackground = (content: ContentProps) => {
     if (content.type === ContentType.MainArticle) {
