d8d7b09cd099f0dcf1c6fc25c58e1cfc0ce0f7df
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-11-10 17:36:30 +0100

    Legger til manglende array key

--- src/components/_common/card/overview-microcard/OverviewMicroCards.tsx
+++ src/components/_common/card/overview-microcard/OverviewMicroCards.tsx
@@ -58,6 +58,7 @@ const CardsList = ({ cardPropsList }: { cardPropsList: CardProps[] }) =>
         <MicroCard
             type={cardTypeMap[cardProps.type]}
             link={{ url: cardProps.url, text: cardProps.title }}
+            key={cardProps.url}
         />
     ));
 
