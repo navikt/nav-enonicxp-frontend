b3e7822c88b43d0a05d5edbb572863f7aa08c11b
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-12-06 14:47:55 +0100

    Fjerner import

--- src/components/_common/user-tests/public-view/UserTestsPublicView.test.tsx
+++ src/components/_common/user-tests/public-view/UserTestsPublicView.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import { UserTestsPublicView } from 'components/_common/user-tests/public-view/UserTestsPublicView';
 import { UserTestsComponentProps } from 'components/_common/user-tests/UserTests';
 import React from 'react';
