323a1051addecabfd9a0bf0e2dbd60d5c4f944f3
Terje Karlsen <terjeofnorway@gmail.com>
2024-01-30 08:53:06 +0100

    Legger til ReadMore-part

---
+++ src/components/parts/read-more/ReadMorePart.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ReadMorePartProps } from 'types/component-props/parts/read-more';
+import { EditorHelp } from 'components/_editor-only/editor-help/EditorHelp';
+import { ReadMore } from '@navikt/ds-react';
+import { ParsedHtml } from 'components/_common/parsed-html/ParsedHtml';
+
+export const ReadMorePart = ({ config }: ReadMorePartProps) => {
+    if (!config?.html || !config?.title) {
+        return (
+            <EditorHelp
+                text={'Legg inn tittel og beskrivelse for "les mer".'}
+                type={'error'}
+            />
+        );
+    }
+
+    const { title, html } = config;
+
+    return (
+        <div>
+            <ReadMore header={title}>
+                <ParsedHtml htmlProps={html} />
+            </ReadMore>
+        </div>
+    );
+};
