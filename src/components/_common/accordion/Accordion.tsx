b697c7b8e5798326ea83902334722deb3ae0365c
Terje Karlsen <terjeofnorway@gmail.com>
2024-02-06 14:55:33 +0100

    Legger til analytics-sporing av bruk av Trekkspill

--- src/components/_common/accordion/Accordion.tsx
+++ src/components/_common/accordion/Accordion.tsx
@@ -2,14 +2,29 @@ import { AccordionPartProps } from 'types/component-props/parts/accordion';
 import styles from './Accordion.module.scss';
 import { Accordion as DSAccordion } from '@navikt/ds-react';
 import { ParsedHtml } from '../parsed-html/ParsedHtml';
+import { AnalyticsEvents, logAmplitudeEvent } from 'utils/amplitude';
 
 type AccordionProps = AccordionPartProps['config'];
 
 export const Accordion = ({ accordion }: AccordionProps) => {
+    const openChangeHandler = (isOpen: boolean, title: string) => {
+        logAmplitudeEvent(
+            isOpen ? AnalyticsEvents.ACC_COLLAPSE : AnalyticsEvents.ACC_EXPAND,
+            {
+                tittel: title,
+                opprinnelse: 'trekkspill',
+            }
+        );
+    };
+
     return (
         <DSAccordion className={styles.accordion}>
             {accordion.map((item, index) => (
-                <DSAccordion.Item key={index} className={styles.item}>
+                <DSAccordion.Item
+                    key={index}
+                    className={styles.item}
+                    onOpenChange={(open) => openChangeHandler(open, item.title)}
+                >
                     <DSAccordion.Header className={styles.header}>
                         <div className={styles.headerTitle}>{item.title}</div>
                     </DSAccordion.Header>
