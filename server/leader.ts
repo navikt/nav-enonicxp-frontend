db2ac65561a9eeec6399ab2b7e40dc13c1f31f4f
Anders Bråthen Nomerstad <anders@nomerstad.com>
2024-02-13 19:19:01 +0100

    Cacher isLeader

--- server/leader.ts
+++ server/leader.ts
@@ -1,29 +1,52 @@
 import * as os from 'os';
 
-export const isLeaderPod = async (): Promise<boolean> => {
-    const electorPath = process.env.ELECTOR_PATH;
-    if (!electorPath) {
-        console.log('ELECTOR_PATH is not defined');
-        return true;
+const CACHE_TTL_MS = 60 * 1000;
+
+const cache = {
+    isLeader: true,
+    expires: 0,
+};
+
+const isLeaderPodReal = async (): Promise<boolean> => {
+    if (Date.now() < cache.expires) {
+        return cache.isLeader;
     }
 
-    return fetch(`http://${electorPath}`)
+    const electorPath = process.env.ELECTOR_PATH;
+    if (!electorPath) {
+        console.error('ELECTOR_PATH is not defined!');
+        return cache.isLeader;
+    }
+
+    const isLeader = await fetch(`http://${electorPath}`)
         .then((res) => {
             if (res.ok) {
                 return res.json();
             }
             console.error(
-                `Error fetching leader, assuming I'm leader - ${res.status} ${res.statusText}`
+                `Error fetching leader - ${res.status} ${res.statusText}`
             );
-            return true;
+            return null;
         })
         .then((json) => {
             return json.name === os.hostname();
         })
         .catch((e) => {
-            console.error(
-                `Error determining leader, assuming I'm leader - ${e}`
-            );
-            return true;
+            console.error(`Error determining leader - ${e}`);
+            return null;
         });
+
+    if (isLeader === null) {
+        return cache.isLeader;
+    }
+
+    cache.isLeader = isLeader;
+    cache.expires = Date.now() + CACHE_TTL_MS;
+
+    return isLeader;
 };
+
+const isLeaderPodDummy = () => true;
+
+export const isLeaderPod =
+    process.env.ENV === 'localhost' ? isLeaderPodDummy : isLeaderPodReal;
