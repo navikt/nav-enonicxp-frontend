2ee0f4a62472bb866e1e7f64711363a33ed9330f
Anders Bråthen Nomerstad <anders@nomerstad.com>
2024-02-14 13:12:32 +0100

    Prettify logging ved lokal kjøring

--- server/logger.ts
+++ server/logger.ts
@@ -1,3 +1,25 @@
-import pino from 'pino';
+import pino, { LoggerOptions } from 'pino';
 
-export const logger = pino();
+const commonOptions: LoggerOptions = {
+    base: { context: 'custom-server' },
+};
+
+const options: LoggerOptions =
+    process.env.ENV === 'localhost'
+        ? {
+              ...commonOptions,
+              level: 'debug',
+              base: {},
+              transport: {
+                  target: 'pino-pretty',
+                  options: {
+                      colorize: true,
+                  },
+              },
+          }
+        : {
+              ...commonOptions,
+              base: { ...commonOptions.base, pid: process.pid },
+          };
+
+export const logger = pino(options);
