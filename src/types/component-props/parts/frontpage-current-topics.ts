23dd5debb4452ca60b9ed450baf2afd09eaf15e6
Terje Karlsen <terjeofnorway@gmail.com>
2024-03-05 15:44:55 +0100

    Legger til lenke ut fra aktuelt-seksjon på forside

--- src/types/component-props/parts/frontpage-current-topics.ts
+++ src/types/component-props/parts/frontpage-current-topics.ts
@@ -1,11 +1,14 @@
 import { PartType } from '../parts';
 import { PartComponentProps } from '../_component-common';
 import { ContentListData } from 'types/content-props/content-list-props';
+import { ContentProps } from 'types/content-props/_content-common';
+import { LinkSelectable } from '../_mixins';
 
 export interface FrontpageCurrentTopicsProps extends PartComponentProps {
     descriptor: PartType.FrontpageCurrentTopics;
     config: {
         title: string;
         contentList?: { data: ContentListData };
+        link: LinkSelectable;
     };
 }
