e85ddde789826c1b6098896744373c037188f2ba
Terje Karlsen <terjeofnorway@gmail.com>
2024-03-07 14:32:31 +0100

    Fjerner ubrukt importering etter mergekonflikt

--- src/components/pages/office-branch-page/OfficeBranchPage.tsx
+++ src/components/pages/office-branch-page/OfficeBranchPage.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import { OfficeBranchPageProps } from 'types/content-props/dynamic-page-props';
-import { OfficePageHeader } from '../../_common/headers/office-page-header/OfficePageHeader';
-import { OfficeDetails } from 'components/_common/office-details/OfficeDetails';
-import { classNames } from 'utils/classnames';
-import { logger } from 'srcCommon/logger';
 
 import { LocalOfficePage } from './localOffice/LocalOfficePage';
 import { GeneralOfficePage } from './localOffice/GeneralOfficePage';
