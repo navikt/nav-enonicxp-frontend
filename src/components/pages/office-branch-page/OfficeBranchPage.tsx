e3c7d6705bc6a23f1f4e23c7a5f55d00f5095070
Terje Karlsen <terjeofnorway@gmail.com>
2024-03-08 12:24:24 +0100

    Legger til eten kontorside for hjelpemiddelsentral

--- src/components/pages/office-branch-page/OfficeBranchPage.tsx
+++ src/components/pages/office-branch-page/OfficeBranchPage.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { OfficeBranchPageProps } from 'types/content-props/dynamic-page-props';
 
 import { LocalOfficePage } from './localOffice/LocalOfficePage';
-import { GeneralOfficePage } from './localOffice/GeneralOfficePage';
+import { GeneralOfficePage } from './generalOffice/GeneralOfficePage';
+import { HMSOfficePage } from './hmsOffice/HMSOfficePage';
 
 export const OfficeBranchPage = (props: OfficeBranchPageProps) => {
     const { type } = props.data;
 
     if (type === 'LOKAL') {
         return <LocalOfficePage {...props} />;
+    }
+    if (type === 'HMS') {
+        return <HMSOfficePage {...props} />;
     }
 
     return <GeneralOfficePage {...props} />;
