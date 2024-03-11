5ab5effcf739e0c1fc7af7f3c14ef538a887ce4c
Terje Karlsen <terjeofnorway@gmail.com>
2024-03-11 16:12:29 +0100

    LenkeInline arver props direkte fra LenkeBase

--- src/components/_common/lenke/LenkeInline.tsx
+++ src/components/_common/lenke/LenkeInline.tsx
@@ -2,24 +2,11 @@ import React from 'react';
 import { LenkeBase } from './LenkeBase';
 import { classNames } from '../../../utils/classnames';
 import style from './LenkeInline.module.scss';
-import { AnalyticsEvents } from 'utils/amplitude';
 
-type Props = {
-    href: string;
-    className?: string;
-    analyticsEvent?: AnalyticsEvents;
-    analyticsLinkGroup?: string;
-    analyticsComponent?: string;
-    analyticsLabel?: string;
-    children: React.ReactNode;
-} & React.AnchorHTMLAttributes<HTMLAnchorElement>;
+type Props = React.ComponentProps<typeof LenkeBase>;
 
-export const LenkeInline = ({ href, className, children, ...rest }: Props) => (
-    <LenkeBase
-        href={href}
-        className={classNames(style.lenkeInline, className)}
-        {...rest}
-    >
+export const LenkeInline = ({ className, children, ...rest }: Props) => (
+    <LenkeBase className={classNames(style.lenkeInline, className)} {...rest}>
         {children}
     </LenkeBase>
 );
