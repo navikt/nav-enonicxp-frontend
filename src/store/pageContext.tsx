8a4ea4537177216861e3e6201705f4cbe0f9b8d1
Terje Karlsen <terjeofnorway@gmail.com>
2024-03-22 08:54:06 +0100

    Setter initiell pageprops til tomt objekt

--- src/store/pageContext.tsx
+++ src/store/pageContext.tsx
@@ -2,22 +2,24 @@ import React, { createContext, useContext } from 'react';
 import { ContentProps } from 'types/content-props/_content-common';
 import { logger } from 'srcCommon/logger';
 
-const PageContext = createContext<ContentProps>(null);
+const PageContext = createContext<ContentProps | {}>({});
 
 const PageContextProvider: React.FC<any> = ({ children, content }) => {
     return (
-        <PageContext.Provider value={content}>{children}</PageContext.Provider>
+        <PageContext.Provider value={content || {}}>
+            {children}
+        </PageContext.Provider>
     );
 };
 
-const usePageContentProps = () => {
-    const context = useContext(PageContext);
+const usePageContentProps = (): ContentProps => {
+    const context = useContext(PageContext) as ContentProps;
 
     if (!context) {
         logger.error('Could not get page context from useContext');
     }
 
-    return context || {};
+    return context;
 };
 
 export { PageContextProvider, usePageContentProps };
