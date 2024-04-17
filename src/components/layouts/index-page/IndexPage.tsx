ffac9d942afbf09c757536f492dbc65779a42711
Bjørn Harbo Dahle <bdahle@hey.com>
2024-04-17 13:48:03 +0200

    fjern ubrukte imports

--- src/components/layouts/index-page/IndexPage.tsx
+++ src/components/layouts/index-page/IndexPage.tsx
@@ -3,8 +3,6 @@ import { LayoutContainer } from 'components/layouts/LayoutContainer';
 import Region from 'components/layouts/Region';
 import { ContentProps, ContentType } from 'types/content-props/_content-common';
 import { IndexPageProps } from 'types/component-props/pages/index-page';
-import { getAudience } from 'types/component-props/_mixins';
-import { classNames } from 'utils/classnames';
 import { AreaPageHeader } from './area-page/AreaPageHeader';
 import { FrontPageAreaNavigation } from './front-page/FrontPageAreaNavigation';
 
