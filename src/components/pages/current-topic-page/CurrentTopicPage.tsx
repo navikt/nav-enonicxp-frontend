a345d2bf385e9c5c45c7c1cfa2e75774345c6c1d
Terje Karlsen <terjeofnorway@gmail.com>
2024-05-02 11:14:01 +0200

    Navne om NewsHeader til FeaturedHeader

--- src/components/pages/current-topic-page/CurrentTopicPage.tsx
+++ src/components/pages/current-topic-page/CurrentTopicPage.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { ComponentMapper } from 'components/ComponentMapper';
 import { CurrentTopicPageProps } from 'types/content-props/dynamic-page-props';
-import { NewsHeader } from 'components/_common/headers/featured-header/FeaturedHeader';
+import { FeaturedHeader } from 'components/_common/headers/featured-header/FeaturedHeader';
 
 import style from './CurrentTopicPage.module.scss';
 
 export const CurrentTopicPage = (props: CurrentTopicPageProps) => {
     return (
         <article className={style.currentTopicPage}>
-            <NewsHeader contentProps={props} />
+            <FeaturedHeader contentProps={props} />
             <div className={style.contentWrapper}>
                 <div className={style.contentAligner}>
                     <div className={style.content}>
