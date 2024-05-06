b4b7817bed4d0c065febafb6ecc0b0bac96b5d97
bdahle <bdahle@hey.com>
2024-05-06 13:48:43 +0200

    PageNavigationMenuV2 basic WIP

---
+++ src/components/_common/page-navigation-menu-v2/PageNavigationMenuV2.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { LenkeBase } from '../lenke/LenkeBase';
+import { AnchorLink } from 'components/parts/page-navigation-menu/PageNavigationMenuPart';
+
+const getValidLinks = (anchorLinks: AnchorLink[]): AnchorLink[] =>
+    anchorLinks.filter((link) => link.anchorId && link.linkText && !link.isDupe);
+
+type Props = {
+    anchorLinks?: AnchorLink[];
+};
+
+export const PageNavigationMenuV2 = ({ anchorLinks = [] }: Props) => {
+    const links = getValidLinks(anchorLinks);
+
+    console.log('links:', links);
+
+    return (
+        <>
+            {links.map((anchorLink) => (
+                <li key={anchorLink.anchorId}>
+                    <LenkeBase
+                        href={`#${anchorLink.anchorId}`}
+                        // href="#hvem"
+                        // onClick={setLocationHashAndScrollToTarget}
+                        analyticsLinkGroup={'Innhold'}
+                        analyticsComponent={'Meny for intern-navigasjon'}
+                        // className={classNames(
+                        //     style.pageNavLink,
+                        //     currentViewStyle.pageNavLink,
+                        //     scrollDirection && sidebarStyle[scrollDirection],
+                        //     isCurrent && sidebarStyle.current
+                        // )}
+                        // id={linkId}
+                    >
+                        {anchorLink.linkText}
+                    </LenkeBase>
+                </li>
+            ))}
+        </>
+    );
+};
