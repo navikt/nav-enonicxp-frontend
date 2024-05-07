06fbdb60823375ee8bb4da6b9f8ea8b54d8173f2
Anders Nomerstad <anders@nomerstad.com>
2024-05-07 13:57:15 +0200

    Fjerner logging

--- server/src/server-setup/server-setup-dev.ts
+++ server/src/server-setup/server-setup-dev.ts
@@ -1,9 +1,6 @@
-import cookieParser from 'cookie-parser';
 import { Express } from 'express';
 import { NextServer } from 'next/dist/server/next';
 import { logger } from 'srcCommon/logger';
-
-const LOGIN_COOKIE = 'dev-login';
 
 const DEV_NAIS_DOMAIN = 'ansatt.dev.nav.no';
 const APP_ORIGIN = process.env.APP_ORIGIN;
@@ -25,13 +22,9 @@ export const serverSetupDev = (expressApp: Express, nextApp: NextServer) => {
         return nextRequestHandler(req, res);
     });
 
-    logger.info(`App origin: ${APP_ORIGIN}`);
-
     if (APP_ORIGIN.endsWith(DEV_NAIS_DOMAIN)) {
-        logger.info('Enabling redirects');
         // Redirects requests to previously used domain names (intern|ekstern).dev.nav.no
         expressApp.all('*', (req, res, next) => {
-            logger.info(`Hostname is ${req.hostname}`);
             if (!req.hostname.endsWith(DEV_NAIS_DOMAIN)) {
                 logger.info('Redirecting!');
                 return res.redirect(302, `${APP_ORIGIN}${req.path}`);
