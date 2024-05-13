8aa740a07fdcc355eadd6ab89eaa99a7afa212e5
bdahle <bdahle@hey.com>
2024-05-13 16:17:56 +0200

    Revert "fjern illustrasjon fra minicard"
    This reverts commit 1b81a7bb2ffa9f6d45ba6cd9e7793da2289d9567.

--- src/components/_common/card/MiniCard.tsx
+++ src/components/_common/card/MiniCard.tsx
@@ -1,29 +1,45 @@
 import React from 'react';
 import { BodyShort } from '@navikt/ds-react';
 import { classNames } from 'utils/classnames';
+import { AnimatedIconsProps } from 'types/content-props/animated-icons';
 import { CardSize, CardType } from 'types/card';
+import { Illustration } from 'components/_common/illustration/Illustration';
 import { LenkeBase } from 'components/_common/lenke/LenkeBase';
 import { LinkProps } from 'types/link-props';
+import { usePageContentProps } from 'store/pageContext';
 import { useCard } from './useCard';
 
 import sharedStyle from './Card.module.scss';
 import style from './MiniCard.module.scss';
 
 export type MiniKortProps = {
+    illustration?: AnimatedIconsProps;
     link: LinkProps;
     type: CardType;
     header?: string;
     className?: string;
+    preferStaticIllustration?: boolean;
+    withFallbackIllustration?: boolean;
 };
 
 export const MiniCard = (props: MiniKortProps) => {
-    const { link, type, header, className } = props;
+    const {
+        link,
+        illustration,
+        type,
+        header,
+        className,
+        preferStaticIllustration,
+        withFallbackIllustration,
+    } = props;
     const { text } = link;
-    const { userEventProps, analyticsProps } = useCard({
+    const { isHovering, userEventProps, analyticsProps } = useCard({
         type,
         size: CardSize.Mini,
         link,
     });
+
+    const { editorView } = usePageContentProps();
 
     return (
         <>
@@ -34,6 +50,13 @@ export const MiniCard = (props: MiniKortProps) => {
             )}
             <div {...userEventProps} className={classNames(sharedStyle.card, className)}>
                 <div className={classNames(sharedStyle.bed, style.mini, type)}>
+                    <Illustration
+                        className={style.illustration}
+                        illustration={illustration}
+                        isHovering={isHovering}
+                        preferStaticIllustration={preferStaticIllustration || editorView === 'edit'}
+                        withFallbackIllustration={withFallbackIllustration}
+                    />
                     <LenkeBase
                         className={classNames(sharedStyle.lenkeBaseOverride, style.title)}
                         href={link.url}
