b07513760c9853aeb18a57f4529938c591efa2cb
Terje Karlsen <terjeofnorway@gmail.com>
2024-05-13 16:29:40 +0200

    Første forslag til implementering av grid i nye sidemaler

--- src/components/layouts/page-with-side-menus/PageWithSideMenus.tsx
+++ src/components/layouts/page-with-side-menus/PageWithSideMenus.tsx
@@ -32,13 +32,17 @@ export const PageWithSideMenus = ({ pageProps, layoutProps }: Props) => {
             pageProps={pageProps}
             layoutProps={layoutProps}
         >
-            {isProductPage && <GeneralPageHeader pageProps={pageProps} />}
-            {!isProductPage && <Region pageProps={pageProps} regionProps={topPageContent} />}
-            {showInternalNav && (
-                <PageNavigationMenu anchorLinks={anchorLinks} title={leftMenuHeader} />
-            )}
-            <Region pageProps={pageProps} regionProps={pageContent} />
-            <Region pageProps={pageProps} regionProps={bottomRow} />
+            <div className={styles.mainContent}>
+                {isProductPage && <GeneralPageHeader pageProps={pageProps} />}
+                {!isProductPage && <Region pageProps={pageProps} regionProps={topPageContent} />}
+                {showInternalNav && (
+                    <PageNavigationMenu anchorLinks={anchorLinks} title={leftMenuHeader} />
+                )}
+                <Region pageProps={pageProps} regionProps={pageContent} />
+            </div>
+            <div className={styles.bottomContent}>
+                <Region pageProps={pageProps} regionProps={bottomRow} />
+            </div>
         </LayoutContainer>
     );
 };
