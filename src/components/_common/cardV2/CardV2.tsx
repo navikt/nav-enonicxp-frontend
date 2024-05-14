2712c727886283c8d94232f55260853eb3754481
bdahle <bdahle@hey.com>
2024-05-14 14:45:59 +0200

    grunnleggende kort med styling

--- src/components/_common/cardV2/CardV2.tsx
+++ src/components/_common/cardV2/CardV2.tsx
@@ -1,3 +1,22 @@
 import React from 'react';
+import { ArrowRightIcon } from '@navikt/aksel-icons';
+import { BodyShort } from '@navikt/ds-react';
+import { LenkeBase } from '../lenke/LenkeBase';
 
-export const CardV2 = () => <>MicroCard</>;
+import style from './CardV2.module.scss';
+
+export const CardV2 = () => (
+    <LenkeBase
+        href="https://www.nav.no/sykepenger"
+        // href={link.url}
+        // {...analyticsProps}
+    >
+        <div className={style.container}>
+            <div className={style.text}>
+                <BodyShort size="medium">HELSE OG SYKDOM (default, 18px)</BodyShort>
+                Har blitt sykmeldt
+            </div>
+            <ArrowRightIcon fontSize="1.5rem" />
+        </div>
+    </LenkeBase>
+);
