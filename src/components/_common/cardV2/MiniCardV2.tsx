6f6d6c053cbcf93013af11bca73bc792bd9669f9
Bjørn Harbo Dahle <bdahle@hey.com>
2024-05-16 08:52:03 +0200

    fiks relative imports

--- src/components/_common/cardV2/MiniCardV2.tsx
+++ src/components/_common/cardV2/MiniCardV2.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ArrowRightIcon } from '@navikt/aksel-icons';
 import { BodyShort } from '@navikt/ds-react';
-import { LenkeBase } from '../lenke/LenkeBase';
+import { LenkeBase } from 'components/_common/lenke/LenkeBase';
 import { LinkProps } from 'types/link-props';
-import { useCard } from '../card/useCard';
+import { useCard } from 'components/_common/card/useCard';
 import { CardSize, CardType } from 'types/card';
 
 import style from './MiniCardV2.module.scss';
