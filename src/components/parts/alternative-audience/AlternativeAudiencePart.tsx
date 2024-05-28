348323d5a0e5206a0e3d5669a0999482a862c77b
Terje Karlsen <terjeofnorway@gmail.com>
2024-05-28 09:52:44 +0200

    Fjerner tomt objekt i argument for gammel audiencePart

--- src/components/parts/alternative-audience/AlternativeAudiencePart.tsx
+++ src/components/parts/alternative-audience/AlternativeAudiencePart.tsx
@@ -3,7 +3,6 @@ import { EditorHelp } from 'components/_editor-only/editor-help/EditorHelp';
 import { AlternativeAudience } from 'components/_common/alternativeAudience/AlternativeAudience';
 import { ContentType } from 'types/content-props/_content-common';
 import { createTypeGuard } from 'types/_type-guards';
-import { PartComponentProps, PartType } from 'types/component-props/parts';
 import { usePageContentProps } from 'store/pageContext';
 
 const isValidContentType = createTypeGuard([
@@ -16,8 +15,8 @@ export type PartConfigAlternativeAudience = {
     showProductName: boolean;
 };
 
-export const AlternativeAudiencePart = ({}: PartComponentProps<PartType.AlternativeAudience>) => {
-    const { data, type, displayName } = usePageContentProps();
+export const AlternativeAudiencePart = () => {
+    const { data, type } = usePageContentProps();
 
     if (!isValidContentType(type)) {
         return <EditorHelp text={`Ugyldig content-type ${type}`} />;
