0d490344e2278f8021298a642c470255709a5201
bdahle <bdahle@hey.com>
2024-06-05 15:33:24 +0200

    styling tagline

--- src/components/_common/card/LargeCardV2.tsx
+++ src/components/_common/card/LargeCardV2.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { BodyLong, BodyShort } from '@navikt/ds-react';
-import { classNames } from 'utils/classnames';
 import { AnimatedIconsProps } from 'types/content-props/animated-icons';
 import { CardSize, CardType } from 'types/card';
-import { Illustration } from 'components/_common/illustration/Illustration';
 import { LenkeBase } from 'components/_common/lenke/LenkeBase';
 import { LinkProps } from 'types/link-props';
-import { usePageContentProps } from 'store/pageContext';
 import { useCard } from './useCard';
 
 import style from './LargeCardV2.module.scss';
-import sharedStyle from './Card.module.scss';
 import { IllustrationStatic } from '../illustration/static/IllustrationStatic';
 
 enum LayoutVariation {
@@ -50,19 +46,21 @@ export const LargeCardV2 = (props: Props) => {
         type === CardType.Situation ? LayoutVariation.SITUATION : LayoutVariation.DEFAULT;
 
     return (
-        <LenkeBase className={classNames(style.container)} href={link.url} {...analyticsProps}>
+        <div className={style.container}>
             {hasIllustration && (
                 <IllustrationStatic illustration={illustration} className={style.illustration} />
             )}
             <div className={style.textContainer}>
-                <BodyShort className={style.linkText} size="medium">
-                    {link.text}
-                </BodyShort>
-                <BodyLong className={style.description}>{description}</BodyLong>
+                <LenkeBase href={link.url} {...analyticsProps}>
+                    <BodyShort className={style.linkText} size="medium">
+                        {link.text}
+                    </BodyShort>
+                </LenkeBase>
+                <BodyLong>{description}</BodyLong>
                 <BodyShort className={style.tagline} size="medium">
                     {tagline}
                 </BodyShort>
             </div>
-        </LenkeBase>
+        </div>
     );
 };
