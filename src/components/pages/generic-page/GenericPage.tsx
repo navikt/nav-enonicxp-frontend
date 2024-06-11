74c9004038df0cc1d0c94754473beaf519e764c1
Terje Karlsen <terjeofnorway@gmail.com>
2024-06-11 13:44:13 +0200

    Legger til bedre styling på generiske sider

--- src/components/pages/generic-page/GenericPage.tsx
+++ src/components/pages/generic-page/GenericPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { GenericPageProps } from 'types/content-props/dynamic-page-props';
 import { ComponentMapper } from 'components/ComponentMapper';
-import { ThemedPageHeader } from 'components/_common/headers/themed-page-header/ThemedPageHeader';
+import { classNames } from 'utils/classnames';
+
+import style from './GenericPage.module.scss';
 
 export const GenericPage = (props: GenericPageProps) => {
     return (
-        <article className={'genericPage'}>
+        <article className={classNames('genericPage', style.genericPage)}>
             <div className={'content'}>
                 <ComponentMapper componentProps={props.page} pageProps={props} />
             </div>
