b3829780d3239587e1e92e260bb756c86b45f0df
bdahle <bdahle@hey.com>
2024-06-18 15:10:19 +0200

    reduser avstand til toppen hvis språkvelger

--- src/components/pages/situation-page/SituationPage.tsx
+++ src/components/pages/situation-page/SituationPage.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { SituationPageProps } from 'types/content-props/dynamic-page-props';
 import { ComponentMapper } from 'components/ComponentMapper';
+import { usePageContentProps } from 'store/pageContext';
+import { classNames } from 'utils/classnames';
 
 import styles from './SituationPage.module.scss';
 
 export const SituationPage = (props: SituationPageProps) => {
+    const { languages } = usePageContentProps();
+    const hasMultipleLanguages = languages && languages?.length > 0;
+
     return (
         <article className={styles.situationPage}>
-            <div className={styles.content}>
+            <div className={classNames(styles.content, hasMultipleLanguages && styles.pullUp)}>
                 <ComponentMapper componentProps={props.page} pageProps={props} />
             </div>
         </article>
