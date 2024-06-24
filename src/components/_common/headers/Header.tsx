d806c76e7e247b64b2568d43e30457ad0e97bab2
Terje Karlsen <terjeofnorway@gmail.com>
2024-06-24 12:03:04 +0200

    Forenkler lenking til ankerlenke for h2

--- src/components/_common/headers/Header.tsx
+++ src/components/_common/headers/Header.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Heading } from '@navikt/ds-react';
-import { LinkIcon } from '@navikt/aksel-icons';
 import { classNames } from 'utils/classnames';
 import { Level, levelToSize, Size } from 'types/typo-style';
-import { useClient } from 'utils/useClient';
-import { AnalyticsEvents, logAmplitudeEvent } from 'utils/amplitude';
 
 // eslint-disable-next-line css-modules/no-unused-class
 import style from './Header.module.scss';
@@ -19,42 +16,17 @@ type Props = {
 
 export const Header = ({ children, size, level, anchorId, className }: Props) => {
     const anchor = anchorId ? (anchorId.startsWith('#') ? anchorId : `#${anchorId}`) : undefined;
-    const { hasMouse } = useClient();
-
-    const copyLinkToClipboard = (e: React.MouseEvent) => {
-        if (navigator?.clipboard?.writeText) {
-            const baseUrl = (e.target as HTMLAnchorElement)?.baseURI?.split('#')[0];
-            if (baseUrl) {
-                navigator?.clipboard?.writeText(`${baseUrl}${anchor}`);
-            }
-
-            logAmplitudeEvent(AnalyticsEvents.COPY_LINK, {
-                seksjon: children?.toString(),
-            });
-        }
-    };
-
     const fallbackSizeByLevel = levelToSize[level] || 'large';
 
     return (
-        <div
-            className={classNames(style.header, hasMouse && style.anchorButtonEnabled, className)}
-            id={anchorId}
-            tabIndex={-1}
-        >
+        <div className={classNames(style.header, className)} id={anchorId} tabIndex={-1}>
             <Heading size={size || fallbackSizeByLevel} level={level}>
-                {children}
-                {anchor && level === '2' && (
-                    <div className={style.anchorHotspot}>
-                        <a
-                            href={anchor}
-                            onClick={copyLinkToClipboard}
-                            className={style.anchorButton}
-                            aria-hidden
-                        >
-                            <LinkIcon aria-hidden />
-                        </a>
-                    </div>
+                {anchor && level === '2' ? (
+                    <a href={anchor} className={style.anchor}>
+                        {children}
+                    </a>
+                ) : (
+                    children
                 )}
             </Heading>
         </div>
