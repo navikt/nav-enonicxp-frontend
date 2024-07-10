7afc1e11ef8cf902177bf52bd34e1221baa3c8cf
bdahle <bdahle@hey.com>
2024-07-10 14:36:08 +0200

    auto migration ting 8.1.11

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,17 +11,27 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
     addons: [
         getAbsolutePath('@storybook/addon-onboarding'),
         getAbsolutePath('@storybook/addon-links'),
         getAbsolutePath('@storybook/addon-essentials'),
         getAbsolutePath('@chromatic-com/storybook'),
         getAbsolutePath('@storybook/addon-interactions'),
+        getAbsolutePath("@storybook/addon-mdx-gfm")
     ],
+
     framework: {
         name: getAbsolutePath('@storybook/nextjs'),
         options: {},
     },
+
     staticDirs: ['../public'],
+
+    docs: {},
+
+    typescript: {
+        reactDocgen: 'react-docgen-typescript'
+    }
 };
 export default config;
