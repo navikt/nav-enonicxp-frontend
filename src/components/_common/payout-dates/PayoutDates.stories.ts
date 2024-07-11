d5532465cdf0ea6f33db3630065d572b7dc83a22
bdahle <bdahle@hey.com>
2024-07-11 10:28:08 +0200

    PayoutDates story

---
+++ src/components/_common/payout-dates/PayoutDates.stories.ts
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { PayoutDates } from './PayoutDates';
+
+const meta = {
+    title: 'Components/Common/PayoutDates',
+    component: PayoutDates,
+    parameters: {
+        layout: 'centered',
+    },
+} satisfies Meta<typeof PayoutDates>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        payoutDatesData: {
+            year: 2024,
+            dates: {
+                jan: 15,
+                feb: 15,
+                mar: 14,
+                apr: 15,
+                may: 15,
+                jun: 17,
+                jul: 15,
+                aug: 15,
+                sep: 15,
+                oct: 12,
+                nov: 15,
+                dec: 15,
+            },
+        },
+    },
+};
