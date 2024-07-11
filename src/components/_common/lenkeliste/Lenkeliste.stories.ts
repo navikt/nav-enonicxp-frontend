219ef27c1dfc2dc4d5aa83cec14bac54474f1aac
bdahle <bdahle@hey.com>
2024-07-11 10:13:44 +0200

    utvid lenkeliste story

--- src/components/_common/lenkeliste/Lenkeliste.stories.ts
+++ src/components/_common/lenkeliste/Lenkeliste.stories.ts
@@ -7,18 +7,32 @@ const meta = {
     parameters: {
         layout: 'centered',
     },
+    args: {
+        lenker: [
+            { url: 'test', text: 'Lorem' },
+            { url: 'test', text: 'Ipsum' },
+            { url: 'test', text: 'Dolor' },
+        ],
+    },
 } satisfies Meta<typeof Lenkeliste>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+export const Default: Story = {
+    args: {
+        listType: 'default',
+    },
+};
+
 export const Chevron: Story = {
     args: {
-        lenker: [
-            { url: 'test', text: 'test' },
-            { url: 'test', text: 'test' },
-            { url: 'test', text: 'test' },
-        ],
         listType: 'chevron',
+    },
+};
+
+export const Bullet: Story = {
+    args: {
+        listType: 'bulletlist',
     },
 };
