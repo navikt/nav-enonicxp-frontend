faa2e4f5121d6ed68d7b86caf2b13a425a218374
bdahle <bdahle@hey.com>
2024-07-22 14:21:15 +0200

    story CopyLink

---
+++ src/components/_common/copyLink/copyLink.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { CopyLink } from './copyLink';
+
+const meta = {
+    title: 'Components/Common/CopyLink',
+    component: CopyLink,
+    args: {
+        anchor: 'https://www.nav.no',
+        heading: 'Heading',
+    },
+} satisfies Meta<typeof CopyLink>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const HideLabel: Story = {
+    args: {
+        showLabel: false,
+    },
+};
