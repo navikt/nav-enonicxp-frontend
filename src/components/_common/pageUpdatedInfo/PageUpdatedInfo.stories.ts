b198874cfcddc7f5a09e8f5e4a05a21ba344a96c
bdahle <bdahle@hey.com>
2024-07-22 13:53:56 +0200

    story PageUpdatedInfo

---
+++ src/components/_common/pageUpdatedInfo/PageUpdatedInfo.stories.ts
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { PageUpdatedInfo } from './PageUpdatedInfo';
+
+const meta = {
+    title: 'Components/Common/PageUpdatedInfo',
+    component: PageUpdatedInfo,
+    args: {
+        datetime: '2024-01-01',
+    },
+} satisfies Meta<typeof PageUpdatedInfo>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const English: Story = {
+    args: {
+        language: 'en',
+    },
+};
+
+export const Ukrainian: Story = {
+    args: {
+        language: 'uk',
+    },
+};
+
+export const SitutationPage: Story = {
+    args: {
+        isSituationPage: true,
+    },
+};
