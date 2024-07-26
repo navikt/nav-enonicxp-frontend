7d4b379beb19c3b8917a108a877bd2fc86345fd4
bdahle <bdahle@hey.com>
2024-07-26 12:45:50 +0200

    story MiniCardV1

---
+++ src/components/_common/card/MiniCardV1/MiniCardV1.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { CardType } from 'types/card';
+import { MiniCardV1 } from './MiniCardV1';
+
+const meta = {
+    title: 'Components/Common/Card/MiniCardV1',
+    component: MiniCardV1,
+    args: {
+        link: { url: '', text: 'Tittel' },
+        type: CardType.Product,
+    },
+} satisfies Meta<typeof MiniCardV1>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const WithHeader: Story = {
+    args: {
+        header: 'Header',
+    },
+};
