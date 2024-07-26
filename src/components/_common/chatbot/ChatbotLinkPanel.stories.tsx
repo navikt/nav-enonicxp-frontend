602352af04fac29763cc83c307c7cc79847908b5
bdahle <bdahle@hey.com>
2024-07-26 10:41:13 +0200

    chatbot i mappe

--- src/components/_common/chatbot/ChatbotLinkPanel.stories.tsx
+++ src/components/_common/chatbot/ChatbotLinkPanel.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { ChatbotLinkPanel } from './ChatbotLinkPanel';
 
 const meta = {
-    title: 'Components/Common/ChatbotLinkPanel',
+    title: 'Components/Common/Chatbot/ChatbotLinkPanel',
     component: ChatbotLinkPanel,
     args: {
         analyticsGroup: '',
