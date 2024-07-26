7d6bd3a2cfbe30809aa843c2769441b05407a506
bdahle <bdahle@hey.com>
2024-07-26 09:03:41 +0200

    story AlertInContext

---
+++ src/components/_common/alert-in-context/AlertInContext.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { AlertInContext } from './AlertInContext';
+
+const meta = {
+    title: 'Components/Common/AlertInContext',
+    component: AlertInContext,
+    args: {
+        alert: {
+            data: {
+                type: 'information',
+                text: 'Dette er en AlertInContext',
+                target: {
+                    _selected: '1',
+                    formDetails: {
+                        _selected: 'targetContent',
+                        targetContent: 'Dette er en AlertInContext',
+                    },
+                },
+            },
+        },
+    },
+} satisfies Meta<typeof AlertInContext>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Information: Story = {};
+
+export const Critical: Story = {
+    args: {
+        alert: {
+            data: {
+                ...meta.args.alert.data,
+                type: 'critical',
+            },
+        },
+    },
+};
