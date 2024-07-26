9bd64cbfd1d65212a37a9fd486565f901a6614cd
bdahle <bdahle@hey.com>
2024-07-26 10:46:38 +0200

    story FancyChevron

---
+++ src/components/_common/chevron/FancyChervron.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { FancyChevron } from './FancyChevron';
+
+const meta = {
+    title: 'Components/Common/FancyChevron',
+    component: FancyChevron,
+    args: {
+        color: 'white',
+    },
+} satisfies Meta<typeof FancyChevron>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const White: Story = {};
+
+export const Blue: Story = {
+    args: {
+        color: 'blue',
+    },
+};
+
+export const Small: Story = {
+    args: {
+        scale: 0.5,
+    },
+};
