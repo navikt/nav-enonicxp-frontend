ec987bf1170cda1ba7a95ba06e015fa6a8de97c8
bdahle <bdahle@hey.com>
2024-08-05 15:08:28 +0200

    legg til addon-storysource

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,6 +11,7 @@ const config: StorybookConfig = {
         getAbsolutePath('@chromatic-com/storybook'),
         getAbsolutePath('@storybook/addon-interactions'),
         getAbsolutePath('@storybook/addon-mdx-gfm'),
+        '@storybook/addon-storysource',
     ],
 
     framework: {
