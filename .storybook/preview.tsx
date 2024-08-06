57ee6da27271c20f864fd1569df3afe962f88027
bdahle <bdahle@hey.com>
2024-08-06 13:12:16 +0200

    fjern test av context decorator

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,24 +1,5 @@
-// import React from 'react';
 import type { Preview } from '@storybook/react';
-// import { PageContextProvider } from '../src/store/pageContext';
-import { ContentProps, ContentType } from '../src/types/content-props/_content-common';
 import '../src/global.scss';
-
-// TODO: kopiert fra api/component-preview.tsx
-const contentPropsFull: ContentProps = {
-    type: ContentType.DynamicPage,
-    _id: '',
-    _path: '',
-    createdTime: '',
-    modifiedTime: '',
-    displayName: '',
-    language: 'no',
-    isDraft: true,
-    editorView: 'edit',
-    isPagePreview: false,
-    data: {},
-    // ...contentProps,
-};
 
 const preview: Preview = {
     parameters: {
@@ -30,13 +11,6 @@ const preview: Preview = {
         }, //TODO: det over hører til Example, fjerne hvis ikke brukt
         layout: 'centered',
     },
-    // decorators: [
-    //     (Story) => (
-    //         <PageContextProvider content={contentPropsFull}>
-    //             <Story />
-    //         </PageContextProvider>
-    //     ),
-    // ],
     tags: ['autodocs'],
 };
 
