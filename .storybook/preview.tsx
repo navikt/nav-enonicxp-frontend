7ef5c4e4a69f8f7a155b304b039ecf465ab35fa3
bdahle <bdahle@hey.com>
2024-08-06 13:13:36 +0200

    fjern Example stories

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,12 +3,6 @@ import '../src/global.scss';
 
 const preview: Preview = {
     parameters: {
-        controls: {
-            matchers: {
-                color: /(background|color)$/i,
-                date: /Date$/i,
-            },
-        }, //TODO: det over hører til Example, fjerne hvis ikke brukt
         layout: 'centered',
     },
     tags: ['autodocs'],
