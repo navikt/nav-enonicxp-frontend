11ec0fb1015a282e705c10e6ec313076c626b087
bdahle <bdahle@hey.com>
2024-08-27 13:26:07 +0200

    basic story MicroCard

---
+++ src/components/_common/card/MicroCard/MicroCard.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { CardType } from 'types/card';
+import { MicroCard } from './MicroCard';
+
+const meta = {
+    component: MicroCard,
+} satisfies Meta<typeof MicroCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        link: { url: '', text: 'MicroCard' },
+        type: CardType.Product,
+    },
+};
