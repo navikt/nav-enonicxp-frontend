8717b90ae6efc4a6f8c729ccb84e9c5b0e95dd35
bdahle <bdahle@hey.com>
2024-08-28 09:45:11 +0200

    story LargeCardV1

---
+++ src/components/_common/card/LargeCardV1/LargeCardV1.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { CardType } from 'types/card';
+import { ContentType } from 'types/content-props/_content-common';
+import { MediaType } from 'types/media';
+import { LargeCardV1 } from './LargeCardV1';
+
+const meta = {
+    component: LargeCardV1,
+} satisfies Meta<typeof LargeCardV1>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        link: { url: '', text: 'LargeCardV1' },
+        description: 'Description',
+        tagline: 'Tagline',
+        type: CardType.Product,
+    },
+};
+
+export const Illustration: Story = {
+    args: {
+        ...Default.args,
+        illustration: {
+            type: ContentType.Pictograms,
+            data: { icons: [{ icon: { type: MediaType.Vector, mediaUrl: 'placeholder' } }] },
+        },
+    },
+};
