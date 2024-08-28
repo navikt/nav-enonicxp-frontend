f2bb8f13a370bedb56d5d5deae55888915e62892
bdahle <bdahle@hey.com>
2024-08-28 10:20:08 +0200

    args til meta

--- src/components/_common/card/LargeCardV1/LargeCardV1.stories.tsx
+++ src/components/_common/card/LargeCardV1/LargeCardV1.stories.tsx
@@ -6,38 +6,30 @@ import { LargeCardV1 } from './LargeCardV1';
 
 const meta = {
     component: LargeCardV1,
-} satisfies Meta<typeof LargeCardV1>;
-
-export default meta;
-
-type Story = StoryObj<typeof meta>;
-
-export const Default: Story = {
     args: {
         link: { url: '', text: 'LargeCardV1' },
         description: 'Description',
         tagline: 'Tagline',
         type: CardType.Product,
     },
-};
+} satisfies Meta<typeof LargeCardV1>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
 
 export const Illustration: Story = {
     args: {
-        ...Default.args,
         illustration: {
             type: ContentType.Pictograms,
             data: { icons: [{ icon: { type: MediaType.Vector, mediaUrl: 'placeholder' } }] },
         },
     },
-    parameters: {
-        pseudo: { hover: true },
-    },
 };
 
 export const ProductHover: Story = {
-    args: {
-        ...Default.args,
-    },
     parameters: {
         pseudo: { hover: true },
     },
