2a0112e6db05dc647580a51134c2453d2a075300
bdahle <bdahle@hey.com>
2024-09-03 09:04:10 +0200

    siste i press-landing i egne mapper

--- src/components/pages/press-landing-page/PressLandingPage.tsx
+++ src/components/pages/press-landing-page/PressLandingPage.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { PressLandingPageProps } from 'types/content-props/dynamic-page-props';
-import { PressTopSection } from 'components/_common/press-landing/PressTopSection';
+import { PressTopSection } from 'components/_common/press-landing//PressTopSection/PressTopSection';
 import { PressNews } from 'components/_common/press-landing/PressNews/PressNews';
-import { PressShortcuts } from 'components/_common/press-landing/PressShortcuts';
+import { PressShortcuts } from 'components/_common/press-landing/PressShortcuts/PressShortcuts';
 
 export const PressLandingPage = (props: PressLandingPageProps) => {
     return (
