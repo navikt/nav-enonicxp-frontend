93180f0d21d28d9a0fc96360fd57c1f6ac9bb520
bdahle <bdahle@hey.com>
2024-09-03 08:55:12 +0200

    PressNews egen mappe

--- src/components/_common/press-landing/PressNews/PressNews.tsx
+++ src/components/_common/press-landing/PressNews/PressNews.tsx
@@ -2,7 +2,7 @@ import { Heading, Link } from '@navikt/ds-react';
 import { ChevronRightIcon } from '@navikt/aksel-icons';
 import { PressLandingPageProps } from 'types/content-props/dynamic-page-props';
 import { translator } from 'translations';
-import { PressNewsItem } from './PressNewsItem/PressNewsItem';
+import { PressNewsItem } from 'components/_common/press-landing/PressNewsItem/PressNewsItem';
 
 import styles from './PressNews.module.scss';
 
--- src/components/pages/press-landing-page/PressLandingPage.tsx
+++ src/components/pages/press-landing-page/PressLandingPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PressLandingPageProps } from 'types/content-props/dynamic-page-props';
 import { PressTopSection } from 'components/_common/press-landing/PressTopSection';
-import { PressNews } from 'components/_common/press-landing/PressNews';
+import { PressNews } from 'components/_common/press-landing/PressNews/PressNews';
 import { PressShortcuts } from 'components/_common/press-landing/PressShortcuts';
 
 export const PressLandingPage = (props: PressLandingPageProps) => {
