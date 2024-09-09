e7aac9e499902a86f692a03bd742eb9e86430a03
bdahle <bdahle@hey.com>
2024-09-09 16:18:04 +0200

    basic story LenkepanelListe

---
+++ src/components/_common/lenkepanel-liste/LenkepanelListe.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { LenkepanelListe } from './LenkepanelListe';
+
+const meta = {
+    component: LenkepanelListe,
+} satisfies Meta<typeof LenkepanelListe>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        title: 'LenkepanelListe',
+        ingress: 'En liten ingresstekst',
+        items: [
+            { title: 'Lenkepanel 1', url: { text: 'test' } },
+            { title: 'Lenkepanel 2', url: { text: 'test' } },
+            { title: 'Lenkepanel 3', url: { text: 'test' } },
+        ],
+    },
+};
