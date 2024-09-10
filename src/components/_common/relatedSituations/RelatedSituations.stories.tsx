a7aa3572e28f61f0c16d718c7c940e1c5d7c0923
bdahle <bdahle@hey.com>
2024-09-10 08:25:56 +0200

    basic story

---
+++ src/components/_common/relatedSituations/RelatedSituations.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { RelatedSituations } from './RelatedSituations';
+
+const meta = {
+    component: RelatedSituations,
+} satisfies Meta<typeof RelatedSituations>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        relatedSituations: [
+            { _id: '1', _path: '', displayName: 'test', data: { title: 'En situasjon' } },
+            { _id: '2', _path: '', displayName: 'test', data: { title: 'En annen situasjon' } },
+        ],
+        title: 'Related situations',
+        description: 'Beskrivelse av relaterte situasjoner',
+    },
+};
