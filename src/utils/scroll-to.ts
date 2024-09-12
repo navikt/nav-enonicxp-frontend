cd9e08973de78087f2f707fee295c1a0a2d2382a
bdahle <bdahle@hey.com>
2024-09-12 10:01:10 +0200

    hent dynamisk header offset, per nå enten 80, eller 72 på mobil

--- src/utils/scroll-to.ts
+++ src/utils/scroll-to.ts
@@ -22,12 +22,26 @@ export const smoothScrollToTarget = (targetId: string, offset = 0) => {
     }
 };
 
+const getHeaderOffset = (): number => {
+    const fallbackHeaderHeight = 80;
+    const headerElement = document.getElementById('decorator-header') as HTMLElement;
+
+    if (headerElement) {
+        const computedStyleHeader = getComputedStyle(headerElement);
+        const headerHeight = computedStyleHeader.getPropertyValue('--header-height');
+        return parseInt(headerHeight) || fallbackHeaderHeight;
+    } else {
+        return fallbackHeaderHeight;
+    }
+};
+
 export const handleStickyScrollOffset = (isOpening: boolean, current: HTMLDivElement | null) => {
     if (!isOpening && current) {
         const verticalPosition = current.getBoundingClientRect().top;
+
         if (verticalPosition < 0) {
             window.scrollBy({
-                top: verticalPosition - 80,
+                top: verticalPosition - getHeaderOffset(),
                 behavior: 'instant',
             });
         }
