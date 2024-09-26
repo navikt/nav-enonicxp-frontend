e75570adf12949101ccb0c6b0c5538609aa74c50
bdahle <bdahle@hey.com>
2024-09-26 15:33:02 +0200

    basic story ThemedPageHeader

---
+++ src/components/_common/headers/themed-page-header/ThemedPageHeader.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ContentType } from 'types/content-props/_content-common';
+import { MediaType } from 'types/media';
+import { ThemedPageHeader } from './ThemedPageHeader';
+
+const meta = {
+    component: ThemedPageHeader,
+} satisfies Meta<typeof ThemedPageHeader>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        contentProps: {
+            type: ContentType.FormIntermediateStepPage,
+            displayName: 'Søknad om arbeidsavklaringspenger (AAP)',
+            modifiedTime: '2023-07-03T07:02:26.724412Z',
+            data: {
+                title: 'Søknad om arbeidsavklaringspenger (AAP)',
+                illustration: {
+                    type: ContentType.Pictograms,
+                    data: {
+                        icons: [{ icon: { type: MediaType.Vector, mediaUrl: 'placeholder' } }],
+                    },
+                },
+                taxonomy: [],
+            },
+        },
+    },
+};
