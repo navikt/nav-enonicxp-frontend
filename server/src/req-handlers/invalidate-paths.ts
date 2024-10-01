78e1cf9331c35c0ca61bdf1922e1b260c34e061a
Terje Karlsen <terjeofnorway@gmail.com>
2024-10-01 14:13:54 +0200

    Escaper html for innkommen eventid etter alert fra CodeQL

--- server/src/req-handlers/invalidate-paths.ts
+++ server/src/req-handlers/invalidate-paths.ts
@@ -1,13 +1,15 @@
 import { RequestHandler } from 'express';
 import PageCacheHandler from 'cache/page-cache-handler';
 import { logger } from 'srcCommon/logger';
+import escapeHtml from 'escape-html';
+import { escape } from 'querystring';
 
 export const handleInvalidatePathsReq: RequestHandler = (req, res) => {
-    const { eventid } = req.headers;
+    const { eventid = '' } = req.headers;
     const { paths } = req.body;
 
     if (!Array.isArray(paths)) {
-        const msg = `Invalid path array for event ${eventid}`;
+        const msg = `Invalid path array for event ${escapeHtml(eventid.toString())}`;
         logger.error(msg);
         res.status(400).send(msg);
         return;
@@ -17,7 +19,7 @@ export const handleInvalidatePathsReq: RequestHandler = (req, res) => {
 
     paths.forEach((path) => cacheHandler.delete(path));
 
-    const msg = `Received cache invalidation event for ${paths.length} paths - event id ${eventid}`;
+    const msg = `Received cache invalidation event for ${paths.length} paths - event id ${escapeHtml(eventid.toString())}`;
     logger.info(msg);
 
     res.status(200).send(msg);
