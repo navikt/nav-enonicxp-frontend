08de1f19c7cb506739685114cc7e3774124238c6
bdahle <bdahle@hey.com>
2024-10-03 12:11:31 +0200

    Remove centered layout parameter from Storybook preview configuration

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,9 +2,6 @@ import type { Preview } from '@storybook/react';
 import '../src/global.scss';
 
 const preview: Preview = {
-    parameters: {
-        layout: 'centered',
-    },
     tags: ['autodocs'],
 };
 
