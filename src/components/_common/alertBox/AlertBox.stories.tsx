f88269eb4ff55c4062de0e223430d5e38e894c0d
bdahle <bdahle@hey.com>
2024-10-10 11:07:41 +0200

    bruk aksel-typografi på samme måte som på utvidet barnetrygd

--- src/components/_common/alertBox/AlertBox.stories.tsx
+++ src/components/_common/alertBox/AlertBox.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BodyLong, Heading } from '@navikt/ds-react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { AlertBox } from './AlertBox';
 
@@ -24,16 +25,18 @@ export const Long: Story = {
     args: {
         children: (
             <div>
-                <h3>Lovendring om etterbetaling av barnetrygd</h3>
-                <p>
+                <Heading size="medium" spacing>
+                    Lovendring om etterbetaling av barnetrygd
+                </Heading>
+                <BodyLong spacing>
                     Fra 1. juli 2024 er regelverket for etterbetaling av barnetrygd endret. Etter de
                     nye reglene kan barnetrygden bli etterbetalt for inntil tre måneder.
-                </p>
-                <p>
+                </BodyLong>
+                <BodyLong spacing>
                     Søker du før 1. oktober 2024, vil saken din likevel bli behandlet etter gammelt
                     regelverk. Etter gammelt regelverk kan du få etterbetalt barnetrygd i inntil tre
                     år.
-                </p>
+                </BodyLong>
             </div>
         ),
     },
