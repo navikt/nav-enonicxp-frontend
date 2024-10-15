974b5363b217a784ceeb0dc92a3d26826da7aa69
bdahle <bdahle@hey.com>
2024-10-15 11:31:45 +0200

    stories for renderOn loggedin

--- src/components/_common/auth-dependant-render/AuthDependantRender.stories.tsx
+++ src/components/_common/auth-dependant-render/AuthDependantRender.stories.tsx
@@ -1,4 +1,3 @@
-// AuthDependantRender.stories.tsx
 import React from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 import { configureStore } from '@reduxjs/toolkit';
@@ -6,14 +5,13 @@ import { Provider } from 'react-redux';
 import authStateReducer, { AuthState } from 'store/slices/authState';
 import { AuthDependantRender } from './AuthDependantRender';
 
-// Create a mock Redux store
 const createMockStore = (authState: AuthState) =>
     configureStore({
         reducer: {
             authState: authStateReducer,
         },
         preloadedState: {
-            authState, // Preload the store with a specific auth state
+            authState,
         },
     });
 
@@ -27,36 +25,30 @@ const withMockedAuthState = (authState: AuthState) => {
     return Decorator;
 };
 
-const meta = {
-    component: AuthDependantRender,
-} satisfies Meta<typeof AuthDependantRender>;
+const meta = { args: { children: 'Visible' }, component: AuthDependantRender } satisfies Meta<
+    typeof AuthDependantRender
+>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// Example story when the user is logged in
-export const LoggedInStory: Story = {
+export const RenderLoggedInStateLoggedIn: Story = {
     args: {
         renderOn: 'loggedIn',
-        children: 'This content is visible when logged in',
     },
-    decorators: [withMockedAuthState({ authState: 'loggedIn' })], // Mock the authState as 'loggedIn'
+    decorators: [withMockedAuthState({ authState: 'loggedIn' })],
 };
 
-// Example story when the user is logged out
-export const LoggedOutStory: Story = {
-    args: {
-        renderOn: 'loggedOut',
-        children: 'This content is visible when logged out',
-    },
-    decorators: [withMockedAuthState({ authState: 'loggedOut' })], // Mock the authState as 'loggedOut'
-};
-
-// Example story when authState is waiting
-export const WaitingAuthStory: Story = {
+export const RenderLoggedInStateLoggedOut: Story = {
     args: {
         renderOn: 'loggedIn',
-        children: 'Waiting for authentication...',
     },
-    decorators: [withMockedAuthState({ authState: 'waiting' })], // Mock the authState as 'waiting'
+    decorators: [withMockedAuthState({ authState: 'loggedOut' })],
+};
+
+export const RenderLoggedInStateWaiting: Story = {
+    args: {
+        renderOn: 'loggedIn',
+    },
+    decorators: [withMockedAuthState({ authState: 'waiting' })],
 };
