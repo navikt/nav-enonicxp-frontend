58130ec28b6c46120e363a5ba635f215fe78df37
taniaholst <tania.holst@nav.no>
2024-10-18 13:48:33 +0200

    nå da?

--- src/components/_common/form-details/FormDetails.stories.tsx
+++ src/components/_common/form-details/FormDetails.stories.tsx
@@ -1,46 +1,44 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { FormDetails } from 'components/_common/form-details/FormDetails';
+import { FormDetailsData } from 'types/content-props/form-details';
 
-// Problem:
-// formDetails is defined in meta
-// formDetails is not defined in the languageDisclaimer story, but should read from meta
-// if I add formDetails to a const, it is not defined anywhere
-// if I add it to all stories, sonarCloud will complain about duplicated code
+const baseFormDetails = {
+    formType: [
+        {
+            _selected: 'complaint',
+            application: { variations: [{ label: 'Søk om tolk' }] },
+            complaint: {
+                variations: [
+                    {
+                        type: 'appeal',
+                        label: 'Søk om tolk',
+                        link: {
+                            _selected: 'external',
+                            internal: { target: { _path: '', displayName: '' } },
+                            external: { url: '', text: '' },
+                        },
+                    },
+                ],
+            },
+            addendum: {
+                variations: [{ label: 'Tillegg' }],
+            },
+        },
+    ],
+    alerts: [],
+    title: 'Søk om tolk for første gang',
+    ingress: {
+        processedHtml:
+            'Søknaden skal brukes hvis du søker om tolk første gang. Hvis du tidligere har søkt og fått innvilget tolk, kan du bruke bestillingsløsningen neste gang du trenger tolk.',
+        macros: [],
+    },
+} satisfies FormDetailsData; // Ensure it satisfies the correct type
 
+// Meta definition
 const meta = {
     component: FormDetails,
     args: {
-        formDetails: {
-            formType: [
-                {
-                    _selected: 'complaint',
-                    application: { variations: [{ label: 'Søk om tolk' }] },
-                    complaint: {
-                        variations: [
-                            {
-                                type: 'appeal',
-                                label: 'Søk om tolk',
-                                link: {
-                                    _selected: 'external',
-                                    internal: { target: { _path: '', displayName: '' } },
-                                    external: { url: '', text: '' },
-                                },
-                            },
-                        ],
-                    },
-                    addendum: {
-                        variations: [{ label: 'Tillegg' }],
-                    },
-                },
-            ],
-            alerts: [],
-            title: 'Søk om tolk for første gang',
-            ingress: {
-                processedHtml:
-                    'Søknaden skal brukes hvis du søker om tolk første gang. Hvis du tidligere har søkt og fått innvilget tolk, kan du bruke bestillingsløsningen neste gang du trenger tolk.',
-                macros: [],
-            },
-        },
+        formDetails: baseFormDetails,
         displayConfig: { showTitle: true, showIngress: true },
     },
 } satisfies Meta<typeof FormDetails>;
@@ -52,15 +50,15 @@ export const Default: Story = {};
 
 export const LanguageDisclaimer: Story = {
     args: {
-        formDetails: {
+        formDetails: Object.assign({}, baseFormDetails, {
             languageDisclaimer: 'Språkdisclaimer',
-        },
+        }),
     },
 };
 
 export const Alerts: Story = {
     args: {
-        formDetails: {
+        formDetails: Object.assign({}, baseFormDetails, {
             alerts: [
                 {
                     data: {
@@ -73,6 +71,6 @@ export const Alerts: Story = {
                     },
                 },
             ],
-        },
+        }),
     },
 };
