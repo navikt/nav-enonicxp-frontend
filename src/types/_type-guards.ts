6fb234fbccc8c5c592b9a53151e100989507969e
Anders Nomerstad <anders@nomerstad.com>
2024-10-21 16:33:52 +0200

    Fjerner unødvendig trim på canonicalUrl (denne valideres backend)

--- src/types/_type-guards.ts
+++ src/types/_type-guards.ts
@@ -6,8 +6,5 @@ export const isPropsWithContent = (props: any): props is { content: ContentProps
 
 export const createTypeGuard = <Type>(allowedValues: ReadonlyArray<Type>) => {
     const allowedValuesSet: ReadonlySet<Type> = new Set(allowedValues);
-
-    const isAllowedValue = (value: unknown): value is Type => allowedValuesSet.has(value as Type);
-
-    return isAllowedValue;
+    return (value: unknown): value is Type => allowedValuesSet.has(value as Type);
 };
