48bc53c0e23af9b7017276880db67f54e19c38a3
Anders Nomerstad <anders@nomerstad.com>
2024-10-23 12:06:58 +0200

    Page for render basert på props

--- src/pages/render-from-props.tsx
+++ src/pages/render-from-props.tsx
@@ -2,20 +2,25 @@ import { GetServerSideProps } from 'next';
 import { parseBody } from 'next/dist/server/api-utils/node/parse-body';
 import { PageBase } from 'components/PageBase';
 import { validateSecretHeader } from 'srcCommon/auth';
-import { makeErrorProps } from 'utils/make-error-props';
 
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+// Render a page using props from the request body
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     if (!validateSecretHeader(req) || req.method !== 'POST') {
         return {
             notFound: true,
         };
     }
 
-    const body = await parseBody(req, '5MB');
+    const contentProps = (await parseBody(req, '5MB'))?.contentProps;
+
+    if (!contentProps) {
+        // The type for req/res is incomplete...
+        return (res as any).status(400).send('Missing contentProps in request body');
+    }
 
     return {
         props: {
-            content: body?.contentProps ?? makeErrorProps(),
+            content: contentProps,
         },
     };
 };
