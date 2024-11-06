4a38679e02d42c84357b51fadf074ab831aed0b6
bdahle <bdahle@hey.com>
2024-11-06 13:00:38 +0100

    fjern ubrukt spanning-prop

--- src/components/pages/template-page/mocks/linkPanelsDataMock.ts
+++ src/components/pages/template-page/mocks/linkPanelsDataMock.ts
@@ -7,31 +7,26 @@ export const linkPanelsDataMock: SectionPageData = {
             url: {},
             title: 'Lenke 1',
             ingress: 'Ingress 1',
-            spanning: false,
         },
         {
             url: {},
             title: 'Lenke 2',
             ingress: 'Ingress 2',
-            spanning: false,
         },
         {
             url: {},
             title: 'Lenke 3',
             ingress: 'Ingress 3',
-            spanning: false,
         },
         {
             url: {},
             title: 'Lenke 4',
             ingress: 'Ingress 4',
-            spanning: false,
         },
         {
             url: {},
             title: 'Lenke 5',
             ingress: 'Ingress 5',
-            spanning: false,
         },
     ],
 };
