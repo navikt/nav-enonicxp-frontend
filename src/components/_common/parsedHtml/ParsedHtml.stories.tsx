af9b229bb2fcc52d244b9e5b01dc54f34f874841
bdahle <bdahle@hey.com>
2024-11-08 13:33:59 +0100

    header levels i story

--- src/components/_common/parsedHtml/ParsedHtml.stories.tsx
+++ src/components/_common/parsedHtml/ParsedHtml.stories.tsx
@@ -27,11 +27,17 @@ const ul =
 const ol =
     '<ol><li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.&nbsp;</li><li>Aduis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.&nbsp;</li></ol>';
 
-const h4 = '<h4>Lorem ipsum</h4>';
-
 const a = '<a href="/"><u>lenke</u></a>';
 
+const h1 = '<h2>Lorem ipsum</h2>';
+const h2 = '<h2>Lorem ipsum</h2>';
+const h3 = '<h3>Lorem ipsum</h3>';
+const h4 = '<h4>Lorem ipsum</h4>';
+const h5 = '<h5>Lorem ipsum</h5>';
+const h6 = '<h6>Lorem ipsum</h6>';
+
 const RandomizedHtml = p + ul + ol + p + h4 + a + ul + h4 + ol + p + a;
+const HeadersHtml = h1 + h2 + h3 + h4 + h5 + h6;
 
 export const Default: Story = {
     args: {
@@ -47,6 +53,25 @@ export const WithStyling: Story = {
     args: {
         htmlProps: {
             processedHtml: RandomizedHtml,
+            macros: [],
+        },
+    },
+};
+
+export const Headers: Story = {
+    args: {
+        htmlProps: {
+            processedHtml: HeadersHtml,
+            macros: [],
+        },
+    },
+};
+
+export const HeadersWithStyling: Story = {
+    decorators: [withDefaultHtmlStyling],
+    args: {
+        htmlProps: {
+            processedHtml: HeadersHtml,
             macros: [],
         },
     },
