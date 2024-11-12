0916f542618e43a339e08f53780e40ac05419130
bdahle <bdahle@hey.com>
2024-11-12 09:21:31 +0100

    frontpagecard classname

--- src/components/_common/frontpageCard/FrontPageCard.tsx
+++ src/components/_common/frontpageCard/FrontPageCard.tsx
@@ -2,7 +2,9 @@ import { PictogramsProps } from 'types/content-props/pictograms';
 import { MiniCardV1 } from 'components/_common/card/MiniCardV1/MiniCardV1';
 import { LinkProps } from 'types/link-props';
 import { CardType } from 'types/card';
+import { classNames } from 'utils/classnames';
 
+import style from 'components/_common/pageUpdatedInfo/PageUpdatedInfo.module.scss';
 import styles from './FrontPageCard.module.scss';
 
 type Props = {
@@ -30,7 +32,7 @@ export const FrontPageCard = ({
             illustration={illustration}
             link={link}
             type={type}
-            className={styles.frontpageCard}
+            className={classNames(styles.frontpageCard, style.pageUpdatedInfo)}
             tryFallbackIllustration={tryFallbackIllustration}
         />
     );
