4f217557513de2029313e1a9e6f33a4a8a1aa2ed
taniaholst <tania.holst@nav.no>
2024-11-18 12:23:58 +0100

    endret tekst i alert

--- src/components/_editor-only/warnings/kort-id-warning/KortUrlWarning.tsx
+++ src/components/_editor-only/warnings/kort-id-warning/KortUrlWarning.tsx
@@ -8,15 +8,14 @@ export const KortUrlWarning = ({ maalgruppe }: Props) => {
     if (maalgruppe === 'employer') {
         return (
             <li>
-                Når målgruppen er satt til “arbeidsgiver,” må kort-URL-en begynne med
-                {' "/arbeidsgiver"'}.
+                Når målgruppen er satt til arbeidsgiver, må kort-URL-en begynne med /arbeidsgiver/
             </li>
         );
     } else if (maalgruppe === 'provider') {
         return (
             <li>
-                Når målgruppen er satt til “samarbeidspartner,” må kort-URL-en begynne med
-                {' "/samarbeidspartner"'}.
+                Når målgruppen er satt til samarbeidspartner, må kort-URL-en begynne med
+                /samarbeidspartner/
             </li>
         );
     } else {
