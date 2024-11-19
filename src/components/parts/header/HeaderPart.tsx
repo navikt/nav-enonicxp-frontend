11326f2ab825c486ab4e142bee88355f08f0fcd5
bdahle <bdahle@hey.com>
2024-11-19 13:16:14 +0100

    fjern styling Headerpart

--- src/components/parts/header/HeaderPart.tsx
+++ src/components/parts/header/HeaderPart.tsx
@@ -3,8 +3,6 @@ import { Header } from 'components/_common/headers/Header';
 import { HeadingTag, headingToLevel, headingToSize } from 'types/typo-style';
 import { PartComponentProps, PartType } from 'types/component-props/parts';
 
-import style from './HeaderPart.module.scss';
-
 export type PartConfigHeader = {
     title: string;
     anchorId: string;
@@ -27,7 +25,7 @@ export const HeaderPart = ({ config }: PartComponentProps<PartType.Header>) => {
     const size = headingToSize[tag];
 
     return (
-        <Header level={level} size={size} anchorId={anchorId} className={style.headerPart}>
+        <Header level={level} size={size} anchorId={anchorId}>
             {title}
         </Header>
     );
