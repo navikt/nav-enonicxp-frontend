f668b0e5e1187fbc8d2e1f18db27a77c02223d19
taniaholst <tania.holst@nav.no>
2024-12-02 12:50:46 +0100

    :speech_balloon: Endret fra i til på

--- src/components/_editor-only/warnings/form-numbers-warning/FormNumbersWarning.tsx
+++ src/components/_editor-only/warnings/form-numbers-warning/FormNumbersWarning.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const FormNumbersWarning = () => {
-    return <li>Skjemanummer må være i formatet {'NAV XX-XX.XX'}</li>;
+    return <li>Skjemanummer må være på formatet {'NAV XX-XX.XX'}</li>;
 };
