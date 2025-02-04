844ea7d746a66b535a9bf597a91cbe31395d858f
Terje Karlsen <terjeofnorway@gmail.com>
2025-02-04 13:57:31 +0100

    Implementerer bedre defauting og optional chaining

--- src/store/hooks/useOverviewFilters.ts
+++ src/store/hooks/useOverviewFilters.ts
@@ -16,7 +16,7 @@ import type Fuse from 'fuse.js';
 
 export type OverviewFilterableItem = {
     area: Area[];
-    taxonomy: ProductTaxonomy[];
+    taxonomy?: ProductTaxonomy[];
     type?: ContentType;
 };
 
