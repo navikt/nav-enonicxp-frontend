9d498ee14ce6b05aefc9e4845489c6a4e9f9c34d
Terje Karlsen <terjeofnorway@gmail.com>
2025-03-13 17:44:45 +0100

    Skriver om logikk for UX Signals

--- packages/nextjs/src/components/_common/uxsignalsWidget/UxSignalsWidget.tsx
+++ packages/nextjs/src/components/_common/uxsignalsWidget/UxSignalsWidget.tsx
@@ -1,22 +1,69 @@
 import React, { useEffect } from 'react';
 import { EditorHelp } from 'components/_editor-only/editor-help/EditorHelp';
+import { getCurrentConsent } from '@navikt/nav-dekoratoren-moduler';
 
 import style from './UxSignalsWidget.module.scss';
 
-type Props = {
+type UxSignalsWidgetProps = {
     embedCode: string;
 };
 
-export const UxSignalsWidget = ({ embedCode }: Props) => {
-    useEffect(() => {
+type Consent = {
+    consent: {
+        analytics: boolean;
+        surveys: boolean;
+    };
+    userActionTaken: boolean;
+};
+
+const uxSignalsScriptUrl = 'https://widget.uxsignals.com/embed.js';
+
+export const UxSignalsWidget = ({ embedCode }: UxSignalsWidgetProps) => {
+    // If the cookie banner is visible, the user has not taken any action yet.
+    // Wait and see if the user takes action before adding the script if consent is given..
+    const checkConsentOrWait = ({ consent, userActionTaken }: Consent) => {
+        if (consent.surveys && consent.analytics) {
+            addUXSignalsScript();
+            return;
+        }
+        if (!userActionTaken) {
+            setTimeout(() => {
+                console.log('waiting...');
+                checkConsentOrWait(getCurrentConsent());
+            }, 1000);
+        }
+
+        console.log('no consent or user action taken');
+    };
+
+    const addUXSignalsScript = () => {
+        if (document.querySelector(`script[src="${uxSignalsScriptUrl}"]`)) {
+            return;
+        }
+
+        console.log('adding script');
+
         const script = document.createElement('script');
-        script.src = 'https://widget.uxsignals.com/embed.js';
+        script.src = uxSignalsScriptUrl;
         script.async = true;
         document.body.appendChild(script);
-        return () => {
+    };
+
+    const removeUXSignalsScript = () => {
+        console.log('removing script');
+        const script = document.querySelector(`script[src="${uxSignalsScriptUrl}"]`);
+        if (script) {
             document.body.removeChild(script);
+        }
+    };
+
+    useEffect(() => {
+        const consent = getCurrentConsent();
+        checkConsentOrWait(consent);
+        return () => {
+            removeUXSignalsScript();
         };
-    });
+    }, []);
 
     return (
         <>
