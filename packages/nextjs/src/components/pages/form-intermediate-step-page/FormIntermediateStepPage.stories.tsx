4d84d46bc8decb807218572153856ea096dd3111
bdahle <bdahle@hey.com>
2025-03-31 12:21:08 +0200

    første utkast story FormIntermediateStepPage

---
+++ packages/nextjs/src/components/pages/form-intermediate-step-page/FormIntermediateStepPage.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ContentType } from 'types/content-props/_content-common';
+import { MediaType } from 'types/media';
+import { FormIntermediateStepPage } from './FormIntermediateStepPage';
+
+const meta = {
+    component: FormIntermediateStepPage,
+} satisfies Meta<typeof FormIntermediateStepPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        language: 'no',
+        displayName: 'Form Intermediate Step',
+        type: ContentType.FormIntermediateStepPage,
+        data: {
+            textAboveTitle: 'Forrige side',
+            title: 'Tittel',
+            illustration: {
+                type: ContentType.Pictograms,
+                data: {
+                    icons: [
+                        {
+                            icon: {
+                                type: MediaType.Vector,
+                                mediaUrl: 'placeholder',
+                            },
+                        },
+                    ],
+                },
+            },
+            editorial: {
+                processedHtml: '<p>Hva vil du gjøre?</p>',
+                macros: [],
+            },
+            steps: [
+                {
+                    label: 'Finn ledige stillinger',
+                    explanation:
+                        'Finn ledige stillinger, registrer CV og jobbprofil på Arbeidsplassen',
+                    nextStep: {
+                        _selected: 'next',
+                    },
+                },
+                {
+                    label: 'Finn ledige stillinger',
+                    explanation:
+                        'Finn ledige stillinger, registrer CV og jobbprofil på Arbeidsplassen',
+                    nextStep: {
+                        _selected: 'next',
+                    },
+                },
+            ],
+        },
+    },
+};
