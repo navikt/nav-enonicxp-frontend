42fb2ae4f09b444a6f35d83d5f6e18d80c5a9d4b
bdahle <bdahle@hey.com>
2025-03-31 13:17:54 +0200

    story for steg

--- packages/nextjs/src/components/pages/form-intermediate-step-page/FormIntermediateStepPage.stories.tsx
+++ packages/nextjs/src/components/pages/form-intermediate-step-page/FormIntermediateStepPage.stories.tsx
@@ -64,3 +64,66 @@ export const Default: Story = {
         },
     },
 };
+
+export const SecondStep: Story = {
+    args: {
+        _path: '/form-intermediate-step',
+        language: 'no',
+        displayName: 'Form Intermediate Step',
+        type: ContentType.FormIntermediateStepPage,
+        data: {
+            textAboveTitle: 'Forrige side',
+            title: 'Tittel',
+            illustration: {
+                type: ContentType.Pictograms,
+                data: {
+                    icons: [
+                        {
+                            icon: {
+                                type: MediaType.Vector,
+                                mediaUrl: 'placeholder',
+                            },
+                        },
+                    ],
+                },
+            },
+            editorial: {
+                processedHtml: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>',
+                macros: [],
+            },
+            steps: [
+                {
+                    label: 'Første steg',
+                    explanation: 'Dette er første steg',
+                    nextStep: {
+                        _selected: 'next',
+                        next: {
+                            textAboveTitle: 'Forrige side',
+                            title: 'Andre steg',
+                            editorial: {
+                                processedHtml: '<p>Dette er andre steg</p>',
+                                macros: [],
+                            },
+                            steps: [
+                                {
+                                    label: 'Neste steg',
+                                    explanation: 'Dette er neste steg',
+                                    nextStep: {
+                                        _selected: 'next',
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                },
+            ],
+        },
+    },
+    parameters: {
+        nextjs: {
+            router: {
+                asPath: '/form-intermediate-step?stegvalg=0',
+            },
+        },
+    },
+};
