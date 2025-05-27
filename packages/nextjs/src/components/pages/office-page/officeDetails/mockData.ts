152554d6bb5cebbedd7f5d67a95da22548f892b6
bdahle <bdahle@hey.com>
2025-05-27 10:33:34 +0200

    utvid åpningstider

--- packages/nextjs/src/components/pages/office-page/officeDetails/mockData.ts
+++ packages/nextjs/src/components/pages/office-page/officeDetails/mockData.ts
@@ -38,10 +38,10 @@ export const mockReception1: AudienceReception = {
     officeType: 'NAV_KONTOR',
     besoeksadresse: {
         type: 'stedsadresse' as const,
-        gatenavn: 'Karl Johans gate',
+        gatenavn: 'Testgate',
         husnummer: '1',
-        postnummer: '0154',
-        poststed: 'OSLO',
+        postnummer: '1234',
+        poststed: 'Herøy',
     },
     aapningstider: [
         {
@@ -54,6 +54,18 @@ export const mockReception1: AudienceReception = {
             fra: '09:00',
             til: '15:00',
         },
+        {
+            dag: 'Onsdag',
+            stengt: 'true',
+        },
+        {
+            dag: 'Torsdag',
+            kunTimeavtale: 'true',
+        },
+        { dag: 'Fredag', fra: '12:00', til: '15:00' },
+        { dato: '1999-12-24', stengt: 'true' },
+        { dato: '2042-12-24', stengt: 'true' },
+        { dato: '2042-12-31', fra: '09:00', til: '12:00' },
     ],
 };
 
@@ -64,7 +76,7 @@ export const mockReception2: AudienceReception = {
         gatenavn: 'Storgata',
         husnummer: '2',
         postnummer: '0155',
-        poststed: 'OSLO',
+        poststed: 'Dønna',
     },
     aapningstider: [
         {
