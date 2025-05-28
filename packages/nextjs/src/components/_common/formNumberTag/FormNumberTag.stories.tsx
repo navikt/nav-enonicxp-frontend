7f7e575ba247c32ebd20d5f636bacc724214da5d
taniaholst <tania.holst@nav.no>
2025-05-28 13:34:13 +0200

    storybook formNumberTag

---
+++ packages/nextjs/src/components/_common/formNumberTag/FormNumberTag.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import FormNumberTag from './FormNumberTag';
+
+const meta = {
+    component: FormNumberTag,
+    args: {
+        formNumber: 'NAV 44-23.69',
+    },
+} satisfies Meta<typeof FormNumberTag>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {},
+};
+
+export const Selected: Story = {
+    args: { selected: true},
+};
