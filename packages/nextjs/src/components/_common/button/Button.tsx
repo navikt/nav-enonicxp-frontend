2ad73ad27a42f4a0c0a8e024d6de98a718203dfd
Tania Holst <holst.tania@gmail.com>
2025-06-17 13:00:36 +0200

    bytter lenkebase med button i chatopion samt legger analytics på button

--- packages/nextjs/src/components/_common/button/Button.tsx
+++ packages/nextjs/src/components/_common/button/Button.tsx
@@ -5,6 +5,12 @@ import { LenkeBase } from 'components/_common/lenke/lenkeBase/LenkeBase';
 import { XpImageProps } from 'types/media';
 import { XpImage } from 'components/_common/image/XpImage';
 
+import { AnalyticsEvents, logAnalyticsEvent } from 'utils/analytics';
+import { onlyText } from 'utils/react-children';
+import { innholdsTypeMap } from 'types/content-props/_content-common';
+import { usePageContentProps } from 'store/pageContext';
+import { getDecoratorParams } from 'utils/decorator-utils';
+import { useLayoutConfig } from 'components/layouts/useLayoutConfig';
 import style from './Button.module.scss';
 
 type Props = {
@@ -20,6 +26,9 @@ type Props = {
     onClick?: (e: React.MouseEvent) => void;
     className?: string;
     children: React.ReactNode;
+    analyticsEvent?: AnalyticsEvents;
+    analyticsComponent?: string;
+    analyticsLabel?: string;
 };
 
 export const Button = ({
@@ -35,13 +44,28 @@ export const Button = ({
     onClick,
     className,
     children,
+    analyticsEvent,
+    analyticsComponent,
+    analyticsLabel,
 }: Props) => {
+    const contentProps = usePageContentProps();
+    const { context } = getDecoratorParams(contentProps);
+    const { layoutConfig } = useLayoutConfig();
+    const analyticsData = {
+        komponent: analyticsComponent,
+        seksjon: layoutConfig.title,
+        lenketekst: analyticsLabel || onlyText(children),
+        målgruppe: context,
+        innholdstype: innholdsTypeMap[contentProps.type],
+    };
+
     return (
         <DsButton
             as={LenkeBase}
             href={href || '#'}
             className={classNames(style.button, fullWidth && style.buttonFullWidth, className)}
             onClick={(e) => {
+                logAnalyticsEvent(analyticsEvent || AnalyticsEvents.NAVIGATION, analyticsData);
                 if (!href) {
                     e.preventDefault();
                 }
