c29ea9246fb0933e840425de0dd29fafac633ec0
Terje Karlsen <terje@terjeofnorway.no>
2025-07-02 16:30:13 +0200

    Leser BUILD_ID fra nextjs-workspacepakken

--- packages/server/src/next-utils.ts
+++ packages/server/src/next-utils.ts
@@ -6,8 +6,14 @@ let cachedBuildId: string | null = null;
 export const getNextBuildId = async (): Promise<string> => {
     if (cachedBuildId) return cachedBuildId;
 
+    let buildIdPath = path.join(process.cwd(), 'nextjs', '.next', 'BUILD_ID');
+
+    if (process.env.NODE_ENV === 'test' || process.env.JEST_WORKER_ID !== undefined) {
+        // The test (process.cwd) already runs in the nextjs directory.
+        buildIdPath = path.join(process.cwd(), '.next', 'BUILD_ID');
+    }
+
     try {
-        const buildIdPath = path.join(process.cwd(), '.next', 'BUILD_ID');
         cachedBuildId = await fs.readFile(buildIdPath, 'utf-8');
         return cachedBuildId.trim();
     } catch (error) {
