ffa6e6b8cf6eee1292da604e81aba3f889056f5c
Tania Holst <holst.tania@gmail.com>
2025-07-29 14:31:13 +0200

    hvis htmlarea er i intro-seksjonen skal dette ignoreres

--- packages/nextjs/src/components/_editor-only/global-warnings/warnings/part-utenfor-innholdsseksjon/htmlAreaIsInPageContentButNotInContentSection.tsx
+++ packages/nextjs/src/components/_editor-only/global-warnings/warnings/part-utenfor-innholdsseksjon/htmlAreaIsInPageContentButNotInContentSection.tsx
@@ -6,10 +6,12 @@ export const htmlAreaIsInPageContentButNotInContentSection = (node: {
 
     const startsWithPageContent = /^\/pageContent\//;
     const isInContentSection = /^\/pageContent\/\d+\/content\/\d+$/;
+    const isInIntroSection = /^\/pageContent\/\d+\/intro\/\d+$/;
 
     return (
         node.descriptor === 'no.nav.navno:html-area' &&
         startsWithPageContent.test(node.path) &&
-        !isInContentSection.test(node.path)
+        !isInContentSection.test(node.path) &&
+        !isInIntroSection.test(node.path)
     );
 };
