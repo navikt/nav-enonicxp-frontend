5cd7834d0d27df03f693ed0f6f1632b1d5e1e0c2
Terje Karlsen <terje@terjeofnorway.no>
2025-08-13 19:55:47 +0200

    Oppdaterer til Express5 (#2394)
    * Oppdaterer til Express5
    * Caster params for buildId
    * Sørger for at liveness fungerer korrekt
    * Tester annen path
    * Forenkler siste path handler

--- packages/server/src/server-setup/server-setup-failover.ts
+++ packages/server/src/server-setup/server-setup-failover.ts
@@ -7,13 +7,13 @@ export const serverSetupFailover = (expressApp: Express, nextApp: InferredNextWr
     const nextRequestHandler = nextApp.getRequestHandler();
 
     // Assets from /_next and internal apis should be served as normal
-    expressApp.get(['/_next/*', '/api/internal/*'], (req, res) => {
+    expressApp.get(['/_next{/*path}', '/api/internal{/*path}'], (req, res) => {
         return nextRequestHandler(req, res);
     });
 
     // We don't want the full site to be publicly available via failover instance.
     // This is served via the public-facing regular frontend when needed
-    expressApp.all('*', (req, res) => {
+    expressApp.all('/*path', (req, res) => {
         if (!validateSecretHeader(req)) {
             res.status(404).send();
             return;
