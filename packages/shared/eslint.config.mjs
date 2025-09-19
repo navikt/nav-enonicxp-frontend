724d64dfa9714ea3e8b4232524155958004efd4d
Terje Karlsen <terje@terjeofnorway.no>
2025-09-19 14:58:23 +0200

    Fikser feil i bygg pga dobbel React

--- packages/shared/eslint.config.mjs
+++ packages/shared/eslint.config.mjs
@@ -25,6 +25,7 @@ export default [
                 },
             ],
             '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/triple-slash-reference': 'off',
         },
     },
     {
