e368f9284fb175900fd591042bdad08d8e50fbcb
Terje Karlsen <terje@terjeofnorway.no>
2025-10-15 12:44:27 +0200

    Siste endring av 'entity' => 'thing' for json-ld-generering

--- packages/nextjs/src/components/_common/metatags/structuredData/json-ld-generator.ts
+++ packages/nextjs/src/components/_common/metatags/structuredData/json-ld-generator.ts
@@ -6,28 +6,28 @@ import { generateOrganizationThing } from './things/organizationThing';
 import { applyWebPageReferences, generateWebPageThing } from './things/webPageThing';
 import { createThingLibraries } from './helpers/thingHelpers';
 
-const createReferencesBetweenEntities = (entities: Thing[]): Thing[] => {
-    const { thingsByType } = createThingLibraries(entities);
+const createReferencesBetweenThings = (things: Thing[]): Thing[] => {
+    const { thingsByType } = createThingLibraries(things);
 
-    return entities.map((entity) => {
-        if (!entity || typeof entity !== 'object' || Array.isArray(entity)) {
-            return entity;
+    return things.map((thing) => {
+        if (!thing || typeof thing !== 'object' || Array.isArray(thing)) {
+            return thing;
         }
 
-        // Apply reference rules based on entity type
-        if (entity['@type'] === 'WebPage') {
-            return applyWebPageReferences(entity, thingsByType);
+        // Apply reference rules based on thing type
+        if (thing['@type'] === 'WebPage') {
+            return applyWebPageReferences(thing, thingsByType);
         }
 
-        if (entity['@type'] === 'GovernmentOffice') {
-            return applyOfficeBranchReferences(entity, thingsByType);
+        if (thing['@type'] === 'GovernmentOffice') {
+            return applyOfficeBranchReferences(thing, thingsByType);
         }
 
-        return entity;
+        return thing;
     });
 };
 
-const buildCommonEntities = (content: ContentProps): Thing[] => {
+const buildCommonThings = (content: ContentProps): Thing[] => {
     return [
         generateImageObjectThing(),
         generateOrganizationThing(),
@@ -35,36 +35,36 @@ const buildCommonEntities = (content: ContentProps): Thing[] => {
     ];
 };
 
-const buildPageSpecificEntities = (content: ContentProps): Thing[] => {
-    const entities: Thing[] = [];
+const buildPageSpecificThings = (content: ContentProps): Thing[] => {
+    const things: Thing[] = [];
 
-    // Add office branch entity for office pages
+    // Add office branch thing for office pages
     if (content.type === ContentType.OfficePage) {
         // For now, do not augment with office branch data. We need a baseline before moving on to
         // more complex structures.
-        // entities.push(generateOfficeBranchThing({ content }));
+        // things.push(generateOfficeBranchThing({ content }));
     }
 
-    return entities;
+    return things;
 };
 
-const buildAllEntities = (content: ContentProps): Thing[] => {
-    const commonEntities = buildCommonEntities(content);
-    const pageSpecificEntities = buildPageSpecificEntities(content);
+const buildAllThings = (content: ContentProps): Thing[] => {
+    const commonThings = buildCommonThings(content);
+    const pageSpecificThings = buildPageSpecificThings(content);
 
-    return [...commonEntities, ...pageSpecificEntities];
+    return [...commonThings, ...pageSpecificThings];
 };
 
-const generateGraph = (graphEntities: Thing[]): JsonLdData => {
+const generateGraph = (things: Thing[]): JsonLdData => {
     return {
         '@context': 'https://schema.org',
-        '@graph': graphEntities,
+        '@graph': things,
     };
 };
 
 export const generateJsonLd = (content: ContentProps): JsonLdData | null => {
-    const unreferencedEntities = buildAllEntities(content);
-    const referencedEntities = createReferencesBetweenEntities(unreferencedEntities);
+    const unreferencedThings = buildAllThings(content);
+    const referencedThings = createReferencesBetweenThings(unreferencedThings);
 
-    return generateGraph(referencedEntities);
+    return generateGraph(referencedThings);
 };
