14718982fd6ccb1ae5e046884ab1d22c5ee9031b
bdahle <bdahle@hey.com>
2026-01-12 14:43:08 +0100

    rendre kontaktmodulen på situasjonssider utenfor griden

--- packages/nextjs/src/components/pages/situationPage/SituationPage.tsx
+++ packages/nextjs/src/components/pages/situationPage/SituationPage.tsx
@@ -10,11 +10,35 @@ export const SituationPage = (props: SituationPageProps) => {
     const { languages } = usePageContentProps();
     const hasMultipleLanguages = languages && languages?.length > 0;
 
+    const pageRegions = props.page.regions;
+    const pageContentRegion = pageRegions?.pageContent;
+    const pageContentComponents = pageContentRegion?.components ?? [];
+    const hasTailComponent = pageContentComponents.length > 0;
+
+    const lastComponent = hasTailComponent
+        ? pageContentComponents[pageContentComponents.length - 1]
+        : undefined;
+
+    const componentPropsWithoutLast =
+        hasTailComponent && pageRegions && pageContentRegion
+            ? {
+                  ...props.page,
+                  regions: {
+                      ...pageRegions,
+                      pageContent: {
+                          ...pageContentRegion,
+                          components: pageContentComponents.slice(0, -1),
+                      },
+                  },
+              }
+            : props.page;
+
     return (
         <article className={styles.situationPage}>
             <div className={classNames(styles.content, hasMultipleLanguages && styles.pullUp)}>
-                <ComponentMapper componentProps={props.page} pageProps={props} />
+                <ComponentMapper componentProps={componentPropsWithoutLast} pageProps={props} />
             </div>
+            {lastComponent && <ComponentMapper componentProps={lastComponent} pageProps={props} />}
         </article>
     );
 };
