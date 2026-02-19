3aeadb8472f252c01a6c8d235a383ec8757fdfb1
bdahle <bdahle@hey.com>
2026-02-19 12:23:06 +0100

    Håndterer editorvisning for situasjonsside ved å rendre komponenter innenfor sin region

--- packages/nextjs/src/components/pages/situationPage/SituationPage.tsx
+++ packages/nextjs/src/components/pages/situationPage/SituationPage.tsx
@@ -63,8 +63,12 @@ export const SituationPage = (props: SituationPageProps) => {
     const { languages } = usePageContentProps();
     const hasMultipleLanguages = languages && languages?.length > 0;
 
+    // I editorvisning må alle komponenter rendres innenfor sin region for at XP-editoren
+    // skal kunne finne parent-regionen. Uten dette fungerer ikke f.eks. "Fjern"-knappen.
     const { componentsWithoutContactModule, contactModuleComponent } =
-        extractLastComponentIfContactModule(props.page);
+        props.editorView === 'edit'
+            ? { componentsWithoutContactModule: props.page, contactModuleComponent: undefined }
+            : extractLastComponentIfContactModule(props.page);
 
     return (
         <article className={styles.situationPage}>
