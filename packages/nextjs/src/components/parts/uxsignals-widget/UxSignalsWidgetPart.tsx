fd7028de75f7ce56242356e51e57e556edf88468
Terje Karlsen <terje@terjeofnorway.no>
2026-02-27 13:50:11 +0100

    Legger inn redaktørinfo om cookies og ux signal

--- packages/nextjs/src/components/parts/uxsignals-widget/UxSignalsWidgetPart.tsx
+++ packages/nextjs/src/components/parts/uxsignals-widget/UxSignalsWidgetPart.tsx
@@ -2,17 +2,34 @@ import React from 'react';
 import { EditorHelp } from 'components/_editor-only/editorHelp/EditorHelp';
 import { UxSignalsWidget } from 'components/_common/uxsignalsWidget/UxSignalsWidget';
 import { PartComponentProps, PartType } from 'types/component-props/parts';
+import { usePageContentProps } from 'store/pageContext';
 
 export type PartConfigUxSignalsWidget = {
     embedCode: string;
 };
 
 export const UxSignalsWidgetPart = ({ config }: PartComponentProps<PartType.UxSignalsWidget>) => {
+    const { editorView } = usePageContentProps();
     if (!config?.embedCode) {
         return (
             <EditorHelp text={'Tom UXSignals komponent, sett inn en embed-kode'} type={'error'} />
         );
     }
 
-    return <UxSignalsWidget embedCode={config.embedCode} />;
+    const cookieInformation =
+        editorView === 'edit' ? (
+            <EditorHelp
+                text={
+                    'Merk! For å se undersøkelsen fra UX Signal i forhåndsvisningen i Enonic, må du samtykke til bruk av informasjonskapsler på nav.no. Du kan endre samtykke for informasjonskapsler på i footeren.'
+                }
+                type={'info'}
+            />
+        ) : null;
+
+    return (
+        <>
+            {cookieInformation}
+            <UxSignalsWidget embedCode={config.embedCode} />
+        </>
+    );
 };
