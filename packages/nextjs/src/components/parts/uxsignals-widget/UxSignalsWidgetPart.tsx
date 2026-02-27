cc9a03d73f599631b4f558b08de805c02b19e430
Terje Karlsen <terje@terjeofnorway.no>
2026-02-27 14:04:47 +0100

    Endrer informasjonstekst for cookies i uxwidget

--- packages/nextjs/src/components/parts/uxsignals-widget/UxSignalsWidgetPart.tsx
+++ packages/nextjs/src/components/parts/uxsignals-widget/UxSignalsWidgetPart.tsx
@@ -2,34 +2,17 @@ import React from 'react';
 import { EditorHelp } from 'components/_editor-only/editorHelp/EditorHelp';
 import { UxSignalsWidget } from 'components/_common/uxsignalsWidget/UxSignalsWidget';
 import { PartComponentProps, PartType } from 'types/component-props/parts';
-import { usePageContentProps } from 'store/pageContext';
 
 export type PartConfigUxSignalsWidget = {
     embedCode: string;
 };
 
 export const UxSignalsWidgetPart = ({ config }: PartComponentProps<PartType.UxSignalsWidget>) => {
-    const { editorView } = usePageContentProps();
     if (!config?.embedCode) {
         return (
             <EditorHelp text={'Tom UXSignals komponent, sett inn en embed-kode'} type={'error'} />
         );
     }
 
-    const cookieInformation =
-        editorView === 'edit' ? (
-            <EditorHelp
-                text={
-                    'Merk! For å se undersøkelsen fra UX Signal i forhåndsvisningen i Enonic, må du samtykke til bruk av informasjonskapsler på nav.no. Du kan endre samtykke for informasjonskapsler på i footeren.'
-                }
-                type={'info'}
-            />
-        ) : null;
-
-    return (
-        <>
-            {cookieInformation}
-            <UxSignalsWidget embedCode={config.embedCode} />
-        </>
-    );
+    return <UxSignalsWidget embedCode={config.embedCode} />;
 };
