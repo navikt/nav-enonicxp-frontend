190434027a9b3c093de65d2ed01504e546b4561e
bdahle <bdahle@hey.com>
2026-05-07 15:43:34 +0200

    Erstatt Alert med InfoCard.Message i VarselboksPart

--- packages/nextjs/src/components/parts/varselboks/VarselboksPart.tsx
+++ packages/nextjs/src/components/parts/varselboks/VarselboksPart.tsx
@@ -1,30 +1,14 @@
 import React from 'react';
-import { AlertProps } from '@navikt/ds-react';
+import { InfoCard } from '@navikt/ds-react';
+import { InformationSquareIcon } from '@navikt/aksel-icons';
 import { ParsedHtml } from 'components/_common/parsedHtml/ParsedHtml';
-import { Varselboks } from 'components/_common/varselboks/Varselboks';
 import { EditorHelp } from 'components/_editor-only/editorHelp/EditorHelp';
 import { PartComponentProps, PartType } from 'types/component-props/parts';
 import { ProcessedHtmlProps } from 'types/processed-html-props';
 import style from './VarselboksPart.module.scss';
 
-// These types were used by a previous version of the design system component
-// and are still used for the type property on the backend
-type AlertTypeLegacy = 'info' | 'advarsel' | 'feil' | 'suksess';
-
-const legacyTypeToVariant: {
-    [type in AlertTypeLegacy]: AlertProps['variant'];
-} = {
-    info: 'info',
-    advarsel: 'warning',
-    feil: 'error',
-    suksess: 'success',
-};
-
 export type PartConfigVarselboks = {
     content: ProcessedHtmlProps;
-    type: 'info' | 'advarsel' | 'feil' | 'suksess';
-    size?: 'small' | 'medium';
-    inline?: boolean;
     margin: string;
 };
 
@@ -33,19 +17,18 @@ export const VarselboksPart = ({ config }: PartComponentProps<PartType.Varselbok
         return <EditorHelp text={'Varselboksen er ikke konfigurert'} />;
     }
 
-    const { content, type, size, inline, margin } = config;
+    const { content, margin } = config;
 
     return (
-        <Varselboks
-            variant={legacyTypeToVariant[type] || 'info'}
-            size={size}
-            inline={inline}
+        <InfoCard
             className={style.varselboks}
             style={{
                 ...(margin && { margin }),
             }}
         >
-            <ParsedHtml htmlProps={content} />
-        </Varselboks>
+            <InfoCard.Message icon={<InformationSquareIcon aria-hidden />}>
+                <ParsedHtml htmlProps={content} />
+            </InfoCard.Message>
+        </InfoCard>
     );
 };
