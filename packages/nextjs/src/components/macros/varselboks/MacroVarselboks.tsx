f9589b030c6a13edfc942d15dc14b186522f9ace
bdahle <bdahle@hey.com>
2026-05-08 11:38:45 +0200

    Refactor MacroVarselboks to use InfoCard instead of Varselboks and remove unused props

--- packages/nextjs/src/components/macros/varselboks/MacroVarselboks.tsx
+++ packages/nextjs/src/components/macros/varselboks/MacroVarselboks.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
+import { InfoCard } from '@navikt/ds-react';
+import { InformationSquareIcon } from '@navikt/aksel-icons';
 import { ParsedHtml } from 'components/_common/parsedHtml/ParsedHtml';
 import { MacroPropsCommon, MacroType } from 'types/macro-props/_macros-common';
-import { Varselboks } from 'components/_common/varselboks/Varselboks';
+
 
 export interface MacroVarselboksProps extends MacroPropsCommon {
     name: MacroType.Varselboks;
     config: {
         alert_box: {
             body: string;
-            type: 'error' | 'warning' | 'info' | 'success';
-            size?: 'small' | 'medium';
-            inline?: boolean;
         };
     };
 }
@@ -20,11 +19,13 @@ export const MacroVarselboks = ({ config }: MacroVarselboksProps) => {
         return null;
     }
 
-    const { body, size, type, inline } = config.alert_box;
+    const { body } = config.alert_box;
 
     return (
-        <Varselboks variant={type} size={size} inline={inline}>
-            <ParsedHtml htmlProps={body} />
-        </Varselboks>
+        <InfoCard>
+            <InfoCard.Message icon={<InformationSquareIcon aria-hidden />}>
+                <ParsedHtml htmlProps={body} />
+            </InfoCard.Message>
+        </InfoCard>
     );
 };
