e87201d75fa0cc773de9c30356f5a9db2106240d
Bjørn Harbo Dahle <bdahle@hey.com>
2026-05-29 11:15:48 +0200

    Add logAnalyticsEvent and Events proxy to mock analytics functionality (#2914)
    * Add logAnalyticsEvent and Events proxy to mock analytics functionality
    
    * Refactor analytics event handling by removing the Proxy for Events and directly exporting from analytics-types
    
    * Re-eksporter Events fra dekoratoren-moduler i Storybook-mocken

--- packages/nextjs/.storybook/mocks/nav-dekoratoren-moduler.js
+++ packages/nextjs/.storybook/mocks/nav-dekoratoren-moduler.js
@@ -1,6 +1,10 @@
+export { Events } from '@navikt/nav-dekoratoren-moduler/csr/index.js';
+
 export const getCurrentConsent = () => ({
     consent: { analytics: true, surveys: true },
     userActionTaken: true,
 });
 
 export const setParams = () => {};
+
+export const logAnalyticsEvent = () => Promise.resolve();
