f250887f8b90b03a243c0540f57cab78f6432092
Terje Karlsen <terje@terjeofnorway.no>
2026-07-31 09:39:10 +0200

    Fjerner pino-pretty fra logger

--- packages/shared/src/logger.ts
+++ packages/shared/src/logger.ts
@@ -1,28 +1,13 @@
 import pino, { LoggerOptions } from 'pino';
-import { PHASE_PRODUCTION_BUILD } from 'next/constants';
 
-const isBuildPhase =
-    typeof process.env !== 'undefined' && process.env.NEXT_PHASE === PHASE_PRODUCTION_BUILD;
-
-const options: LoggerOptions =
-    process.env.ENV === 'localhost' || isBuildPhase
-        ? {
-              base: {},
-              transport: {
-                  target: 'pino-pretty',
-                  options: {
-                      colorize: true,
-                  },
-              },
-          }
-        : {
-              base: { buildId: process.env.BUILD_ID },
-              formatters: {
-                  level: (label) => {
-                      return { level: label };
-                  },
-              },
-          };
+const options: LoggerOptions = {
+    base: { buildId: process.env.BUILD_ID },
+    formatters: {
+        level: (label) => {
+            return { level: label };
+        },
+    },
+};
 
 const pinoInstance = pino(options);
 
