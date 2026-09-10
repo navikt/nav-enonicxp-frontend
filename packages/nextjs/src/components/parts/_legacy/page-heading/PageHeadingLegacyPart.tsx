376b957148fc0261ca4a2956a681659e80c525db
Martin Berglund <martin.berglund@nav.no>
2026-09-10 13:10:02 +0200

    Få med nødvendige data og typer

--- packages/nextjs/src/components/parts/_legacy/page-heading/PageHeadingLegacyPart.tsx
+++ packages/nextjs/src/components/parts/_legacy/page-heading/PageHeadingLegacyPart.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { BodyLong, Heading } from '@navikt/ds-react';
 import ArtikkelDato from 'components/parts/_legacy/artikkel/komponenter/ArtikkelDato';
 import { ContentProps, ContentType } from 'types/content-props/_content-common';
+import { ArtikkelData } from 'types/content-props/artikkel-props';
 
 import style from './PageHeading.module.scss';
 
 export const PageHeadingLegacyPart = (props: ContentProps) => {
     const displayName = props.displayName;
+    const data = props.data as ArtikkelData;
+    const isNewsArticle = data.contentType === 'news' || data.contentType === 'pressRelease';
     const ingress = props.type !== ContentType.SectionPage && props.data?.ingress;
 
     return (
